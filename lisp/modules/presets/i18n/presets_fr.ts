da57ac42a0ae83a72d7ef246ab15612971258ddd
Olivier Humbert <trebmuh@users.noreply.github.com>
2017-02-14 14:49:05 +0100

    Update French translation (#63)
    French translation of lisp/modules/presets

--- lisp/modules/presets/i18n/presets_fr.ts
+++ lisp/modules/presets/i18n/presets_fr.ts
@@ -5,12 +5,12 @@
     <message>
         <location filename="../presets_ui.py" line="215"/>
         <source>Create Cue</source>
-        <translation type="unfinished"></translation>
+        <translation>Créer un cue</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="216"/>
         <source>Load on selected Cues</source>
-        <translation type="unfinished"></translation>
+        <translation>Charger les cues sélectionnés</translation>
     </message>
 </context>
 <context>
@@ -18,122 +18,122 @@
     <message>
         <location filename="../presets_ui.py" line="364"/>
         <source>Presets</source>
-        <translation type="unfinished"></translation>
+        <translation>Pré-réglages</translation>
     </message>
     <message>
         <location filename="../presets.py" line="52"/>
         <source>Load preset</source>
-        <translation type="unfinished"></translation>
+        <translation>Charger un pré-réglage</translation>
     </message>
     <message>
         <location filename="../presets.py" line="56"/>
         <source>Save as preset</source>
-        <translation type="unfinished"></translation>
+        <translation>Sauvegarder un pré-réglage sous</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="52"/>
         <source>Cannot scan presets</source>
-        <translation type="unfinished"></translation>
+        <translation>Impossible d&apos;examiner les pré-réglages</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="60"/>
         <source>Error while deleting preset &quot;{}&quot;</source>
-        <translation type="unfinished"></translation>
+        <translation>Erreur lors de la suppression du pré-réglage &quot;{}&quot;</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="68"/>
         <source>Cannot load preset &quot;{}&quot;</source>
-        <translation type="unfinished"></translation>
+        <translation>Impossible de charger le pré-réglage &quot;{}&quot;</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="76"/>
         <source>Cannot save preset &quot;{}&quot;</source>
-        <translation type="unfinished"></translation>
+        <translation>Impossible de sauvegarder le pré-réglage &quot;{}&quot;</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="84"/>
         <source>Cannot rename preset &quot;{}&quot;</source>
-        <translation type="unfinished"></translation>
+        <translation>Impossible de renommer le pré-réglage &quot;{}&quot;</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="96"/>
         <source>Select Preset</source>
-        <translation type="unfinished"></translation>
+        <translation>Sélectionner le pré-réglage</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="109"/>
         <source>Preset already exists, overwrite?</source>
-        <translation type="unfinished"></translation>
+        <translation>Le pré-réglage existe déjà, l&apos;écraser ?</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="419"/>
         <source>Preset name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nom du pré-réglage</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="211"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajouter</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="212"/>
         <source>Rename</source>
-        <translation type="unfinished"></translation>
+        <translation>Renommer</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="213"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>Éditer</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="214"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Supprimer</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="217"/>
         <source>Export selected</source>
-        <translation type="unfinished"></translation>
+        <translation>Exporter le sélectionné</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="218"/>
         <source>Import</source>
-        <translation type="unfinished"></translation>
+        <translation>Importer</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="301"/>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation>Attention</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="256"/>
         <source>The same name is already used!</source>
-        <translation type="unfinished"></translation>
+        <translation>Le même nom est déjà utilisé !</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="301"/>
         <source>Cannot create a cue from this preset: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>Impossible de créer un cue depuis ce pré-réglage : {}</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="339"/>
         <source>Cannot export correctly.</source>
-        <translation type="unfinished"></translation>
+        <translation>Impossible d&apos;exporter correctement</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="351"/>
         <source>Some presets already exists, overwrite?</source>
-        <translation type="unfinished"></translation>
+        <translation>Certains pré-réglages existent déjà, les écraser ?</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="364"/>
         <source>Cannot import correctly.</source>
-        <translation type="unfinished"></translation>
+        <translation>Impossible d&apos;importer correctement.</translation>
     </message>
     <message>
         <location filename="../presets_ui.py" line="420"/>
         <source>Cue type</source>
-        <translation type="unfinished"></translation>
+        <translation>Type de cue</translation>
     </message>
 </context>
 </TS>
