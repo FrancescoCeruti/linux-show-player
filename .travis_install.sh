13574d38077ce1fe06571594b4eca7ef65d5f8d0
Francesco Ceruti <ceppofrancy@gmail.com>
2017-02-17 15:21:00 +0100

    Update build script

--- .travis_install.sh
+++ .travis_install.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-# If the python3 version is < 3.5
-if [ `python3 -c 'import sys; print(sys.version_info.major >= 3 and sys.version_info.minor >= 5)'` = "False" ];
-then
-    curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-    pyenv install 3.5.3
-    export PATH="~/.pyenv/bin:$PATH"
-    eval "$(pyenv init -)"
-    #eval "$(pyenv virtualenv-init -)"
-    pyenv global 3.5.3
-fi
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+pyenv install 3.5.3
+export PATH="~/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+#eval "$(pyenv virtualenv-init -)"
+pyenv global 3.5.3
