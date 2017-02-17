1236de76f3051603518e77274de7cda9b329bade
Francesco Ceruti <ceppofrancy@gmail.com>
2017-02-17 15:56:17 +0100

    Update travis build scripts (maybe now?)

--- .travis_install.sh
+++ .travis_install.sh
@@ -4,9 +4,10 @@ set -e
 set -x
 
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-export PATH="~/.pyenv/bin:$PATH"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 #eval "$(pyenv virtualenv-init -)"
-pyenv update
-pyenv install 3.5.3
+pyenv install --list
+pyenv install -s 3.5.3
 pyenv global 3.5.3
