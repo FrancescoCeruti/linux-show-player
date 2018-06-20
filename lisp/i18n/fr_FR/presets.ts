085fcd8bce574fda10a36da08b1d0ebb8277dcee
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-20 20:02:18 +0200

    New translations presets.ts (French)

--- lisp/i18n/fr_FR/presets.ts
+++ lisp/i18n/fr_FR/presets.ts
@@ -6,12 +6,12 @@
     <message>
       <location filename="../presets_ui.py" line="215"/>
       <source>Create Cue</source>
-      <translation type="unfinished">Create Cue</translation>
+      <translation>Cr&#xE9;er un cue</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="216"/>
       <source>Load on selected Cues</source>
-      <translation type="unfinished">Load on selected Cues</translation>
+      <translation>Charger les cues s&#xE9;lectionn&#xE9;s</translation>
     </message>
   </context>
   <context>
@@ -19,122 +19,122 @@
     <message>
       <location filename="../presets_ui.py" line="364"/>
       <source>Presets</source>
-      <translation type="unfinished">Presets</translation>
+      <translation>Pr&#xE9;-r&#xE9;glages</translation>
     </message>
     <message>
       <location filename="../presets.py" line="52"/>
       <source>Load preset</source>
-      <translation type="unfinished">Load preset</translation>
+      <translation>Charger un pr&#xE9;-r&#xE9;glage</translation>
     </message>
     <message>
       <location filename="../presets.py" line="56"/>
       <source>Save as preset</source>
-      <translation type="unfinished">Save as preset</translation>
+      <translation>Sauvegarder un pr&#xE9;-r&#xE9;glage sous</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="52"/>
       <source>Cannot scan presets</source>
-      <translation type="unfinished">Cannot scan presets</translation>
+      <translation>Impossible d&apos;examiner les pr&#xE9;-r&#xE9;glages</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="60"/>
       <source>Error while deleting preset &quot;{}&quot;</source>
-      <translation type="unfinished">Error while deleting preset &quot;{}&quot;</translation>
+      <translation>Erreur lors de la suppression du pr&#xE9;-r&#xE9;glage &quot;{}&quot;</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="68"/>
       <source>Cannot load preset &quot;{}&quot;</source>
-      <translation type="unfinished">Cannot load preset &quot;{}&quot;</translation>
+      <translation>Impossible de charger le pr&#xE9;-r&#xE9;glage &quot;{}&quot;</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="76"/>
       <source>Cannot save preset &quot;{}&quot;</source>
-      <translation type="unfinished">Cannot save preset &quot;{}&quot;</translation>
+      <translation>Impossible de sauvegarder le pr&#xE9;-r&#xE9;glage &quot;{}&quot;</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="84"/>
       <source>Cannot rename preset &quot;{}&quot;</source>
-      <translation type="unfinished">Cannot rename preset &quot;{}&quot;</translation>
+      <translation>Impossible de renommer le pr&#xE9;-r&#xE9;glage &quot;{}&quot;</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="96"/>
       <source>Select Preset</source>
-      <translation type="unfinished">Select Preset</translation>
+      <translation>S&#xE9;lectionner le pr&#xE9;-r&#xE9;glage</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="109"/>
       <source>Preset already exists, overwrite?</source>
-      <translation type="unfinished">Preset already exists, overwrite?</translation>
+      <translation>Le pr&#xE9;-r&#xE9;glage existe d&#xE9;j&#xE0;, l&apos;&#xE9;craser ?</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="419"/>
       <source>Preset name</source>
-      <translation type="unfinished">Preset name</translation>
+      <translation>Nom du pr&#xE9;-r&#xE9;glage</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="211"/>
       <source>Add</source>
-      <translation type="unfinished">Add</translation>
+      <translation>Ajouter</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="212"/>
       <source>Rename</source>
-      <translation type="unfinished">Rename</translation>
+      <translation>Renommer</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="213"/>
       <source>Edit</source>
-      <translation type="unfinished">Edit</translation>
+      <translation>&#xC9;diter</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="214"/>
       <source>Remove</source>
-      <translation type="unfinished">Remove</translation>
+      <translation>Supprimer</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="217"/>
       <source>Export selected</source>
-      <translation type="unfinished">Export selected</translation>
+      <translation>Exporter le s&#xE9;lectionn&#xE9;</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="218"/>
       <source>Import</source>
-      <translation type="unfinished">Import</translation>
+      <translation>Importer</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="301"/>
       <source>Warning</source>
-      <translation type="unfinished">Warning</translation>
+      <translation>Attention</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="256"/>
       <source>The same name is already used!</source>
-      <translation type="unfinished">The same name is already used!</translation>
+      <translation>Le m&#xEA;me nom est d&#xE9;j&#xE0; utilis&#xE9; !</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="301"/>
       <source>Cannot create a cue from this preset: {}</source>
-      <translation type="unfinished">Cannot create a cue from this preset: {}</translation>
+      <translation>Impossible de cr&#xE9;er un cue depuis ce pr&#xE9;-r&#xE9;glage : {}</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="339"/>
       <source>Cannot export correctly.</source>
-      <translation type="unfinished">Cannot export correctly.</translation>
+      <translation>Impossible d&apos;exporter correctement</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="351"/>
       <source>Some presets already exists, overwrite?</source>
-      <translation type="unfinished">Some presets already exists, overwrite?</translation>
+      <translation>Certains pr&#xE9;-r&#xE9;glages existent d&#xE9;j&#xE0;, les &#xE9;craser ?</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="364"/>
       <source>Cannot import correctly.</source>
-      <translation type="unfinished">Cannot import correctly.</translation>
+      <translation>Impossible d&apos;importer correctement.</translation>
     </message>
     <message>
       <location filename="../presets_ui.py" line="420"/>
       <source>Cue type</source>
-      <translation type="unfinished">Cue type</translation>
+      <translation>Type de cue</translation>
     </message>
   </context>
 </TS>
