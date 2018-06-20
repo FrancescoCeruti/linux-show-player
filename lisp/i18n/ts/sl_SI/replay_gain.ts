f56e44f16f4c04e6fa39029aae1792b5ee9c4e0e
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-20 20:22:05 +0200

    New translations replay_gain.ts (Slovenian)

---
+++ lisp/i18n/ts/sl_SI/replay_gain.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0"?>
+<!DOCTYPE TS>
+<TS language="sl" version="2.0" sourcelanguage="en">
+  <context>
+    <name>ReplayGain</name>
+    <message>
+      <location filename="../gain_ui.py" line="101"/>
+      <source>ReplayGain / Normalization</source>
+      <translation>Oja&#x10D;anje predvajanja / Normalizacija</translation>
+    </message>
+    <message>
+      <location filename="../replay_gain.py" line="163"/>
+      <source>Calculate</source>
+      <translation>Izra&#x10D;unaj</translation>
+    </message>
+    <message>
+      <location filename="../replay_gain.py" line="168"/>
+      <source>Reset all</source>
+      <translation>Ponastavi vse</translation>
+    </message>
+    <message>
+      <location filename="../replay_gain.py" line="173"/>
+      <source>Reset selected</source>
+      <translation>Ponastavi izbrano</translation>
+    </message>
+    <message>
+      <location filename="../gain_ui.py" line="103"/>
+      <source>Threads number</source>
+      <translation>&#x160;tevilo niti</translation>
+    </message>
+    <message>
+      <location filename="../gain_ui.py" line="104"/>
+      <source>Apply only to selected media</source>
+      <translation>Uveljavi samo na izbranih medijih</translation>
+    </message>
+    <message>
+      <location filename="../gain_ui.py" line="106"/>
+      <source>ReplayGain to (dB SPL)</source>
+      <translation>Oja&#x10D;aj predvajanje na (dB SPL)</translation>
+    </message>
+    <message>
+      <location filename="../gain_ui.py" line="108"/>
+      <source>Normalize to (dB)</source>
+      <translation>Normaliziraj na (dB)</translation>
+    </message>
+    <message>
+      <location filename="../gain_ui.py" line="132"/>
+      <source>Processing files ...</source>
+      <translation>Procesiram datoteke ...</translation>
+    </message>
+  </context>
+</TS>
