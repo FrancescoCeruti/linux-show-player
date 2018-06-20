a95035cc0bb5f0eccb0134981ed0cd45ce8b4514
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-20 20:02:25 +0200

    New translations triggers.ts (Spanish)

--- lisp/i18n/es_ES/triggers.ts
+++ lisp/i18n/es_ES/triggers.ts
@@ -6,22 +6,22 @@
     <message>
       <location filename="../triggers_handler.py" line="30"/>
       <source>Started</source>
-      <translation type="unfinished">Started</translation>
+      <translation>Iniciado</translation>
     </message>
     <message>
       <location filename="../triggers_handler.py" line="31"/>
       <source>Paused</source>
-      <translation type="unfinished">Paused</translation>
+      <translation>Pausado</translation>
     </message>
     <message>
       <location filename="../triggers_handler.py" line="32"/>
       <source>Stopped</source>
-      <translation type="unfinished">Stopped</translation>
+      <translation>Detenido</translation>
     </message>
     <message>
       <location filename="../triggers_handler.py" line="33"/>
       <source>Ended</source>
-      <translation type="unfinished">Ended</translation>
+      <translation>Terminado</translation>
     </message>
   </context>
   <context>
@@ -29,7 +29,7 @@
     <message>
       <location filename="../triggers_settings.py" line="36"/>
       <source>Triggers</source>
-      <translation type="unfinished">Triggers</translation>
+      <translation>Disparadores</translation>
     </message>
   </context>
   <context>
@@ -37,17 +37,17 @@
     <message>
       <location filename="../triggers_settings.py" line="56"/>
       <source>Add</source>
-      <translation type="unfinished">Add</translation>
+      <translation>A&#xF1;adir</translation>
     </message>
     <message>
       <location filename="../triggers_settings.py" line="60"/>
       <source>Remove</source>
-      <translation type="unfinished">Remove</translation>
+      <translation>Eliminar</translation>
     </message>
     <message>
       <location filename="../triggers_settings.py" line="136"/>
       <source>Trigger</source>
-      <translation type="unfinished">Trigger</translation>
+      <translation>Disparador</translation>
     </message>
     <message>
       <location filename="../triggers_settings.py" line="136"/>
@@ -57,7 +57,7 @@
     <message>
       <location filename="../triggers_settings.py" line="136"/>
       <source>Action</source>
-      <translation type="unfinished">Action</translation>
+      <translation>Acci&#xF3;n</translation>
     </message>
   </context>
 </TS>
