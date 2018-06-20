bea9f1afa2b446c944e2b35cd69a623502f69e8f
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-20 20:21:53 +0200

    New translations timecode.ts (Spanish)

---
+++ lisp/i18n/ts/es_ES/timecode.ts
@@ -0,0 +1,83 @@
+<?xml version="1.0"?>
+<!DOCTYPE TS>
+<TS language="es-ES" version="2.0" sourcelanguage="en">
+  <context>
+    <name>SettingsPageName</name>
+    <message>
+      <location filename="../timecode_settings.py" line="35"/>
+      <source>Timecode Settings</source>
+      <translation>Ajustes de C&#xF3;digo de Tiemo</translation>
+    </message>
+    <message>
+      <location filename="../timecode_settings.py" line="103"/>
+      <source>Timecode</source>
+      <translation>C&#xF3;digo de Tiempo</translation>
+    </message>
+  </context>
+  <context>
+    <name>Timecode</name>
+    <message>
+      <location filename="../timecode.py" line="146"/>
+      <source>Cannot send timecode.</source>
+      <translation>No se puede mandar C&#xF3;digo de Tiempo</translation>
+    </message>
+    <message>
+      <location filename="../timecode.py" line="146"/>
+      <source>OLA has stopped.</source>
+      <translation>OLA se ha detenido</translation>
+    </message>
+  </context>
+  <context>
+    <name>TimecodeSettings</name>
+    <message>
+      <location filename="../timecode_settings.py" line="64"/>
+      <source>OLA Timecode Settings</source>
+      <translation>Ajustes de C&#xF3;digo de Tiempo OLA</translation>
+    </message>
+    <message>
+      <location filename="../timecode_settings.py" line="66"/>
+      <source>Enable Plugin</source>
+      <translation>Activar Plugin</translation>
+    </message>
+    <message>
+      <location filename="../timecode_settings.py" line="67"/>
+      <source>High-Resolution Timecode</source>
+      <translation>C&#xF3;digo de Tiempo de Alta Resoluci&#xF3;n</translation>
+    </message>
+    <message>
+      <location filename="../timecode_settings.py" line="69"/>
+      <source>Timecode Format:</source>
+      <translation>Formato del C&#xF3;digo de Tiemp&#xF2;</translation>
+    </message>
+    <message>
+      <location filename="../timecode_settings.py" line="78"/>
+      <source>OLA status</source>
+      <translation>Estado de OLA</translation>
+    </message>
+    <message>
+      <location filename="../timecode_settings.py" line="78"/>
+      <source>OLA is not running - start the OLA daemon.</source>
+      <translation>OLA no se est&#xE1; ejecutanto - iniciar el daemon OLA</translation>
+    </message>
+    <message>
+      <location filename="../timecode_settings.py" line="145"/>
+      <source>Replace HOURS by a static track number</source>
+      <translation>Reemplazar HORAS por un n&#xFA;mero de track est&#xE1;tico</translation>
+    </message>
+    <message>
+      <location filename="../timecode_settings.py" line="148"/>
+      <source>Enable ArtNet Timecode</source>
+      <translation>Activar C&#xF3;digo de Tiempo ArtNet</translation>
+    </message>
+    <message>
+      <location filename="../timecode_settings.py" line="150"/>
+      <source>Track number</source>
+      <translation>N&#xFA;mero de Track</translation>
+    </message>
+    <message>
+      <location filename="../timecode_settings.py" line="152"/>
+      <source>To send ArtNet Timecode you need to setup a running OLA session!</source>
+      <translation>&#xA1;Para mandar C&#xF3;digo de Tiempo ArtNet necesita configurar una sesi&#xF3;n OLA en ejecuci&#xF3;n!</translation>
+    </message>
+  </context>
+</TS>
