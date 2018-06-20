4ec54d26e36f37659663ac657903c9735c96be06
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-20 20:22:26 +0200

    New translations midi.ts (Czech)

---
+++ lisp/i18n/ts/cs_CZ/midi.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0"?>
+<!DOCTYPE TS>
+<TS language="cs" version="2.0" sourcelanguage="en">
+  <context>
+    <name>MIDISettings</name>
+    <message>
+      <location filename="../midi_settings.py" line="40"/>
+      <source>MIDI default devices</source>
+      <translation>V&#xFD;choz&#xED; za&#x159;&#xED;zen&#xED; MIDI</translation>
+    </message>
+    <message>
+      <location filename="../midi_settings.py" line="45"/>
+      <source>Input</source>
+      <translation>Vstup</translation>
+    </message>
+    <message>
+      <location filename="../midi_settings.py" line="51"/>
+      <source>Output</source>
+      <translation>V&#xFD;stup</translation>
+    </message>
+  </context>
+  <context>
+    <name>SettingsPageName</name>
+    <message>
+      <location filename="../midi_settings.py" line="32"/>
+      <source>MIDI settings</source>
+      <translation>Nastaven&#xED; MIDI</translation>
+    </message>
+  </context>
+</TS>
