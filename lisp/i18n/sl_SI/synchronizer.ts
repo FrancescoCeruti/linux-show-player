d785a6727a4e27c472507fef04c88a08a895479a
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-20 20:01:41 +0200

    New translations synchronizer.ts (Slovenian)

--- lisp/i18n/sl_SI/synchronizer.ts
+++ lisp/i18n/sl_SI/synchronizer.ts
@@ -6,52 +6,52 @@
     <message>
       <location filename="../peers_dialog.py" line="81"/>
       <source>Manage connected peers</source>
-      <translation type="unfinished">Manage connected peers</translation>
+      <translation>Upravljaj povezane sole&#x17E;nike</translation>
     </message>
     <message>
       <location filename="../peers_dialog.py" line="83"/>
       <source>Discover peers</source>
-      <translation type="unfinished">Discover peers</translation>
+      <translation>Odkrij sole&#x17E;nike</translation>
     </message>
     <message>
       <location filename="../peers_dialog.py" line="85"/>
       <source>Manually add a peer</source>
-      <translation type="unfinished">Manually add a peer</translation>
+      <translation>Ro&#x10D;no dodaj sole&#x17E;nik</translation>
     </message>
     <message>
       <location filename="../peers_dialog.py" line="87"/>
       <source>Remove selected peer</source>
-      <translation type="unfinished">Remove selected peer</translation>
+      <translation>Odstrani izbrani sole&#x17E;nik</translation>
     </message>
     <message>
       <location filename="../peers_dialog.py" line="89"/>
       <source>Remove all peers</source>
-      <translation type="unfinished">Remove all peers</translation>
+      <translation>Odstrani vse sole&#x17E;nike</translation>
     </message>
     <message>
       <location filename="../peers_dialog.py" line="93"/>
       <source>Address</source>
-      <translation type="unfinished">Address</translation>
+      <translation>Naslov</translation>
     </message>
     <message>
       <location filename="../peers_dialog.py" line="93"/>
       <source>Peer IP</source>
-      <translation type="unfinished">Peer IP</translation>
+      <translation>IP sole&#x17E;nika</translation>
     </message>
     <message>
       <location filename="../peers_dialog.py" line="105"/>
       <source>Error</source>
-      <translation type="unfinished">Error</translation>
+      <translation>Napaka</translation>
     </message>
     <message>
       <location filename="../peers_dialog.py" line="105"/>
       <source>Already connected</source>
-      <translation type="unfinished">Already connected</translation>
+      <translation>&#x17D;e povezan</translation>
     </message>
     <message>
       <location filename="../peers_dialog.py" line="116"/>
       <source>Cannot add peer</source>
-      <translation type="unfinished">Cannot add peer</translation>
+      <translation>Ne morem dodat sole&#x17E;nika</translation>
     </message>
   </context>
   <context>
@@ -59,27 +59,27 @@
     <message>
       <location filename="../synchronizer.py" line="38"/>
       <source>Synchronization</source>
-      <translation type="unfinished">Synchronization</translation>
+      <translation>Sinhronizacija</translation>
     </message>
     <message>
       <location filename="../synchronizer.py" line="41"/>
       <source>Manage connected peers</source>
-      <translation type="unfinished">Manage connected peers</translation>
+      <translation>Upravljaj povezane sole&#x17E;nike</translation>
     </message>
     <message>
       <location filename="../synchronizer.py" line="46"/>
       <source>Show your IP</source>
-      <translation type="unfinished">Show your IP</translation>
+      <translation>Prika&#x17E;i tvoj IP</translation>
     </message>
     <message>
       <location filename="../synchronizer.py" line="63"/>
       <source>Your IP is:</source>
-      <translation type="unfinished">Your IP is:</translation>
+      <translation>Tvoj IP je:</translation>
     </message>
     <message>
       <location filename="../peers_discovery_dialog.py" line="55"/>
       <source>Discovering peers ...</source>
-      <translation type="unfinished">Discovering peers ...</translation>
+      <translation>Odkrivam sole&#x17E;nike ...</translation>
     </message>
   </context>
 </TS>
