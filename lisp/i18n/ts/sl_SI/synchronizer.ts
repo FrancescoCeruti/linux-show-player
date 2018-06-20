2a66ddb477b3aa80daf629e598aa2a4052b62cf7
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-20 20:22:04 +0200

    New translations synchronizer.ts (Slovenian)

---
+++ lisp/i18n/ts/sl_SI/synchronizer.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0"?>
+<!DOCTYPE TS>
+<TS language="sl" version="2.0" sourcelanguage="en">
+  <context>
+    <name>SyncPeerDialog</name>
+    <message>
+      <location filename="../peers_dialog.py" line="81"/>
+      <source>Manage connected peers</source>
+      <translation>Upravljaj povezane sole&#x17E;nike</translation>
+    </message>
+    <message>
+      <location filename="../peers_dialog.py" line="83"/>
+      <source>Discover peers</source>
+      <translation>Odkrij sole&#x17E;nike</translation>
+    </message>
+    <message>
+      <location filename="../peers_dialog.py" line="85"/>
+      <source>Manually add a peer</source>
+      <translation>Ro&#x10D;no dodaj sole&#x17E;nik</translation>
+    </message>
+    <message>
+      <location filename="../peers_dialog.py" line="87"/>
+      <source>Remove selected peer</source>
+      <translation>Odstrani izbrani sole&#x17E;nik</translation>
+    </message>
+    <message>
+      <location filename="../peers_dialog.py" line="89"/>
+      <source>Remove all peers</source>
+      <translation>Odstrani vse sole&#x17E;nike</translation>
+    </message>
+    <message>
+      <location filename="../peers_dialog.py" line="93"/>
+      <source>Address</source>
+      <translation>Naslov</translation>
+    </message>
+    <message>
+      <location filename="../peers_dialog.py" line="93"/>
+      <source>Peer IP</source>
+      <translation>IP sole&#x17E;nika</translation>
+    </message>
+    <message>
+      <location filename="../peers_dialog.py" line="105"/>
+      <source>Error</source>
+      <translation>Napaka</translation>
+    </message>
+    <message>
+      <location filename="../peers_dialog.py" line="105"/>
+      <source>Already connected</source>
+      <translation>&#x17D;e povezan</translation>
+    </message>
+    <message>
+      <location filename="../peers_dialog.py" line="116"/>
+      <source>Cannot add peer</source>
+      <translation>Ne morem dodat sole&#x17E;nika</translation>
+    </message>
+  </context>
+  <context>
+    <name>Synchronizer</name>
+    <message>
+      <location filename="../synchronizer.py" line="38"/>
+      <source>Synchronization</source>
+      <translation>Sinhronizacija</translation>
+    </message>
+    <message>
+      <location filename="../synchronizer.py" line="41"/>
+      <source>Manage connected peers</source>
+      <translation>Upravljaj povezane sole&#x17E;nike</translation>
+    </message>
+    <message>
+      <location filename="../synchronizer.py" line="46"/>
+      <source>Show your IP</source>
+      <translation>Prika&#x17E;i tvoj IP</translation>
+    </message>
+    <message>
+      <location filename="../synchronizer.py" line="63"/>
+      <source>Your IP is:</source>
+      <translation>Tvoj IP je:</translation>
+    </message>
+    <message>
+      <location filename="../peers_discovery_dialog.py" line="55"/>
+      <source>Discovering peers ...</source>
+      <translation>Odkrivam sole&#x17E;nike ...</translation>
+    </message>
+  </context>
+</TS>
