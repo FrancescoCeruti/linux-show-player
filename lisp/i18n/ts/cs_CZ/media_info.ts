147a61c831a57cd2aedb35ab57f7a8eedce5f670
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-20 20:22:27 +0200

    New translations media_info.ts (Czech)

---
+++ lisp/i18n/ts/cs_CZ/media_info.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0"?>
+<!DOCTYPE TS>
+<TS language="cs" version="2.0" sourcelanguage="en">
+  <context>
+    <name>MediaInfo</name>
+    <message>
+      <location filename="../media_info.py" line="97"/>
+      <source>Media Info</source>
+      <translation>&#xDA;daje o z&#xE1;znamu</translation>
+    </message>
+    <message>
+      <location filename="../media_info.py" line="51"/>
+      <source>Error</source>
+      <translation>Chyba</translation>
+    </message>
+    <message>
+      <location filename="../media_info.py" line="51"/>
+      <source>No info to display</source>
+      <translation>&#x17D;&#xE1;dn&#xE9; &#xFA;daje k zobrazen&#xED;</translation>
+    </message>
+    <message>
+      <location filename="../media_info.py" line="107"/>
+      <source>Info</source>
+      <translation>Informace</translation>
+    </message>
+    <message>
+      <location filename="../media_info.py" line="107"/>
+      <source>Value</source>
+      <translation>Hodnota</translation>
+    </message>
+  </context>
+</TS>
