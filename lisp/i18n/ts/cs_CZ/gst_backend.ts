90775cf719759bf07e992f49c4d1c69ec9afc590
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-20 20:22:29 +0200

    New translations gst_backend.ts (Czech)

---
+++ lisp/i18n/ts/cs_CZ/gst_backend.ts
@@ -0,0 +1,370 @@
+<?xml version="1.0"?>
+<!DOCTYPE TS>
+<TS language="cs" version="2.0" sourcelanguage="en">
+  <context>
+    <name>AlsaSinkSettings</name>
+    <message>
+      <location filename="../settings/alsa_sink.py" line="56"/>
+      <source>ALSA device</source>
+      <translation>Za&#x159;&#xED;zen&#xED; ALSA</translation>
+    </message>
+    <message>
+      <location filename="../settings/alsa_sink.py" line="51"/>
+      <source>ALSA devices, as defined in an asound configuration file</source>
+      <translation>Za&#x159;&#xED;zen&#xED; ALSA, jak jsou stanovena v souboru s nastaven&#xED;m asound</translation>
+    </message>
+  </context>
+  <context>
+    <name>AudioDynamicSettings</name>
+    <message>
+      <location filename="../settings/audio_dynamic.py" line="50"/>
+      <source>Compressor</source>
+      <translation>Stla&#x10D;ova&#x10D;</translation>
+    </message>
+    <message>
+      <location filename="../settings/audio_dynamic.py" line="52"/>
+      <source>Expander</source>
+      <translation>Rozp&#xED;na&#x10D;</translation>
+    </message>
+    <message>
+      <location filename="../settings/audio_dynamic.py" line="62"/>
+      <source>Soft Knee</source>
+      <translation>M&#x11B;kk&#xE9; koleno</translation>
+    </message>
+    <message>
+      <location filename="../settings/audio_dynamic.py" line="64"/>
+      <source>Hard Knee</source>
+      <translation>Tvrd&#xE9; koleno</translation>
+    </message>
+    <message>
+      <location filename="../settings/audio_dynamic.py" line="94"/>
+      <source>Compressor/Expander</source>
+      <translation>Stla&#x10D;ova&#x10D;/Rozp&#xED;na&#x10D;</translation>
+    </message>
+    <message>
+      <location filename="../settings/audio_dynamic.py" line="96"/>
+      <source>Type</source>
+      <translation>Typ</translation>
+    </message>
+    <message>
+      <location filename="../settings/audio_dynamic.py" line="97"/>
+      <source>Curve Shape</source>
+      <translation>Tvar k&#x159;ivky</translation>
+    </message>
+    <message>
+      <location filename="../settings/audio_dynamic.py" line="98"/>
+      <source>Ratio</source>
+      <translation>Pom&#x11B;r</translation>
+    </message>
+    <message>
+      <location filename="../settings/audio_dynamic.py" line="99"/>
+      <source>Threshold (dB)</source>
+      <translation>Pr&#xE1;h (dB)</translation>
+    </message>
+  </context>
+  <context>
+    <name>AudioPanSettings</name>
+    <message>
+      <location filename="../settings/audio_pan.py" line="60"/>
+      <source>Audio Pan</source>
+      <translation>Vyv&#xE1;&#x17E;en&#xED; zvuku</translation>
+    </message>
+    <message>
+      <location filename="../settings/audio_pan.py" line="82"/>
+      <source>Center</source>
+      <translation>Na st&#x159;ed</translation>
+    </message>
+    <message>
+      <location filename="../settings/audio_pan.py" line="78"/>
+      <source>Left</source>
+      <translation>Vlevo</translation>
+    </message>
+    <message>
+      <location filename="../settings/audio_pan.py" line="80"/>
+      <source>Right</source>
+      <translation>Vpravo</translation>
+    </message>
+  </context>
+  <context>
+    <name>DbMeterSettings</name>
+    <message>
+      <location filename="../settings/db_meter.py" line="81"/>
+      <source>DbMeter settings</source>
+      <translation>Nastaven&#xED; m&#x11B;&#x159;idla decibel&#x16F;</translation>
+    </message>
+    <message>
+      <location filename="../settings/db_meter.py" line="82"/>
+      <source>Time between levels (ms)</source>
+      <translation>&#x10C;as mezi hladinami (ms)</translation>
+    </message>
+    <message>
+      <location filename="../settings/db_meter.py" line="84"/>
+      <source>Peak ttl (ms)</source>
+      <translation>Ttl vrcholu (ms)</translation>
+    </message>
+    <message>
+      <location filename="../settings/db_meter.py" line="85"/>
+      <source>Peak falloff (dB/sec)</source>
+      <translation>Opad&#xE1;v&#xE1;n&#xED; vrcholu (dB/s)</translation>
+    </message>
+  </context>
+  <context>
+    <name>Equalizer10Settings</name>
+    <message>
+      <location filename="../settings/equalizer10.py" line="44"/>
+      <source>10 Bands Equalizer (IIR)</source>
+      <translation>Desetip&#xE1;smov&#xFD; ekvaliz&#xE9;r (IIR)</translation>
+    </message>
+  </context>
+  <context>
+    <name>GstMediaSettings</name>
+    <message>
+      <location filename="../gst_media_settings.py" line="47"/>
+      <source>Change Pipeline</source>
+      <translation>Zm&#x11B;nit komunika&#x10D;n&#xED; spojen&#xED;</translation>
+    </message>
+  </context>
+  <context>
+    <name>GstPipelineEdit</name>
+    <message>
+      <location filename="../gst_pipe_edit.py" line="155"/>
+      <source>Edit Pipeline</source>
+      <translation>Upravit komunika&#x10D;n&#xED; spojen&#xED;</translation>
+    </message>
+  </context>
+  <context>
+    <name>GstSettings</name>
+    <message>
+      <location filename="../gst_settings.py" line="46"/>
+      <source>Pipeline</source>
+      <translation>Komunika&#x10D;n&#xED; spojen&#xED;</translation>
+    </message>
+  </context>
+  <context>
+    <name>JackSinkSettings</name>
+    <message>
+      <location filename="../settings/jack_sink.py" line="55"/>
+      <source>Connections</source>
+      <translation>Spojen&#xED;</translation>
+    </message>
+    <message>
+      <location filename="../settings/jack_sink.py" line="56"/>
+      <source>Edit connections</source>
+      <translation>Upravit spojen&#xED;</translation>
+    </message>
+    <message>
+      <location filename="../settings/jack_sink.py" line="242"/>
+      <source>Output ports</source>
+      <translation>V&#xFD;stupn&#xED; p&#x159;&#xED;pojky</translation>
+    </message>
+    <message>
+      <location filename="../settings/jack_sink.py" line="244"/>
+      <source>Input ports</source>
+      <translation>Vstupn&#xED; p&#x159;&#xED;pojky</translation>
+    </message>
+    <message>
+      <location filename="../settings/jack_sink.py" line="300"/>
+      <source>Connect</source>
+      <translation>Spojit</translation>
+    </message>
+    <message>
+      <location filename="../settings/jack_sink.py" line="296"/>
+      <source>Disconnect</source>
+      <translation>Odpojit</translation>
+    </message>
+  </context>
+  <context>
+    <name>MediaElementName</name>
+    <message>
+      <location filename="../elements/audio_dynamic.py" line="32"/>
+      <source>Compressor/Expander</source>
+      <translation>Stla&#x10D;ova&#x10D;/Rozp&#xED;na&#x10D;</translation>
+    </message>
+    <message>
+      <location filename="../elements/audio_pan.py" line="30"/>
+      <source>Audio Pan</source>
+      <translation>Vyv&#xE1;&#x17E;en&#xED; zvuku</translation>
+    </message>
+    <message>
+      <location filename="../elements/pulse_sink.py" line="30"/>
+      <source>PulseAudio Out</source>
+      <translation>V&#xFD;stup PulseAudio</translation>
+    </message>
+    <message>
+      <location filename="../elements/volume.py" line="31"/>
+      <source>Volume</source>
+      <translation>Hlasitost</translation>
+    </message>
+    <message>
+      <location filename="../elements/db_meter.py" line="31"/>
+      <source>dB Meter</source>
+      <translation>M&#x11B;&#x159;idlo decibel&#x16F;</translation>
+    </message>
+    <message>
+      <location filename="../elements/auto_src.py" line="29"/>
+      <source>System Input</source>
+      <translation>Vstup syst&#xE9;mu</translation>
+    </message>
+    <message>
+      <location filename="../elements/alsa_sink.py" line="30"/>
+      <source>ALSA Out</source>
+      <translation>V&#xFD;stup ALSA</translation>
+    </message>
+    <message>
+      <location filename="../elements/jack_sink.py" line="33"/>
+      <source>JACK Out</source>
+      <translation>V&#xFD;stup JACK</translation>
+    </message>
+    <message>
+      <location filename="../elements/user_element.py" line="31"/>
+      <source>Custom Element</source>
+      <translation>Vlastn&#xED; prvek</translation>
+    </message>
+    <message>
+      <location filename="../elements/auto_sink.py" line="30"/>
+      <source>System Out</source>
+      <translation>V&#xFD;stup syst&#xE9;mu</translation>
+    </message>
+    <message>
+      <location filename="../elements/pitch.py" line="30"/>
+      <source>Pitch</source>
+      <translation>V&#xFD;&#x161;ka t&#xF3;nu</translation>
+    </message>
+    <message>
+      <location filename="../elements/uri_input.py" line="36"/>
+      <source>URI Input</source>
+      <translation>Vstup URI</translation>
+    </message>
+    <message>
+      <location filename="../elements/equalizer10.py" line="30"/>
+      <source>10 Bands Equalizer</source>
+      <translation>Desetip&#xE1;smov&#xFD; ekvaliz&#xE9;r</translation>
+    </message>
+    <message>
+      <location filename="../elements/speed.py" line="31"/>
+      <source>Speed</source>
+      <translation>Rychlost</translation>
+    </message>
+    <message>
+      <location filename="../elements/preset_src.py" line="32"/>
+      <source>Preset Input</source>
+      <translation>Vstup p&#x159;ednastaven&#xED;</translation>
+    </message>
+  </context>
+  <context>
+    <name>PitchSettings</name>
+    <message>
+      <location filename="../settings/pitch.py" line="66"/>
+      <source>Pitch</source>
+      <translation>V&#xFD;&#x161;ka t&#xF3;nu</translation>
+    </message>
+    <message>
+      <location filename="../settings/pitch.py" line="84"/>
+      <source>{0:+} semitones</source>
+      <translation>P&#x16F;lt&#xF3;ny {0:+}</translation>
+    </message>
+  </context>
+  <context>
+    <name>PresetSrcSettings</name>
+    <message>
+      <location filename="../settings/preset_src.py" line="38"/>
+      <source>Presets</source>
+      <translation>P&#x159;ednastaven&#xED;</translation>
+    </message>
+  </context>
+  <context>
+    <name>SettingsPageName</name>
+    <message>
+      <location filename="../gst_settings.py" line="29"/>
+      <source>GStreamer settings</source>
+      <translation>Nastaven&#xED; pro GStreamer</translation>
+    </message>
+    <message>
+      <location filename="../gst_media_settings.py" line="33"/>
+      <source>Media Settings</source>
+      <translation>Nastaven&#xED; m&#xE9;di&#xED;</translation>
+    </message>
+  </context>
+  <context>
+    <name>SpeedSettings</name>
+    <message>
+      <location filename="../settings/speed.py" line="64"/>
+      <source>Speed</source>
+      <translation>Rychlost</translation>
+    </message>
+  </context>
+  <context>
+    <name>UriInputSettings</name>
+    <message>
+      <location filename="../settings/uri_input.py" line="72"/>
+      <source>Source</source>
+      <translation>Zdroj</translation>
+    </message>
+    <message>
+      <location filename="../settings/uri_input.py" line="73"/>
+      <source>Find File</source>
+      <translation>Naj&#xED;t soubor</translation>
+    </message>
+    <message>
+      <location filename="../settings/uri_input.py" line="74"/>
+      <source>Buffering</source>
+      <translation>Ukl&#xE1;d&#xE1;n&#xED; do vyrovn&#xE1;vac&#xED; pam&#x11B;ti</translation>
+    </message>
+    <message>
+      <location filename="../settings/uri_input.py" line="75"/>
+      <source>Use Buffering</source>
+      <translation>Pou&#x17E;&#xED;t ukl&#xE1;d&#xE1;n&#xED; do vyrovn&#xE1;vac&#xED; pam&#x11B;ti</translation>
+    </message>
+    <message>
+      <location filename="../settings/uri_input.py" line="77"/>
+      <source>Attempt download on network streams</source>
+      <translation>Pokusit se o sta&#x17E;en&#xED; na s&#xED;&#x165;ov&#xFD;ch proudech</translation>
+    </message>
+    <message>
+      <location filename="../settings/uri_input.py" line="79"/>
+      <source>Buffer size (-1 default value)</source>
+      <translation>Velikost vyrovn&#xE1;vac&#xED; pam&#x11B;ti (-1 v&#xFD;choz&#xED; hodnota)</translation>
+    </message>
+    <message>
+      <location filename="../settings/uri_input.py" line="105"/>
+      <source>Choose file</source>
+      <translation>Vybrat soubor</translation>
+    </message>
+    <message>
+      <location filename="../settings/uri_input.py" line="105"/>
+      <source>All files</source>
+      <translation>V&#x161;echny soubory</translation>
+    </message>
+  </context>
+  <context>
+    <name>UserElementSettings</name>
+    <message>
+      <location filename="../settings/user_element.py" line="54"/>
+      <source>User defined elements</source>
+      <translation>U&#x17E;ivatelem stanoven&#xE9; prvky</translation>
+    </message>
+    <message>
+      <location filename="../settings/user_element.py" line="56"/>
+      <source>Only for advanced user!</source>
+      <translation>Jen pro pokro&#x10D;il&#xE9;ho u&#x17E;ivatele!</translation>
+    </message>
+  </context>
+  <context>
+    <name>VolumeSettings</name>
+    <message>
+      <location filename="../settings/volume.py" line="79"/>
+      <source>Volume</source>
+      <translation>Hlasitost</translation>
+    </message>
+    <message>
+      <location filename="../settings/volume.py" line="81"/>
+      <source>Normalized volume</source>
+      <translation>Normalizovan&#xE1; hlasitost</translation>
+    </message>
+    <message>
+      <location filename="../settings/volume.py" line="84"/>
+      <source>Reset</source>
+      <translation>Nastavit znovu</translation>
+    </message>
+  </context>
+</TS>
