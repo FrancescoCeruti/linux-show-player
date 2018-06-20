7c53402df846b20bdceb94eaa0db8a0806151eaf
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-20 20:21:49 +0200

    New translations triggers.ts (Italian)

---
+++ lisp/i18n/ts/it_IT/triggers.ts
@@ -0,0 +1,63 @@
+<?xml version="1.0"?>
+<!DOCTYPE TS>
+<TS language="it" version="2.0" sourcelanguage="en">
+  <context>
+    <name>CueTriggers</name>
+    <message>
+      <location filename="../triggers_handler.py" line="30"/>
+      <source>Started</source>
+      <translation>Avviata</translation>
+    </message>
+    <message>
+      <location filename="../triggers_handler.py" line="31"/>
+      <source>Paused</source>
+      <translation>Sospesa</translation>
+    </message>
+    <message>
+      <location filename="../triggers_handler.py" line="32"/>
+      <source>Stopped</source>
+      <translation>Fermata</translation>
+    </message>
+    <message>
+      <location filename="../triggers_handler.py" line="33"/>
+      <source>Ended</source>
+      <translation>Finita</translation>
+    </message>
+  </context>
+  <context>
+    <name>SettingsPageName</name>
+    <message>
+      <location filename="../triggers_settings.py" line="36"/>
+      <source>Triggers</source>
+      <translation type="unfinished">Triggers</translation>
+    </message>
+  </context>
+  <context>
+    <name>TriggersSettings</name>
+    <message>
+      <location filename="../triggers_settings.py" line="56"/>
+      <source>Add</source>
+      <translation>Aggiungi</translation>
+    </message>
+    <message>
+      <location filename="../triggers_settings.py" line="60"/>
+      <source>Remove</source>
+      <translation>Rimuovi</translation>
+    </message>
+    <message>
+      <location filename="../triggers_settings.py" line="136"/>
+      <source>Trigger</source>
+      <translation>Evento</translation>
+    </message>
+    <message>
+      <location filename="../triggers_settings.py" line="136"/>
+      <source>Cue</source>
+      <translation type="unfinished">Cue</translation>
+    </message>
+    <message>
+      <location filename="../triggers_settings.py" line="136"/>
+      <source>Action</source>
+      <translation>Azione</translation>
+    </message>
+  </context>
+</TS>
