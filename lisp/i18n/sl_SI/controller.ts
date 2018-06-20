f3a5f27cc2d0f8af5e1c4a626140eccb71b2ab31
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-20 19:45:13 +0200

    New translations controller.ts (Slovenian)

---
+++ lisp/i18n/sl_SI/controller.ts
@@ -0,0 +1,101 @@
+<?xml version="1.0"?>
+<!DOCTYPE TS>
+<TS language="sl" version="2.0" sourcelanguage="en">
+  <context>
+    <name>ControllerKeySettings</name>
+    <message>
+      <location filename="../protocols/keyboard.py" line="58"/>
+      <source>Key</source>
+      <translation type="unfinished">Key</translation>
+    </message>
+    <message>
+      <location filename="../protocols/keyboard.py" line="58"/>
+      <source>Action</source>
+      <translation type="unfinished">Action</translation>
+    </message>
+    <message>
+      <location filename="../protocols/keyboard.py" line="77"/>
+      <source>Shortcuts</source>
+      <translation type="unfinished">Shortcuts</translation>
+    </message>
+  </context>
+  <context>
+    <name>ControllerMidiSettings</name>
+    <message>
+      <location filename="../protocols/midi.py" line="71"/>
+      <source>MIDI</source>
+      <translation type="unfinished">MIDI</translation>
+    </message>
+    <message>
+      <location filename="../protocols/midi.py" line="76"/>
+      <source>Type</source>
+      <translation type="unfinished">Type</translation>
+    </message>
+    <message>
+      <location filename="../protocols/midi.py" line="76"/>
+      <source>Channel</source>
+      <translation type="unfinished">Channel</translation>
+    </message>
+    <message>
+      <location filename="../protocols/midi.py" line="76"/>
+      <source>Note</source>
+      <translation type="unfinished">Note</translation>
+    </message>
+    <message>
+      <location filename="../protocols/midi.py" line="76"/>
+      <source>Action</source>
+      <translation type="unfinished">Action</translation>
+    </message>
+    <message>
+      <location filename="../protocols/midi.py" line="99"/>
+      <source>Filter &quot;note on&quot;</source>
+      <translation type="unfinished">Filter &quot;note on&quot;</translation>
+    </message>
+    <message>
+      <location filename="../protocols/midi.py" line="102"/>
+      <source>Filter &quot;note off&quot;</source>
+      <translation type="unfinished">Filter &quot;note off&quot;</translation>
+    </message>
+    <message>
+      <location filename="../protocols/midi.py" line="114"/>
+      <source>Capture</source>
+      <translation type="unfinished">Capture</translation>
+    </message>
+    <message>
+      <location filename="../protocols/midi.py" line="147"/>
+      <source>Listening MIDI messages ...</source>
+      <translation type="unfinished">Listening MIDI messages ...</translation>
+    </message>
+  </context>
+  <context>
+    <name>ControllerSettings</name>
+    <message>
+      <location filename="../protocols/keyboard.py" line="78"/>
+      <source>Add</source>
+      <translation type="unfinished">Add</translation>
+    </message>
+    <message>
+      <location filename="../protocols/keyboard.py" line="79"/>
+      <source>Remove</source>
+      <translation type="unfinished">Remove</translation>
+    </message>
+  </context>
+  <context>
+    <name>SettingsPageName</name>
+    <message>
+      <location filename="../controller_settings.py" line="29"/>
+      <source>Cue Control</source>
+      <translation type="unfinished">Cue Control</translation>
+    </message>
+    <message>
+      <location filename="../protocols/midi.py" line="63"/>
+      <source>MIDI Controls</source>
+      <translation type="unfinished">MIDI Controls</translation>
+    </message>
+    <message>
+      <location filename="../protocols/keyboard.py" line="47"/>
+      <source>Keyboard Shortcuts</source>
+      <translation type="unfinished">Keyboard Shortcuts</translation>
+    </message>
+  </context>
+</TS>
