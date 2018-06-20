557d513746d7bdc58a9f4661b44a35d8eeb727f2
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-20 20:22:11 +0200

    New translations controller.ts (Czech)

---
+++ lisp/i18n/ts/cs_CZ/controller.ts
@@ -0,0 +1,101 @@
+<?xml version="1.0"?>
+<!DOCTYPE TS>
+<TS language="cs" version="2.0" sourcelanguage="en">
+  <context>
+    <name>ControllerKeySettings</name>
+    <message>
+      <location filename="../protocols/keyboard.py" line="58"/>
+      <source>Key</source>
+      <translation>Kl&#xE1;vesa</translation>
+    </message>
+    <message>
+      <location filename="../protocols/keyboard.py" line="58"/>
+      <source>Action</source>
+      <translation>&#x10C;innost</translation>
+    </message>
+    <message>
+      <location filename="../protocols/keyboard.py" line="77"/>
+      <source>Shortcuts</source>
+      <translation>Kl&#xE1;vesov&#xE9; zkratky</translation>
+    </message>
+  </context>
+  <context>
+    <name>ControllerMidiSettings</name>
+    <message>
+      <location filename="../protocols/midi.py" line="71"/>
+      <source>MIDI</source>
+      <translation type="unfinished">MIDI</translation>
+    </message>
+    <message>
+      <location filename="../protocols/midi.py" line="76"/>
+      <source>Type</source>
+      <translation>Typ</translation>
+    </message>
+    <message>
+      <location filename="../protocols/midi.py" line="76"/>
+      <source>Channel</source>
+      <translation>Kan&#xE1;l</translation>
+    </message>
+    <message>
+      <location filename="../protocols/midi.py" line="76"/>
+      <source>Note</source>
+      <translation>Nota</translation>
+    </message>
+    <message>
+      <location filename="../protocols/midi.py" line="76"/>
+      <source>Action</source>
+      <translation>&#x10C;innost</translation>
+    </message>
+    <message>
+      <location filename="../protocols/midi.py" line="99"/>
+      <source>Filter &quot;note on&quot;</source>
+      <translation>Filtr &quot;nota zapnuta&quot;</translation>
+    </message>
+    <message>
+      <location filename="../protocols/midi.py" line="102"/>
+      <source>Filter &quot;note off&quot;</source>
+      <translation>Filtr (nota vypnuta&quot;</translation>
+    </message>
+    <message>
+      <location filename="../protocols/midi.py" line="114"/>
+      <source>Capture</source>
+      <translation>Zachyt&#xE1;v&#xE1;n&#xED;</translation>
+    </message>
+    <message>
+      <location filename="../protocols/midi.py" line="147"/>
+      <source>Listening MIDI messages ...</source>
+      <translation>Naslouch&#xE1; se zpr&#xE1;v&#xE1;m MIDI...</translation>
+    </message>
+  </context>
+  <context>
+    <name>ControllerSettings</name>
+    <message>
+      <location filename="../protocols/keyboard.py" line="78"/>
+      <source>Add</source>
+      <translation>P&#x159;idat</translation>
+    </message>
+    <message>
+      <location filename="../protocols/keyboard.py" line="79"/>
+      <source>Remove</source>
+      <translation>Odstranit</translation>
+    </message>
+  </context>
+  <context>
+    <name>SettingsPageName</name>
+    <message>
+      <location filename="../controller_settings.py" line="29"/>
+      <source>Cue Control</source>
+      <translation>Ovl&#xE1;d&#xE1;n&#xED; nar&#xE1;&#x17E;ky</translation>
+    </message>
+    <message>
+      <location filename="../protocols/midi.py" line="63"/>
+      <source>MIDI Controls</source>
+      <translation>Ovl&#xE1;d&#xE1;n&#xED; MIDI</translation>
+    </message>
+    <message>
+      <location filename="../protocols/keyboard.py" line="47"/>
+      <source>Keyboard Shortcuts</source>
+      <translation>Kl&#xE1;vesov&#xE9; zkratky</translation>
+    </message>
+  </context>
+</TS>
