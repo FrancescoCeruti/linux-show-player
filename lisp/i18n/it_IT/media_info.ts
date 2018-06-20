a174ced3baee9a0f466798862ec146e9ed3f2d42
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-20 19:44:59 +0200

    New translations media_info.ts (Italian)

---
+++ lisp/i18n/it_IT/media_info.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0"?>
+<!DOCTYPE TS>
+<TS language="it" version="2.0" sourcelanguage="en">
+  <context>
+    <name>MediaInfo</name>
+    <message>
+      <location filename="../media_info.py" line="97"/>
+      <source>Media Info</source>
+      <translation type="unfinished">Media Info</translation>
+    </message>
+    <message>
+      <location filename="../media_info.py" line="51"/>
+      <source>Error</source>
+      <translation type="unfinished">Error</translation>
+    </message>
+    <message>
+      <location filename="../media_info.py" line="51"/>
+      <source>No info to display</source>
+      <translation type="unfinished">No info to display</translation>
+    </message>
+    <message>
+      <location filename="../media_info.py" line="107"/>
+      <source>Info</source>
+      <translation type="unfinished">Info</translation>
+    </message>
+    <message>
+      <location filename="../media_info.py" line="107"/>
+      <source>Value</source>
+      <translation type="unfinished">Value</translation>
+    </message>
+  </context>
+</TS>
