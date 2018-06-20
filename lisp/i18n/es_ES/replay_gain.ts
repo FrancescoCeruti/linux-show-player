814baf271a9efb00c083a217f092a898686622d9
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-20 20:01:52 +0200

    New translations replay_gain.ts (Spanish)

--- lisp/i18n/es_ES/replay_gain.ts
+++ lisp/i18n/es_ES/replay_gain.ts
@@ -6,47 +6,47 @@
     <message>
       <location filename="../gain_ui.py" line="101"/>
       <source>ReplayGain / Normalization</source>
-      <translation type="unfinished">ReplayGain / Normalization</translation>
+      <translation>ReplayGain / Normalizaci&#xF3;n</translation>
     </message>
     <message>
       <location filename="../replay_gain.py" line="163"/>
       <source>Calculate</source>
-      <translation type="unfinished">Calculate</translation>
+      <translation>Calcular</translation>
     </message>
     <message>
       <location filename="../replay_gain.py" line="168"/>
       <source>Reset all</source>
-      <translation type="unfinished">Reset all</translation>
+      <translation>Reestablecer todo</translation>
     </message>
     <message>
       <location filename="../replay_gain.py" line="173"/>
       <source>Reset selected</source>
-      <translation type="unfinished">Reset selected</translation>
+      <translation>Reestablecer seleccionados</translation>
     </message>
     <message>
       <location filename="../gain_ui.py" line="103"/>
       <source>Threads number</source>
-      <translation type="unfinished">Threads number</translation>
+      <translation>N&#xFA;mero de procesos</translation>
     </message>
     <message>
       <location filename="../gain_ui.py" line="104"/>
       <source>Apply only to selected media</source>
-      <translation type="unfinished">Apply only to selected media</translation>
+      <translation>Aplicar solamente a los media seleccionados</translation>
     </message>
     <message>
       <location filename="../gain_ui.py" line="106"/>
       <source>ReplayGain to (dB SPL)</source>
-      <translation type="unfinished">ReplayGain to (dB SPL)</translation>
+      <translation>ReplayGain a (dB SPL)</translation>
     </message>
     <message>
       <location filename="../gain_ui.py" line="108"/>
       <source>Normalize to (dB)</source>
-      <translation type="unfinished">Normalize to (dB)</translation>
+      <translation>Normalizar a (dB)</translation>
     </message>
     <message>
       <location filename="../gain_ui.py" line="132"/>
       <source>Processing files ...</source>
-      <translation type="unfinished">Processing files ...</translation>
+      <translation>Procesando archivos ...</translation>
     </message>
   </context>
 </TS>
