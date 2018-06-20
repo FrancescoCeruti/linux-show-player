86910cfbbe06fc4cdaefb43259efdde0013fc7c7
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-20 20:22:47 +0200

    New translations midi.ts (Dutch, Belgium)

---
+++ lisp/i18n/ts/nl_BE/midi.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0"?>
+<!DOCTYPE TS>
+<TS language="nl-BE" version="2.0" sourcelanguage="en">
+  <context>
+    <name>MIDISettings</name>
+    <message>
+      <location filename="../midi_settings.py" line="40"/>
+      <source>MIDI default devices</source>
+      <translation type="unfinished">MIDI default devices</translation>
+    </message>
+    <message>
+      <location filename="../midi_settings.py" line="45"/>
+      <source>Input</source>
+      <translation type="unfinished">Input</translation>
+    </message>
+    <message>
+      <location filename="../midi_settings.py" line="51"/>
+      <source>Output</source>
+      <translation type="unfinished">Output</translation>
+    </message>
+  </context>
+  <context>
+    <name>SettingsPageName</name>
+    <message>
+      <location filename="../midi_settings.py" line="32"/>
+      <source>MIDI settings</source>
+      <translation type="unfinished">MIDI settings</translation>
+    </message>
+  </context>
+</TS>
