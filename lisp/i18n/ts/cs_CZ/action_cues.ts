0b7bc734c26b9cec286282c64eef2694193f944c
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-20 20:21:31 +0200

    New translations action_cues.ts (Czech)

---
+++ lisp/i18n/ts/cs_CZ/action_cues.ts
@@ -0,0 +1,246 @@
+<?xml version="1.0"?>
+<!DOCTYPE TS>
+<TS language="cs" version="2.0" sourcelanguage="en">
+  <context>
+    <name>CollectionCue</name>
+    <message>
+      <location filename="../collection_cue.py" line="76"/>
+      <source>Add</source>
+      <translation>P&#x159;idat</translation>
+    </message>
+    <message>
+      <location filename="../collection_cue.py" line="80"/>
+      <source>Remove</source>
+      <translation>Odstranit</translation>
+    </message>
+    <message>
+      <location filename="../collection_cue.py" line="143"/>
+      <source>Cue</source>
+      <translation>Nar&#xE1;&#x17E;ka</translation>
+    </message>
+    <message>
+      <location filename="../collection_cue.py" line="143"/>
+      <source>Action</source>
+      <translation>&#x10C;innost</translation>
+    </message>
+  </context>
+  <context>
+    <name>CommandCue</name>
+    <message>
+      <location filename="../command_cue.py" line="79"/>
+      <source>Process ended with an error status.</source>
+      <translation>Postup skon&#x10D;il s chybov&#xFD;m stavem.</translation>
+    </message>
+    <message>
+      <location filename="../command_cue.py" line="79"/>
+      <source>Exit code: </source>
+      <translation>K&#xF3;d ukon&#x10D;en&#xED;: </translation>
+    </message>
+    <message>
+      <location filename="../command_cue.py" line="123"/>
+      <source>Command</source>
+      <translation>P&#x159;&#xED;kaz</translation>
+    </message>
+    <message>
+      <location filename="../command_cue.py" line="124"/>
+      <source>Command to execute, as in a shell</source>
+      <translation>P&#x159;&#xED;kaz k proveden&#xED;, jako v termin&#xE1;lu</translation>
+    </message>
+    <message>
+      <location filename="../command_cue.py" line="126"/>
+      <source>Discard command output</source>
+      <translation>Zahodit v&#xFD;stup p&#x159;&#xED;kazu</translation>
+    </message>
+    <message>
+      <location filename="../command_cue.py" line="128"/>
+      <source>Ignore command errors</source>
+      <translation>P&#x159;ehl&#xED;&#x17E;et chyby p&#x159;&#xED;kazu</translation>
+    </message>
+    <message>
+      <location filename="../command_cue.py" line="130"/>
+      <source>Kill instead of terminate</source>
+      <translation>Zab&#xED;t nam&#xED;sto ukon&#x10D;it</translation>
+    </message>
+  </context>
+  <context>
+    <name>CueName</name>
+    <message>
+      <location filename="../command_cue.py" line="39"/>
+      <source>Command Cue</source>
+      <translation>Nar&#xE1;&#x17E;ka p&#x159;&#xED;kazu</translation>
+    </message>
+    <message>
+      <location filename="../midi_cue.py" line="34"/>
+      <source>MIDI Cue</source>
+      <translation>Nar&#xE1;&#x17E;ka MIDI</translation>
+    </message>
+    <message>
+      <location filename="../volume_control.py" line="43"/>
+      <source>Volume Control</source>
+      <translation>Ovl&#xE1;d&#xE1;n&#xED; hlasitosti</translation>
+    </message>
+    <message>
+      <location filename="../seek_cue.py" line="36"/>
+      <source>Seek Cue</source>
+      <translation>Nar&#xE1;&#x17E;ka prohled&#xE1;v&#xE1;n&#xED;</translation>
+    </message>
+    <message>
+      <location filename="../collection_cue.py" line="36"/>
+      <source>Collection Cue</source>
+      <translation>Nar&#xE1;&#x17E;ka sb&#xED;rky</translation>
+    </message>
+    <message>
+      <location filename="../stop_all.py" line="33"/>
+      <source>Stop-All</source>
+      <translation>Zastavit v&#x161;e</translation>
+    </message>
+    <message>
+      <location filename="../index_action_cue.py" line="33"/>
+      <source>Index Action</source>
+      <translation>&#x10C;innost indexu</translation>
+    </message>
+  </context>
+  <context>
+    <name>IndexActionCue</name>
+    <message>
+      <location filename="../index_action_cue.py" line="94"/>
+      <source>Index</source>
+      <translation type="unfinished">Index</translation>
+    </message>
+    <message>
+      <location filename="../index_action_cue.py" line="95"/>
+      <source>Use a relative index</source>
+      <translation>Pou&#x17E;&#xED;t pom&#x11B;rn&#xFD; index</translation>
+    </message>
+    <message>
+      <location filename="../index_action_cue.py" line="97"/>
+      <source>Target index</source>
+      <translation>C&#xED;lov&#xFD; index</translation>
+    </message>
+    <message>
+      <location filename="../index_action_cue.py" line="99"/>
+      <source>Action</source>
+      <translation>&#x10C;innost</translation>
+    </message>
+  </context>
+  <context>
+    <name>MIDICue</name>
+    <message>
+      <location filename="../midi_cue.py" line="116"/>
+      <source>MIDI Message</source>
+      <translation>Zpr&#xE1;va MIDI</translation>
+    </message>
+    <message>
+      <location filename="../midi_cue.py" line="117"/>
+      <source>Message type</source>
+      <translation>Typ zpr&#xE1;vy</translation>
+    </message>
+  </context>
+  <context>
+    <name>SeekCue</name>
+    <message>
+      <location filename="../seek_cue.py" line="93"/>
+      <source>Cue</source>
+      <translation>Nar&#xE1;&#x17E;ka</translation>
+    </message>
+    <message>
+      <location filename="../seek_cue.py" line="94"/>
+      <source>Click to select</source>
+      <translation>Klepnout pro vybr&#xE1;n&#xED;</translation>
+    </message>
+    <message>
+      <location filename="../seek_cue.py" line="95"/>
+      <source>Not selected</source>
+      <translation>Nevybr&#xE1;no</translation>
+    </message>
+    <message>
+      <location filename="../seek_cue.py" line="96"/>
+      <source>Seek</source>
+      <translation>Hledat</translation>
+    </message>
+    <message>
+      <location filename="../seek_cue.py" line="97"/>
+      <source>Time to reach</source>
+      <translation>&#x10C;as k dosa&#x17E;en&#xED;</translation>
+    </message>
+  </context>
+  <context>
+    <name>SettingsPageName</name>
+    <message>
+      <location filename="../command_cue.py" line="97"/>
+      <source>Command</source>
+      <translation>P&#x159;&#xED;kaz</translation>
+    </message>
+    <message>
+      <location filename="../midi_cue.py" line="54"/>
+      <source>MIDI Settings</source>
+      <translation>Nastaven&#xED; MIDI</translation>
+    </message>
+    <message>
+      <location filename="../volume_control.py" line="122"/>
+      <source>Volume Settings</source>
+      <translation>Nastaven&#xED; hlasitosti</translation>
+    </message>
+    <message>
+      <location filename="../seek_cue.py" line="54"/>
+      <source>Seek Settings</source>
+      <translation>Nastaven&#xED; prohled&#xE1;v&#xE1;n&#xED;</translation>
+    </message>
+    <message>
+      <location filename="../collection_cue.py" line="54"/>
+      <source>Edit Collection</source>
+      <translation>Upravit sb&#xED;rku</translation>
+    </message>
+    <message>
+      <location filename="../index_action_cue.py" line="58"/>
+      <source>Action Settings</source>
+      <translation>Nastaven&#xED; &#x10D;innosti</translation>
+    </message>
+    <message>
+      <location filename="../stop_all.py" line="65"/>
+      <source>Stop Settings</source>
+      <translation>Nastaven&#xED; zastaven&#xED;</translation>
+    </message>
+  </context>
+  <context>
+    <name>StopAll</name>
+    <message>
+      <location filename="../stop_all.py" line="87"/>
+      <source>Stop Action</source>
+      <translation>&#x10C;innost zastaven&#xED;</translation>
+    </message>
+  </context>
+  <context>
+    <name>VolumeControl</name>
+    <message>
+      <location filename="../volume_control.py" line="112"/>
+      <source>Error during cue execution</source>
+      <translation>Chyba b&#x11B;hem prov&#xE1;d&#x11B;n&#xED; nar&#xE1;&#x17E;ky</translation>
+    </message>
+    <message>
+      <location filename="../volume_control.py" line="182"/>
+      <source>Cue</source>
+      <translation>Nar&#xE1;&#x17E;ka</translation>
+    </message>
+    <message>
+      <location filename="../volume_control.py" line="183"/>
+      <source>Click to select</source>
+      <translation>Klepnout pro vybr&#xE1;n&#xED;</translation>
+    </message>
+    <message>
+      <location filename="../volume_control.py" line="184"/>
+      <source>Not selected</source>
+      <translation>Nevybr&#xE1;no</translation>
+    </message>
+    <message>
+      <location filename="../volume_control.py" line="185"/>
+      <source>Volume to reach</source>
+      <translation>Hlasitost k dosa&#x17E;en&#xED;</translation>
+    </message>
+    <message>
+      <location filename="../volume_control.py" line="186"/>
+      <source>Fade</source>
+      <translation>Prol&#xED;nat</translation>
+    </message>
+  </context>
+</TS>
