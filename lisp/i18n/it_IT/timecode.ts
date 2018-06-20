210cf773ed1100152ed88766a5392cbf73fe8b30
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-20 19:51:41 +0200

    New translations timecode.ts (Italian)

--- lisp/i18n/it_IT/timecode.ts
+++ lisp/i18n/it_IT/timecode.ts
@@ -6,7 +6,7 @@
     <message>
       <location filename="../timecode_settings.py" line="35"/>
       <source>Timecode Settings</source>
-      <translation type="unfinished">Timecode Settings</translation>
+      <translation>Impostazioni Timecode</translation>
     </message>
     <message>
       <location filename="../timecode_settings.py" line="103"/>
@@ -19,12 +19,12 @@
     <message>
       <location filename="../timecode.py" line="146"/>
       <source>Cannot send timecode.</source>
-      <translation type="unfinished">Cannot send timecode.</translation>
+      <translation>Impossibile inviare il timecode.</translation>
     </message>
     <message>
       <location filename="../timecode.py" line="146"/>
       <source>OLA has stopped.</source>
-      <translation type="unfinished">OLA has stopped.</translation>
+      <translation>OLA si &#xE8; fermato.</translation>
     </message>
   </context>
   <context>
@@ -32,52 +32,52 @@
     <message>
       <location filename="../timecode_settings.py" line="64"/>
       <source>OLA Timecode Settings</source>
-      <translation type="unfinished">OLA Timecode Settings</translation>
+      <translation>Impostazioni Timecode OLA</translation>
     </message>
     <message>
       <location filename="../timecode_settings.py" line="66"/>
       <source>Enable Plugin</source>
-      <translation type="unfinished">Enable Plugin</translation>
+      <translation>Attiva Plugin</translation>
     </message>
     <message>
       <location filename="../timecode_settings.py" line="67"/>
       <source>High-Resolution Timecode</source>
-      <translation type="unfinished">High-Resolution Timecode</translation>
+      <translation>Timecode ad alta risoluzione.</translation>
     </message>
     <message>
       <location filename="../timecode_settings.py" line="69"/>
       <source>Timecode Format:</source>
-      <translation type="unfinished">Timecode Format:</translation>
+      <translation>Formato Timecode:</translation>
     </message>
     <message>
       <location filename="../timecode_settings.py" line="78"/>
       <source>OLA status</source>
-      <translation type="unfinished">OLA status</translation>
+      <translation>Stato OLA</translation>
     </message>
     <message>
       <location filename="../timecode_settings.py" line="78"/>
       <source>OLA is not running - start the OLA daemon.</source>
-      <translation type="unfinished">OLA is not running - start the OLA daemon.</translation>
+      <translation>OLA non &#xE8; in esecuzione - avvia il demone OLA</translation>
     </message>
     <message>
       <location filename="../timecode_settings.py" line="145"/>
       <source>Replace HOURS by a static track number</source>
-      <translation type="unfinished">Replace HOURS by a static track number</translation>
+      <translation>Sostiture &quot;ORE&quot; con un numero di traccia statico</translation>
     </message>
     <message>
       <location filename="../timecode_settings.py" line="148"/>
       <source>Enable ArtNet Timecode</source>
-      <translation type="unfinished">Enable ArtNet Timecode</translation>
+      <translation>Attiva ArtNet Timecode</translation>
     </message>
     <message>
       <location filename="../timecode_settings.py" line="150"/>
       <source>Track number</source>
-      <translation type="unfinished">Track number</translation>
+      <translation>Numero di traccia</translation>
     </message>
     <message>
       <location filename="../timecode_settings.py" line="152"/>
       <source>To send ArtNet Timecode you need to setup a running OLA session!</source>
-      <translation type="unfinished">To send ArtNet Timecode you need to setup a running OLA session!</translation>
+      <translation>Per utilizzare il Timecode ArtNet devi avviare una sessione OLA!</translation>
     </message>
   </context>
 </TS>
