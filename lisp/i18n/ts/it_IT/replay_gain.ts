726bc937d764eb049b6db9199c25f31810240687
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-21 11:41:36 +0200

    New translations replay_gain.ts (Italian)

--- lisp/i18n/ts/it_IT/replay_gain.ts
+++ lisp/i18n/ts/it_IT/replay_gain.ts
@@ -1,50 +1,52 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="it" version="2.0">
-<context>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it" sourcelanguage="en">
+  <context>
     <name>ReplayGain</name>
     <message>
-        <location filename="../gain_ui.py" line="101"/>
-        <source>ReplayGain / Normalization</source>
-        <translation>ReplayGain / Normalizzazione</translation>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="54"/>
+      <source>ReplayGain / Normalization</source>
+      <translation>ReplayGain / Normalizzazione</translation>
     </message>
     <message>
-        <location filename="../replay_gain.py" line="163"/>
-        <source>Calculate</source>
-        <translation>Calcola</translation>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="60"/>
+      <source>Calculate</source>
+      <translation>Calcola</translation>
     </message>
     <message>
-        <location filename="../replay_gain.py" line="168"/>
-        <source>Reset all</source>
-        <translation>Resetta tutto</translation>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="65"/>
+      <source>Reset all</source>
+      <translation>Resetta tutto</translation>
     </message>
     <message>
-        <location filename="../replay_gain.py" line="173"/>
-        <source>Reset selected</source>
-        <translation>Resetta selezionati</translation>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="70"/>
+      <source>Reset selected</source>
+      <translation>Resetta selezionati</translation>
     </message>
     <message>
-        <location filename="../gain_ui.py" line="103"/>
-        <source>Threads number</source>
-        <translation>Numero di processi</translation>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="103"/>
+      <source>Threads number</source>
+      <translation>Numero di processi</translation>
     </message>
     <message>
-        <location filename="../gain_ui.py" line="104"/>
-        <source>Apply only to selected media</source>
-        <translation>Applica solo alle traccie selezionate</translation>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="104"/>
+      <source>Apply only to selected media</source>
+      <translation>Applica solo alle traccie selezionate</translation>
     </message>
     <message>
-        <location filename="../gain_ui.py" line="106"/>
-        <source>ReplayGain to (dB SPL)</source>
-        <translation>ReplayGain a (dB SPL)</translation>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="106"/>
+      <source>ReplayGain to (dB SPL)</source>
+      <translation>ReplayGain a (dB SPL)</translation>
     </message>
     <message>
-        <location filename="../gain_ui.py" line="108"/>
-        <source>Normalize to (dB)</source>
-        <translation>Normalizza a (dB)</translation>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="108"/>
+      <source>Normalize to (dB)</source>
+      <translation>Normalizza a (dB)</translation>
     </message>
     <message>
-        <location filename="../gain_ui.py" line="132"/>
-        <source>Processing files ...</source>
-        <translation>Processando ...</translation>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="132"/>
+      <source>Processing files ...</source>
+      <translation>Processando ...</translation>
     </message>
-</context>
+  </context>
 </TS>
