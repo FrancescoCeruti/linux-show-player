0876942464ee3aeb0f09c684085ff30b4ab1e363
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-21 11:42:42 +0200

    New translations media_info.ts (Dutch)

---
+++ lisp/i18n/ts/nl_NL/media_info.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nl" sourcelanguage="en">
+  <context>
+    <name>MediaInfo</name>
+    <message>
+      <location filename="../../../plugins/media_info/media_info.py" line="116"/>
+      <source>Media Info</source>
+      <translation type="unfinished">Media Info</translation>
+    </message>
+    <message>
+      <location filename="../media_info.py" line="51"/>
+      <source>Error</source>
+      <translation type="unfinished">Error</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/media_info/media_info.py" line="60"/>
+      <source>No info to display</source>
+      <translation type="unfinished">No info to display</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/media_info/media_info.py" line="125"/>
+      <source>Info</source>
+      <translation type="unfinished">Info</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/media_info/media_info.py" line="125"/>
+      <source>Value</source>
+      <translation type="unfinished">Value</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/media_info/media_info.py" line="60"/>
+      <source>Warning</source>
+      <translation type="unfinished">Warning</translation>
+    </message>
+  </context>
+</TS>
