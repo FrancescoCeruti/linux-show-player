a031f3c5c1c92d60991e03496e09234dad252078
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-21 11:42:37 +0200

    New translations midi.ts (Czech)

--- lisp/i18n/ts/cs_CZ/midi.ts
+++ lisp/i18n/ts/cs_CZ/midi.ts
@@ -1,28 +1,30 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="cs" version="2.0">
-<context>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs" sourcelanguage="en">
+  <context>
     <name>MIDISettings</name>
     <message>
-        <location filename="../midi_settings.py" line="40"/>
-        <source>MIDI default devices</source>
-        <translation>Výchozí zařízení MIDI</translation>
+      <location filename="../../../plugins/midi/midi_settings.py" line="38"/>
+      <source>MIDI default devices</source>
+      <translation>Výchozí zařízení MIDI</translation>
     </message>
     <message>
-        <location filename="../midi_settings.py" line="45"/>
-        <source>Input</source>
-        <translation>Vstup</translation>
+      <location filename="../../../plugins/midi/midi_settings.py" line="43"/>
+      <source>Input</source>
+      <translation>Vstup</translation>
     </message>
     <message>
-        <location filename="../midi_settings.py" line="51"/>
-        <source>Output</source>
-        <translation>Výstup</translation>
+      <location filename="../../../plugins/midi/midi_settings.py" line="49"/>
+      <source>Output</source>
+      <translation>Výstup</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>SettingsPageName</name>
     <message>
-        <location filename="../midi_settings.py" line="32"/>
-        <source>MIDI settings</source>
-        <translation>Nastavení MIDI</translation>
+      <location filename="../../../plugins/midi/midi_settings.py" line="30"/>
+      <source>MIDI settings</source>
+      <translation>Nastavení MIDI</translation>
     </message>
-</context>
+  </context>
 </TS>
