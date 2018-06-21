ada8b31b9987807d86c3f5fbc681c8a5547abadd
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-21 11:41:53 +0200

    New translations triggers.ts (Slovenian)

--- lisp/i18n/ts/sl_SI/triggers.ts
+++ lisp/i18n/ts/sl_SI/triggers.ts
@@ -1,61 +1,63 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="sl_SI" version="2.0">
-<context>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sl" sourcelanguage="en">
+  <context>
     <name>CueTriggers</name>
     <message>
-        <location filename="../triggers_handler.py" line="30"/>
-        <source>Started</source>
-        <translation>Zagnan</translation>
+      <location filename="../../../plugins/triggers/triggers_handler.py" line="29"/>
+      <source>Started</source>
+      <translation>Zagnan</translation>
     </message>
     <message>
-        <location filename="../triggers_handler.py" line="31"/>
-        <source>Paused</source>
-        <translation>V premoru</translation>
+      <location filename="../../../plugins/triggers/triggers_handler.py" line="30"/>
+      <source>Paused</source>
+      <translation>V premoru</translation>
     </message>
     <message>
-        <location filename="../triggers_handler.py" line="32"/>
-        <source>Stopped</source>
-        <translation>Ustavljen</translation>
+      <location filename="../../../plugins/triggers/triggers_handler.py" line="31"/>
+      <source>Stopped</source>
+      <translation>Ustavljen</translation>
     </message>
     <message>
-        <location filename="../triggers_handler.py" line="33"/>
-        <source>Ended</source>
-        <translation>Končan</translation>
+      <location filename="../../../plugins/triggers/triggers_handler.py" line="32"/>
+      <source>Ended</source>
+      <translation>Končan</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>SettingsPageName</name>
     <message>
-        <location filename="../triggers_settings.py" line="36"/>
-        <source>Triggers</source>
-        <translation>Prožilci</translation>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="36"/>
+      <source>Triggers</source>
+      <translation>Prožilci</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>TriggersSettings</name>
     <message>
-        <location filename="../triggers_settings.py" line="56"/>
-        <source>Add</source>
-        <translation>Dodaj</translation>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="56"/>
+      <source>Add</source>
+      <translation>Dodaj</translation>
     </message>
     <message>
-        <location filename="../triggers_settings.py" line="60"/>
-        <source>Remove</source>
-        <translation>Odstrani</translation>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="60"/>
+      <source>Remove</source>
+      <translation>Odstrani</translation>
     </message>
     <message>
-        <location filename="../triggers_settings.py" line="136"/>
-        <source>Trigger</source>
-        <translation>Prožilec</translation>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="136"/>
+      <source>Trigger</source>
+      <translation>Prožilec</translation>
     </message>
     <message>
-        <location filename="../triggers_settings.py" line="136"/>
-        <source>Cue</source>
-        <translation>Vrsta</translation>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="136"/>
+      <source>Cue</source>
+      <translation>Vrsta</translation>
     </message>
     <message>
-        <location filename="../triggers_settings.py" line="136"/>
-        <source>Action</source>
-        <translation>Akcija</translation>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="136"/>
+      <source>Action</source>
+      <translation>Akcija</translation>
     </message>
-</context>
+  </context>
 </TS>
