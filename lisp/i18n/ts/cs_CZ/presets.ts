9f280379b6e6ab77a7d65a6533a5ce177cfedeb5
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-21 11:41:48 +0200

    New translations presets.ts (Czech)

--- lisp/i18n/ts/cs_CZ/presets.ts
+++ lisp/i18n/ts/cs_CZ/presets.ts
@@ -1,138 +1,150 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="cs" version="2.0">
-<context>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs" sourcelanguage="en">
+  <context>
     <name>Preset</name>
     <message>
-        <location filename="../presets_ui.py" line="215"/>
-        <source>Create Cue</source>
-        <translation>Vytvořit narážku</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="216"/>
+      <source>Create Cue</source>
+      <translation>Vytvořit narážku</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="216"/>
-        <source>Load on selected Cues</source>
-        <translation>Nahrát na vybraných narážkách</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="217"/>
+      <source>Load on selected Cues</source>
+      <translation>Nahrát na vybraných narážkách</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>Presets</name>
     <message>
-        <location filename="../presets_ui.py" line="364"/>
-        <source>Presets</source>
-        <translation>Přednastavení</translation>
+      <location filename="../../../plugins/presets/presets.py" line="55"/>
+      <source>Presets</source>
+      <translation>Přednastavení</translation>
     </message>
     <message>
-        <location filename="../presets.py" line="52"/>
-        <source>Load preset</source>
-        <translation>Nahrát přednastavení</translation>
+      <location filename="../presets.py" line="52"/>
+      <source>Load preset</source>
+      <translation>Nahrát přednastavení</translation>
     </message>
     <message>
-        <location filename="../presets.py" line="56"/>
-        <source>Save as preset</source>
-        <translation>Uložit jako přednastavení</translation>
+      <location filename="../../../plugins/presets/presets.py" line="60"/>
+      <source>Save as preset</source>
+      <translation>Uložit jako přednastavení</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="52"/>
-        <source>Cannot scan presets</source>
-        <translation>Nelze prohledat přednastavení</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="48"/>
+      <source>Cannot scan presets</source>
+      <translation>Nelze prohledat přednastavení</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="60"/>
-        <source>Error while deleting preset &quot;{}&quot;</source>
-        <translation>Chyba při mazání přednastavení &quot;{}&quot;</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="56"/>
+      <source>Error while deleting preset &quot;{}&quot;</source>
+      <translation>Chyba při mazání přednastavení &quot;{}&quot;</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="68"/>
-        <source>Cannot load preset &quot;{}&quot;</source>
-        <translation>Nelze nahrát přednastavení &quot;{}&quot;</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="64"/>
+      <source>Cannot load preset &quot;{}&quot;</source>
+      <translation>Nelze nahrát přednastavení &quot;{}&quot;</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="76"/>
-        <source>Cannot save preset &quot;{}&quot;</source>
-        <translation>Nelze uložit přednastavení &quot;{}&quot;</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="72"/>
+      <source>Cannot save preset &quot;{}&quot;</source>
+      <translation>Nelze uložit přednastavení &quot;{}&quot;</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="84"/>
-        <source>Cannot rename preset &quot;{}&quot;</source>
-        <translation>Nelze přejmenovat přednastavení &quot;{}&quot;</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="80"/>
+      <source>Cannot rename preset &quot;{}&quot;</source>
+      <translation>Nelze přejmenovat přednastavení &quot;{}&quot;</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="96"/>
-        <source>Select Preset</source>
-        <translation>Vybrat přednastavení</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="92"/>
+      <source>Select Preset</source>
+      <translation>Vybrat přednastavení</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="109"/>
-        <source>Preset already exists, overwrite?</source>
-        <translation>Přednastavení již existuje. Přepsat?</translation>
+      <location filename="../presets_ui.py" line="109"/>
+      <source>Preset already exists, overwrite?</source>
+      <translation>Přednastavení již existuje. Přepsat?</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="419"/>
-        <source>Preset name</source>
-        <translation>Název přednastavení</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="420"/>
+      <source>Preset name</source>
+      <translation>Název přednastavení</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="211"/>
-        <source>Add</source>
-        <translation>Přidat</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="212"/>
+      <source>Add</source>
+      <translation>Přidat</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="212"/>
-        <source>Rename</source>
-        <translation>Přejmenovat</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="213"/>
+      <source>Rename</source>
+      <translation>Přejmenovat</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="213"/>
-        <source>Edit</source>
-        <translation>Upravit</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="214"/>
+      <source>Edit</source>
+      <translation>Upravit</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="214"/>
-        <source>Remove</source>
-        <translation>Odstranit</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="215"/>
+      <source>Remove</source>
+      <translation>Odstranit</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="217"/>
-        <source>Export selected</source>
-        <translation>Vybráno vyvedení</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="218"/>
+      <source>Export selected</source>
+      <translation>Vybráno vyvedení</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="218"/>
-        <source>Import</source>
-        <translation>Vyvedení</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="219"/>
+      <source>Import</source>
+      <translation>Vyvedení</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="301"/>
-        <source>Warning</source>
-        <translation>Varování</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="302"/>
+      <source>Warning</source>
+      <translation>Varování</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="256"/>
-        <source>The same name is already used!</source>
-        <translation>Stejný název se již používá!</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="257"/>
+      <source>The same name is already used!</source>
+      <translation>Stejný název se již používá!</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="301"/>
-        <source>Cannot create a cue from this preset: {}</source>
-        <translation>Nelze vytvořit narážku z tohoto přednastavení: &quot;{}&quot;</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="302"/>
+      <source>Cannot create a cue from this preset: {}</source>
+      <translation>Nelze vytvořit narážku z tohoto přednastavení: &quot;{}&quot;</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="339"/>
-        <source>Cannot export correctly.</source>
-        <translation>Nelze vyvést správně.</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="340"/>
+      <source>Cannot export correctly.</source>
+      <translation>Nelze vyvést správně.</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="351"/>
-        <source>Some presets already exists, overwrite?</source>
-        <translation>Některá přednastavení již existují. Přepsat?</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="352"/>
+      <source>Some presets already exists, overwrite?</source>
+      <translation>Některá přednastavení již existují. Přepsat?</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="364"/>
-        <source>Cannot import correctly.</source>
-        <translation>Nelze zavést správně.</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="365"/>
+      <source>Cannot import correctly.</source>
+      <translation>Nelze zavést správně.</translation>
     </message>
     <message>
-        <location filename="../presets_ui.py" line="420"/>
-        <source>Cue type</source>
-        <translation>Typ narážky</translation>
+      <location filename="../../../plugins/presets/presets_ui.py" line="421"/>
+      <source>Cue type</source>
+      <translation>Typ narážky</translation>
     </message>
-</context>
+    <message>
+      <location filename="../../../plugins/presets/presets.py" line="60"/>
+      <source>Load on cue</source>
+      <translation type="unfinished">Load on cue</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets.py" line="60"/>
+      <source>Load on selected cues</source>
+      <translation type="unfinished">Load on selected cues</translation>
+    </message>
+  </context>
 </TS>
