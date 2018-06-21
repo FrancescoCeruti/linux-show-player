8300d716c62fd7b16ba60d2d0488443bec6addb9
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-21 11:53:19 +0200

    New translations osc.ts (Dutch)

---
+++ lisp/i18n/ts/nl_NL/osc.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nl" sourcelanguage="en">
+  <context>
+    <name>OscSettings</name>
+    <message>
+      <location filename="../../../plugins/osc/osc_settings.py" line="39"/>
+      <source>OSC Settings</source>
+      <translation type="unfinished">OSC Settings</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/osc/osc_settings.py" line="46"/>
+      <source>Input Port:</source>
+      <translation type="unfinished">Input Port:</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/osc/osc_settings.py" line="55"/>
+      <source>Output Port:</source>
+      <translation type="unfinished">Output Port:</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/osc/osc_settings.py" line="64"/>
+      <source>Hostname:</source>
+      <translation type="unfinished">Hostname:</translation>
+    </message>
+  </context>
+  <context>
+    <name>SettingsPageName</name>
+    <message>
+      <location filename="../../../plugins/osc/osc_settings.py" line="30"/>
+      <source>OSC settings</source>
+      <translation type="unfinished">OSC settings</translation>
+    </message>
+  </context>
+</TS>
