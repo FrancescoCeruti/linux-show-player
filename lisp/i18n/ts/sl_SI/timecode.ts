699a086743ebf258bafd96c9f9a590b135180916
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-21 11:41:52 +0200

    New translations timecode.ts (Slovenian)

--- lisp/i18n/ts/sl_SI/timecode.ts
+++ lisp/i18n/ts/sl_SI/timecode.ts
@@ -1,81 +1,110 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="sl_SI" version="2.0">
-<context>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sl" sourcelanguage="en">
+  <context>
     <name>SettingsPageName</name>
     <message>
-        <location filename="../timecode_settings.py" line="35"/>
-        <source>Timecode Settings</source>
-        <translation>Nastavitve časovnega zapisa</translation>
+      <location filename="../../../plugins/timecode/settings.py" line="99"/>
+      <source>Timecode Settings</source>
+      <translation>Nastavitve časovnega zapisa</translation>
     </message>
     <message>
-        <location filename="../timecode_settings.py" line="103"/>
-        <source>Timecode</source>
-        <translation>Časovni zapis</translation>
+      <location filename="../../../plugins/timecode/settings.py" line="33"/>
+      <source>Timecode</source>
+      <translation>Časovni zapis</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>Timecode</name>
     <message>
-        <location filename="../timecode.py" line="146"/>
-        <source>Cannot send timecode.</source>
-        <translation>Ne morem poslat časovnega zapisa</translation>
+      <location filename="../../../plugins/timecode/protocols/artnet.py" line="70"/>
+      <source>Cannot send timecode.</source>
+      <translation>Ne morem poslat časovnega zapisa</translation>
     </message>
     <message>
-        <location filename="../timecode.py" line="146"/>
-        <source>OLA has stopped.</source>
-        <translation>OLA se je zaustavil</translation>
+      <location filename="../timecode.py" line="146"/>
+      <source>OLA has stopped.</source>
+      <translation>OLA se je zaustavil</translation>
     </message>
-</context>
-<context>
+    <message>
+      <location filename="../../../plugins/timecode/timecode.py" line="103"/>
+      <source>Cannot load timecode protocol: &quot;{}&quot;</source>
+      <translation type="unfinished">Cannot load timecode protocol: &quot;{}&quot;</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/timecode/protocols/artnet.py" line="65"/>
+      <source>Cannot send timecode. 
+OLA has stopped.</source>
+      <translation type="unfinished">Cannot send timecode. 
+OLA has stopped.</translation>
+    </message>
+  </context>
+  <context>
     <name>TimecodeSettings</name>
     <message>
-        <location filename="../timecode_settings.py" line="64"/>
-        <source>OLA Timecode Settings</source>
-        <translation>Nastavitve OLA  časovnega zapisa</translation>
+      <location filename="../timecode_settings.py" line="64"/>
+      <source>OLA Timecode Settings</source>
+      <translation>Nastavitve OLA  časovnega zapisa</translation>
     </message>
     <message>
-        <location filename="../timecode_settings.py" line="66"/>
-        <source>Enable Plugin</source>
-        <translation>Omogoči vtičnik</translation>
+      <location filename="../timecode_settings.py" line="66"/>
+      <source>Enable Plugin</source>
+      <translation>Omogoči vtičnik</translation>
     </message>
     <message>
-        <location filename="../timecode_settings.py" line="67"/>
-        <source>High-Resolution Timecode</source>
-        <translation>Zelo podobni časovni zapis</translation>
+      <location filename="../timecode_settings.py" line="67"/>
+      <source>High-Resolution Timecode</source>
+      <translation>Zelo podobni časovni zapis</translation>
     </message>
     <message>
-        <location filename="../timecode_settings.py" line="69"/>
-        <source>Timecode Format:</source>
-        <translation>Format časovnega zapisa:</translation>
+      <location filename="../../../plugins/timecode/settings.py" line="132"/>
+      <source>Timecode Format:</source>
+      <translation>Format časovnega zapisa:</translation>
     </message>
     <message>
-        <location filename="../timecode_settings.py" line="78"/>
-        <source>OLA status</source>
-        <translation>OLA stanje</translation>
+      <location filename="../timecode_settings.py" line="78"/>
+      <source>OLA status</source>
+      <translation>OLA stanje</translation>
     </message>
     <message>
-        <location filename="../timecode_settings.py" line="78"/>
-        <source>OLA is not running - start the OLA daemon.</source>
-        <translation>OLA ni zagnan - zaženite OLA daemon.</translation>
+      <location filename="../timecode_settings.py" line="78"/>
+      <source>OLA is not running - start the OLA daemon.</source>
+      <translation>OLA ni zagnan - zaženite OLA daemon.</translation>
     </message>
     <message>
-        <location filename="../timecode_settings.py" line="145"/>
-        <source>Replace HOURS by a static track number</source>
-        <translation>Zamenjaj URE s statično številko sledi</translation>
+      <location filename="../../../plugins/timecode/settings.py" line="75"/>
+      <source>Replace HOURS by a static track number</source>
+      <translation>Zamenjaj URE s statično številko sledi</translation>
     </message>
     <message>
-        <location filename="../timecode_settings.py" line="148"/>
-        <source>Enable ArtNet Timecode</source>
-        <translation>Omogoči ArtNet časovni zapis</translation>
+      <location filename="../timecode_settings.py" line="148"/>
+      <source>Enable ArtNet Timecode</source>
+      <translation>Omogoči ArtNet časovni zapis</translation>
     </message>
     <message>
-        <location filename="../timecode_settings.py" line="150"/>
-        <source>Track number</source>
-        <translation>Številka sledi</translation>
+      <location filename="../../../plugins/timecode/settings.py" line="80"/>
+      <source>Track number</source>
+      <translation>Številka sledi</translation>
     </message>
     <message>
-        <location filename="../timecode_settings.py" line="152"/>
-        <source>To send ArtNet Timecode you need to setup a running OLA session!</source>
-        <translation>Za pošiljanje ArtNet časovnega zapisa, rabite nastavit aktivno OLA sejo!</translation>
+      <location filename="../timecode_settings.py" line="152"/>
+      <source>To send ArtNet Timecode you need to setup a running OLA session!</source>
+      <translation>Za pošiljanje ArtNet časovnega zapisa, rabite nastavit aktivno OLA sejo!</translation>
     </message>
-</context>
+    <message>
+      <location filename="../../../plugins/timecode/settings.py" line="78"/>
+      <source>Enable Timecode</source>
+      <translation type="unfinished">Enable Timecode</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/timecode/settings.py" line="130"/>
+      <source>Timecode Settings</source>
+      <translation type="unfinished">Timecode Settings</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/timecode/settings.py" line="134"/>
+      <source>Timecode Protocol:</source>
+      <translation type="unfinished">Timecode Protocol:</translation>
+    </message>
+  </context>
 </TS>
