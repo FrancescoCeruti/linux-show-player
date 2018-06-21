ea002338d2dee3ed6de579ad3e908b9cbde420ea
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-21 11:41:55 +0200

    New translations triggers.ts (French)

--- lisp/i18n/ts/fr_FR/triggers.ts
+++ lisp/i18n/ts/fr_FR/triggers.ts
@@ -1,61 +1,63 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="fr" version="2.0">
-<context>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr" sourcelanguage="en">
+  <context>
     <name>CueTriggers</name>
     <message>
-        <location filename="../triggers_handler.py" line="30"/>
-        <source>Started</source>
-        <translation>Démarré</translation>
+      <location filename="../../../plugins/triggers/triggers_handler.py" line="29"/>
+      <source>Started</source>
+      <translation>Démarré</translation>
     </message>
     <message>
-        <location filename="../triggers_handler.py" line="31"/>
-        <source>Paused</source>
-        <translation>En pause</translation>
+      <location filename="../../../plugins/triggers/triggers_handler.py" line="30"/>
+      <source>Paused</source>
+      <translation>En pause</translation>
     </message>
     <message>
-        <location filename="../triggers_handler.py" line="32"/>
-        <source>Stopped</source>
-        <translation>Arrêté</translation>
+      <location filename="../../../plugins/triggers/triggers_handler.py" line="31"/>
+      <source>Stopped</source>
+      <translation>Arrêté</translation>
     </message>
     <message>
-        <location filename="../triggers_handler.py" line="33"/>
-        <source>Ended</source>
-        <translation>Fini</translation>
+      <location filename="../../../plugins/triggers/triggers_handler.py" line="32"/>
+      <source>Ended</source>
+      <translation>Fini</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>SettingsPageName</name>
     <message>
-        <location filename="../triggers_settings.py" line="36"/>
-        <source>Triggers</source>
-        <translation>Déclencheurs</translation>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="36"/>
+      <source>Triggers</source>
+      <translation>Déclencheurs</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>TriggersSettings</name>
     <message>
-        <location filename="../triggers_settings.py" line="56"/>
-        <source>Add</source>
-        <translation>Ajouter</translation>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="56"/>
+      <source>Add</source>
+      <translation>Ajouter</translation>
     </message>
     <message>
-        <location filename="../triggers_settings.py" line="60"/>
-        <source>Remove</source>
-        <translation>Retirer</translation>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="60"/>
+      <source>Remove</source>
+      <translation>Retirer</translation>
     </message>
     <message>
-        <location filename="../triggers_settings.py" line="136"/>
-        <source>Trigger</source>
-        <translation>Déclencheur</translation>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="136"/>
+      <source>Trigger</source>
+      <translation>Déclencheur</translation>
     </message>
     <message>
-        <location filename="../triggers_settings.py" line="136"/>
-        <source>Cue</source>
-        <translation>Cue</translation>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="136"/>
+      <source>Cue</source>
+      <translation type="unfinished">Cue</translation>
     </message>
     <message>
-        <location filename="../triggers_settings.py" line="136"/>
-        <source>Action</source>
-        <translation>Action</translation>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="136"/>
+      <source>Action</source>
+      <translation type="unfinished">Action</translation>
     </message>
-</context>
+  </context>
 </TS>
