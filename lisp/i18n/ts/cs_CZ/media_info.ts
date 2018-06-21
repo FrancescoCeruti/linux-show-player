05681c478b2e080646724f7b35de6f35bda74f7c
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-21 11:42:44 +0200

    New translations media_info.ts (Czech)

--- lisp/i18n/ts/cs_CZ/media_info.ts
+++ lisp/i18n/ts/cs_CZ/media_info.ts
@@ -1,30 +1,37 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="cs" version="2.0">
-<context>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs" sourcelanguage="en">
+  <context>
     <name>MediaInfo</name>
     <message>
-        <location filename="../media_info.py" line="97"/>
-        <source>Media Info</source>
-        <translation>Údaje o záznamu</translation>
+      <location filename="../../../plugins/media_info/media_info.py" line="116"/>
+      <source>Media Info</source>
+      <translation>Údaje o záznamu</translation>
     </message>
     <message>
-        <location filename="../media_info.py" line="51"/>
-        <source>Error</source>
-        <translation>Chyba</translation>
+      <location filename="../media_info.py" line="51"/>
+      <source>Error</source>
+      <translation>Chyba</translation>
     </message>
     <message>
-        <location filename="../media_info.py" line="51"/>
-        <source>No info to display</source>
-        <translation>Žádné údaje k zobrazení</translation>
+      <location filename="../../../plugins/media_info/media_info.py" line="60"/>
+      <source>No info to display</source>
+      <translation>Žádné údaje k zobrazení</translation>
     </message>
     <message>
-        <location filename="../media_info.py" line="107"/>
-        <source>Info</source>
-        <translation>Informace</translation>
+      <location filename="../../../plugins/media_info/media_info.py" line="125"/>
+      <source>Info</source>
+      <translation>Informace</translation>
     </message>
     <message>
-        <location filename="../media_info.py" line="107"/>
-        <source>Value</source>
-        <translation>Hodnota</translation>
+      <location filename="../../../plugins/media_info/media_info.py" line="125"/>
+      <source>Value</source>
+      <translation>Hodnota</translation>
     </message>
-</context>
+    <message>
+      <location filename="../../../plugins/media_info/media_info.py" line="60"/>
+      <source>Warning</source>
+      <translation type="unfinished">Warning</translation>
+    </message>
+  </context>
 </TS>
