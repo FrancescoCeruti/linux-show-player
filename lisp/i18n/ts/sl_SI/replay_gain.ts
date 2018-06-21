f41df707ce9e1738342740f0cad9a028ce1cd8ca
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-21 11:41:35 +0200

    New translations replay_gain.ts (Slovenian)

--- lisp/i18n/ts/sl_SI/replay_gain.ts
+++ lisp/i18n/ts/sl_SI/replay_gain.ts
@@ -1,50 +1,52 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="sl_SI" version="2.0">
-<context>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sl" sourcelanguage="en">
+  <context>
     <name>ReplayGain</name>
     <message>
-        <location filename="../gain_ui.py" line="101"/>
-        <source>ReplayGain / Normalization</source>
-        <translation>Ojačanje predvajanja / Normalizacija</translation>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="54"/>
+      <source>ReplayGain / Normalization</source>
+      <translation>Ojačanje predvajanja / Normalizacija</translation>
     </message>
     <message>
-        <location filename="../replay_gain.py" line="163"/>
-        <source>Calculate</source>
-        <translation>Izračunaj</translation>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="60"/>
+      <source>Calculate</source>
+      <translation>Izračunaj</translation>
     </message>
     <message>
-        <location filename="../replay_gain.py" line="168"/>
-        <source>Reset all</source>
-        <translation>Ponastavi vse</translation>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="65"/>
+      <source>Reset all</source>
+      <translation>Ponastavi vse</translation>
     </message>
     <message>
-        <location filename="../replay_gain.py" line="173"/>
-        <source>Reset selected</source>
-        <translation>Ponastavi izbrano</translation>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="70"/>
+      <source>Reset selected</source>
+      <translation>Ponastavi izbrano</translation>
     </message>
     <message>
-        <location filename="../gain_ui.py" line="103"/>
-        <source>Threads number</source>
-        <translation>Število niti</translation>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="103"/>
+      <source>Threads number</source>
+      <translation>Število niti</translation>
     </message>
     <message>
-        <location filename="../gain_ui.py" line="104"/>
-        <source>Apply only to selected media</source>
-        <translation>Uveljavi samo na izbranih medijih</translation>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="104"/>
+      <source>Apply only to selected media</source>
+      <translation>Uveljavi samo na izbranih medijih</translation>
     </message>
     <message>
-        <location filename="../gain_ui.py" line="106"/>
-        <source>ReplayGain to (dB SPL)</source>
-        <translation>Ojačaj predvajanje na (dB SPL)</translation>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="106"/>
+      <source>ReplayGain to (dB SPL)</source>
+      <translation>Ojačaj predvajanje na (dB SPL)</translation>
     </message>
     <message>
-        <location filename="../gain_ui.py" line="108"/>
-        <source>Normalize to (dB)</source>
-        <translation>Normaliziraj na (dB)</translation>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="108"/>
+      <source>Normalize to (dB)</source>
+      <translation>Normaliziraj na (dB)</translation>
     </message>
     <message>
-        <location filename="../gain_ui.py" line="132"/>
-        <source>Processing files ...</source>
-        <translation>Procesiram datoteke ...</translation>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="132"/>
+      <source>Processing files ...</source>
+      <translation>Procesiram datoteke ...</translation>
     </message>
-</context>
+  </context>
 </TS>
