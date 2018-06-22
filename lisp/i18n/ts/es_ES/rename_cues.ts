6fe82a13da55c81c33697d96e2df62fd6b255dc5
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2018-06-22 18:11:10 +0200

    New translations rename_cues.ts (Spanish)

--- lisp/i18n/ts/es_ES/rename_cues.ts
+++ lisp/i18n/ts/es_ES/rename_cues.ts
@@ -46,27 +46,27 @@
     <message>
       <location filename="../../../plugins/rename_cues/rename_ui.py" line="81"/>
       <source>Add numbering</source>
-      <translation type="unfinished">Add numbering</translation>
+      <translation>Añadir numeración</translation>
     </message>
     <message>
       <location filename="../../../plugins/rename_cues/rename_ui.py" line="86"/>
       <source>Reset</source>
-      <translation type="unfinished">Reset</translation>
+      <translation>Reestablecer</translation>
     </message>
     <message>
       <location filename="../../../plugins/rename_cues/rename_ui.py" line="95"/>
       <source>Rename all cue. () in regex below usable with $0, $1 ...</source>
-      <translation type="unfinished">Rename all cue. () in regex below usable with $0, $1 ...</translation>
+      <translation>Cambie el nombre todos cue. () en regex abajo con $0, $1...</translation>
     </message>
     <message>
       <location filename="../../../plugins/rename_cues/rename_ui.py" line="101"/>
       <source>Type your regex here: </source>
-      <translation type="unfinished">Type your regex here: </translation>
+      <translation>Escribir tu regex aquí: </translation>
     </message>
     <message>
       <location filename="../../../plugins/rename_cues/rename_ui.py" line="206"/>
       <source>Regex help</source>
-      <translation type="unfinished">Regex help</translation>
+      <translation>Ayuda de regex</translation>
     </message>
     <message>
       <location filename="../../../plugins/rename_cues/rename_ui.py" line="207"/>
@@ -80,16 +80,16 @@ Exemple :
 Only the numbers are between parenthesis and will be usable with $0 in the first line.
 
 For more information about Regexes, consult python documentation</source>
-      <translation type="unfinished">You can use Regexes to rename your cues.
+      <translation>Puede utilizar Regexes para renombar cues.
 
-Insert expressions captured with regexes in the line below with $0 for the first parenthesis, $1 forthe second, etc...
-In the second line, you can use standard Python Regexes to match expressions in the original cues names. Use parenthesis to capture parts of the matched expression.
+Insertar expresiones capturadas con regexes en la línea abajo con $0 para el primer paréntesis, $1 para el segundo, etc....
+En la segunda línea, puede usar Regexes Python estándard para igualar expresiones en los nombres originales de los cues. Use paréntesis para capturar partes de una expresión igualada.
 
-Exemple : 
-^[a-z]([0-9]+) will find a lower case character ([a-z]), followed by one or more number.
-Only the numbers are between parenthesis and will be usable with $0 in the first line.
+Ejemplo: 
+^[a-z]([0-9]+) encontará un carácter en minúsculas, seguido por uno o más números.
+Solo los números están entre paréntesis y serán usados con $0 en la primera línea.
 
-For more information about Regexes, consult python documentation</translation>
+Para mas información acerca de Regexes, consultar la documentación de Python</translation>
     </message>
   </context>
 </TS>
