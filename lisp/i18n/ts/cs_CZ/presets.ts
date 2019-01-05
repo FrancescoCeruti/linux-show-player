1b9f0304929050fe8022632b094259f7621f7b6e
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2019-01-05 12:13:41 +0100

    New translations presets.ts (Czech)

--- lisp/i18n/ts/cs_CZ/presets.ts
+++ lisp/i18n/ts/cs_CZ/presets.ts
@@ -4,12 +4,12 @@
   <context>
     <name>Preset</name>
     <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="216"/>
+      <location filename="../../../plugins/presets/presets_ui.py" line="230"/>
       <source>Create Cue</source>
       <translation>Vytvořit narážku</translation>
     </message>
     <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="217"/>
+      <location filename="../../../plugins/presets/presets_ui.py" line="231"/>
       <source>Load on selected Cues</source>
       <translation>Nahrát na vybraných narážkách</translation>
     </message>
@@ -17,132 +17,112 @@
   <context>
     <name>Presets</name>
     <message>
-      <location filename="../../../plugins/presets/presets.py" line="55"/>
+      <location filename="../../../plugins/presets/presets.py" line="68"/>
       <source>Presets</source>
       <translation>Přednastavení</translation>
     </message>
     <message>
-      <location filename="../presets.py" line="52"/>
-      <source>Load preset</source>
-      <translation>Nahrát přednastavení</translation>
-    </message>
-    <message>
-      <location filename="../../../plugins/presets/presets.py" line="60"/>
+      <location filename="../../../plugins/presets/presets.py" line="73"/>
       <source>Save as preset</source>
       <translation>Uložit jako přednastavení</translation>
     </message>
     <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="48"/>
+      <location filename="../../../plugins/presets/presets_ui.py" line="66"/>
       <source>Cannot scan presets</source>
       <translation>Nelze prohledat přednastavení</translation>
     </message>
     <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="56"/>
+      <location filename="../../../plugins/presets/presets_ui.py" line="72"/>
       <source>Error while deleting preset &quot;{}&quot;</source>
       <translation>Chyba při mazání přednastavení &quot;{}&quot;</translation>
     </message>
     <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="64"/>
+      <location filename="../../../plugins/presets/presets_ui.py" line="80"/>
       <source>Cannot load preset &quot;{}&quot;</source>
       <translation>Nelze nahrát přednastavení &quot;{}&quot;</translation>
     </message>
     <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="72"/>
+      <location filename="../../../plugins/presets/presets_ui.py" line="88"/>
       <source>Cannot save preset &quot;{}&quot;</source>
       <translation>Nelze uložit přednastavení &quot;{}&quot;</translation>
     </message>
     <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="80"/>
+      <location filename="../../../plugins/presets/presets_ui.py" line="96"/>
       <source>Cannot rename preset &quot;{}&quot;</source>
       <translation>Nelze přejmenovat přednastavení &quot;{}&quot;</translation>
     </message>
     <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="92"/>
+      <location filename="../../../plugins/presets/presets_ui.py" line="108"/>
       <source>Select Preset</source>
       <translation>Vybrat přednastavení</translation>
     </message>
     <message>
-      <location filename="../presets_ui.py" line="109"/>
-      <source>Preset already exists, overwrite?</source>
-      <translation>Přednastavení již existuje. Přepsat?</translation>
-    </message>
-    <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="420"/>
+      <location filename="../../../plugins/presets/presets_ui.py" line="443"/>
       <source>Preset name</source>
       <translation>Název přednastavení</translation>
     </message>
     <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="212"/>
+      <location filename="../../../plugins/presets/presets_ui.py" line="226"/>
       <source>Add</source>
       <translation>Přidat</translation>
     </message>
     <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="213"/>
+      <location filename="../../../plugins/presets/presets_ui.py" line="227"/>
       <source>Rename</source>
       <translation>Přejmenovat</translation>
     </message>
     <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="214"/>
+      <location filename="../../../plugins/presets/presets_ui.py" line="228"/>
       <source>Edit</source>
       <translation>Upravit</translation>
     </message>
     <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="215"/>
+      <location filename="../../../plugins/presets/presets_ui.py" line="229"/>
       <source>Remove</source>
       <translation>Odstranit</translation>
     </message>
     <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="218"/>
+      <location filename="../../../plugins/presets/presets_ui.py" line="234"/>
       <source>Export selected</source>
       <translation>Vybráno vyvedení</translation>
     </message>
     <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="219"/>
+      <location filename="../../../plugins/presets/presets_ui.py" line="237"/>
       <source>Import</source>
       <translation>Vyvedení</translation>
     </message>
     <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="302"/>
+      <location filename="../../../plugins/presets/presets_ui.py" line="320"/>
       <source>Warning</source>
       <translation>Varování</translation>
     </message>
     <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="257"/>
+      <location filename="../../../plugins/presets/presets_ui.py" line="275"/>
       <source>The same name is already used!</source>
       <translation>Stejný název se již používá!</translation>
     </message>
     <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="302"/>
-      <source>Cannot create a cue from this preset: {}</source>
-      <translation>Nelze vytvořit narážku z tohoto přednastavení: &quot;{}&quot;</translation>
-    </message>
-    <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="340"/>
+      <location filename="../../../plugins/presets/presets_ui.py" line="358"/>
       <source>Cannot export correctly.</source>
       <translation>Nelze vyvést správně.</translation>
     </message>
     <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="352"/>
-      <source>Some presets already exists, overwrite?</source>
-      <translation>Některá přednastavení již existují. Přepsat?</translation>
-    </message>
-    <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="365"/>
+      <location filename="../../../plugins/presets/presets_ui.py" line="386"/>
       <source>Cannot import correctly.</source>
       <translation>Nelze zavést správně.</translation>
     </message>
     <message>
-      <location filename="../../../plugins/presets/presets_ui.py" line="421"/>
+      <location filename="../../../plugins/presets/presets_ui.py" line="444"/>
       <source>Cue type</source>
       <translation>Typ narážky</translation>
     </message>
     <message>
-      <location filename="../../../plugins/presets/presets.py" line="60"/>
+      <location filename="../../../plugins/presets/presets.py" line="73"/>
       <source>Load on cue</source>
       <translation type="unfinished">Load on cue</translation>
     </message>
     <message>
-      <location filename="../../../plugins/presets/presets.py" line="60"/>
+      <location filename="../../../plugins/presets/presets.py" line="73"/>
       <source>Load on selected cues</source>
       <translation type="unfinished">Load on selected cues</translation>
     </message>
