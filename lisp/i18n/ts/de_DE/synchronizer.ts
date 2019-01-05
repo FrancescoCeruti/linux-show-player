5f8eccf99c8490174db94a556c0ff30023445efa
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2019-01-05 12:43:09 +0100

    New translations synchronizer.ts (German)

--- lisp/i18n/ts/de_DE/synchronizer.ts
+++ lisp/i18n/ts/de_DE/synchronizer.ts
@@ -6,22 +6,22 @@
     <message>
       <location filename="../../../plugins/synchronizer/synchronizer.py" line="43"/>
       <source>Synchronization</source>
-      <translation type="unfinished">Synchronization</translation>
+      <translation>Synchronisation</translation>
     </message>
     <message>
       <location filename="../../../plugins/synchronizer/synchronizer.py" line="46"/>
       <source>Manage connected peers</source>
-      <translation type="unfinished">Manage connected peers</translation>
+      <translation>Bearbeite verbundene Peers </translation>
     </message>
     <message>
       <location filename="../../../plugins/synchronizer/synchronizer.py" line="52"/>
       <source>Show your IP</source>
-      <translation type="unfinished">Show your IP</translation>
+      <translation>Zeige ihre IP</translation>
     </message>
     <message>
       <location filename="../../../plugins/synchronizer/synchronizer.py" line="78"/>
       <source>Your IP is:</source>
-      <translation type="unfinished">Your IP is:</translation>
+      <translation>Ihre IP ist:</translation>
     </message>
   </context>
 </TS>
