d538f2772c6c224edff70c99f53bd2c2b664f820
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2019-01-05 12:12:14 +0100

    New translations triggers.ts (Spanish)

--- lisp/i18n/ts/es_ES/triggers.ts
+++ lisp/i18n/ts/es_ES/triggers.ts
@@ -4,22 +4,22 @@
   <context>
     <name>CueTriggers</name>
     <message>
-      <location filename="../../../plugins/triggers/triggers_handler.py" line="29"/>
+      <location filename="../../../plugins/triggers/triggers_handler.py" line="27"/>
       <source>Started</source>
       <translation>Iniciado</translation>
     </message>
     <message>
-      <location filename="../../../plugins/triggers/triggers_handler.py" line="30"/>
+      <location filename="../../../plugins/triggers/triggers_handler.py" line="28"/>
       <source>Paused</source>
       <translation>Pausado</translation>
     </message>
     <message>
-      <location filename="../../../plugins/triggers/triggers_handler.py" line="31"/>
+      <location filename="../../../plugins/triggers/triggers_handler.py" line="29"/>
       <source>Stopped</source>
       <translation>Detenido</translation>
     </message>
     <message>
-      <location filename="../../../plugins/triggers/triggers_handler.py" line="32"/>
+      <location filename="../../../plugins/triggers/triggers_handler.py" line="30"/>
       <source>Ended</source>
       <translation>Terminado</translation>
     </message>
@@ -27,7 +27,7 @@
   <context>
     <name>SettingsPageName</name>
     <message>
-      <location filename="../../../plugins/triggers/triggers_settings.py" line="36"/>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="42"/>
       <source>Triggers</source>
       <translation>Disparadores</translation>
     </message>
@@ -35,27 +35,27 @@
   <context>
     <name>TriggersSettings</name>
     <message>
-      <location filename="../../../plugins/triggers/triggers_settings.py" line="56"/>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="63"/>
       <source>Add</source>
       <translation>Añadir</translation>
     </message>
     <message>
-      <location filename="../../../plugins/triggers/triggers_settings.py" line="60"/>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="68"/>
       <source>Remove</source>
       <translation>Eliminar</translation>
     </message>
     <message>
-      <location filename="../../../plugins/triggers/triggers_settings.py" line="136"/>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="148"/>
       <source>Trigger</source>
       <translation>Disparador</translation>
     </message>
     <message>
-      <location filename="../../../plugins/triggers/triggers_settings.py" line="136"/>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="148"/>
       <source>Cue</source>
       <translation>Cue</translation>
     </message>
     <message>
-      <location filename="../../../plugins/triggers/triggers_settings.py" line="136"/>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="148"/>
       <source>Action</source>
       <translation>Acción</translation>
     </message>
