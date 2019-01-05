9080bedc29ed436c38521550a5c39ef0c06ff0d6
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2019-01-05 12:42:38 +0100

    New translations synchronizer.ts (Dutch)

--- lisp/i18n/ts/nl_NL/synchronizer.ts
+++ lisp/i18n/ts/nl_NL/synchronizer.ts
@@ -2,84 +2,26 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="nl" sourcelanguage="en">
   <context>
-    <name>SyncPeerDialog</name>
-    <message>
-      <location filename="../peers_dialog.py" line="81"/>
-      <source>Manage connected peers</source>
-      <translation type="unfinished">Manage connected peers</translation>
-    </message>
-    <message>
-      <location filename="../peers_dialog.py" line="83"/>
-      <source>Discover peers</source>
-      <translation type="unfinished">Discover peers</translation>
-    </message>
-    <message>
-      <location filename="../peers_dialog.py" line="85"/>
-      <source>Manually add a peer</source>
-      <translation type="unfinished">Manually add a peer</translation>
-    </message>
-    <message>
-      <location filename="../peers_dialog.py" line="87"/>
-      <source>Remove selected peer</source>
-      <translation type="unfinished">Remove selected peer</translation>
-    </message>
-    <message>
-      <location filename="../peers_dialog.py" line="89"/>
-      <source>Remove all peers</source>
-      <translation type="unfinished">Remove all peers</translation>
-    </message>
-    <message>
-      <location filename="../peers_dialog.py" line="93"/>
-      <source>Address</source>
-      <translation type="unfinished">Address</translation>
-    </message>
-    <message>
-      <location filename="../peers_dialog.py" line="93"/>
-      <source>Peer IP</source>
-      <translation type="unfinished">Peer IP</translation>
-    </message>
-    <message>
-      <location filename="../peers_dialog.py" line="105"/>
-      <source>Error</source>
-      <translation type="unfinished">Error</translation>
-    </message>
-    <message>
-      <location filename="../peers_dialog.py" line="105"/>
-      <source>Already connected</source>
-      <translation type="unfinished">Already connected</translation>
-    </message>
-    <message>
-      <location filename="../peers_dialog.py" line="116"/>
-      <source>Cannot add peer</source>
-      <translation type="unfinished">Cannot add peer</translation>
-    </message>
-  </context>
-  <context>
     <name>Synchronizer</name>
     <message>
-      <location filename="../../../plugins/synchronizer/synchronizer.py" line="45"/>
+      <location filename="../../../plugins/synchronizer/synchronizer.py" line="43"/>
       <source>Synchronization</source>
-      <translation type="unfinished">Synchronization</translation>
+      <translation>Synchronisatie</translation>
     </message>
     <message>
-      <location filename="../../../plugins/synchronizer/synchronizer.py" line="48"/>
+      <location filename="../../../plugins/synchronizer/synchronizer.py" line="46"/>
       <source>Manage connected peers</source>
-      <translation type="unfinished">Manage connected peers</translation>
+      <translation>Verbonden peers beheren</translation>
     </message>
     <message>
-      <location filename="../../../plugins/synchronizer/synchronizer.py" line="55"/>
+      <location filename="../../../plugins/synchronizer/synchronizer.py" line="52"/>
       <source>Show your IP</source>
-      <translation type="unfinished">Show your IP</translation>
+      <translation>Uw IP weergeven</translation>
     </message>
     <message>
       <location filename="../../../plugins/synchronizer/synchronizer.py" line="78"/>
       <source>Your IP is:</source>
-      <translation type="unfinished">Your IP is:</translation>
-    </message>
-    <message>
-      <location filename="../peers_discovery_dialog.py" line="55"/>
-      <source>Discovering peers ...</source>
-      <translation type="unfinished">Discovering peers ...</translation>
+      <translation>Uw IP is:</translation>
     </message>
   </context>
 </TS>
