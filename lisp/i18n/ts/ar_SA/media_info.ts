753719acb9bd0f1f3bc68385e6edfd25326fe69a
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2019-03-13 17:42:49 +0100

    New translations media_info.ts (Arabic)

---
+++ lisp/i18n/ts/ar_SA/media_info.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ar" sourcelanguage="en">
+  <context>
+    <name>MediaInfo</name>
+    <message>
+      <location filename="../../../plugins/media_info/media_info.py" line="129"/>
+      <source>Media Info</source>
+      <translation type="unfinished">Media Info</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/media_info/media_info.py" line="69"/>
+      <source>No info to display</source>
+      <translation type="unfinished">No info to display</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/media_info/media_info.py" line="139"/>
+      <source>Info</source>
+      <translation type="unfinished">Info</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/media_info/media_info.py" line="139"/>
+      <source>Value</source>
+      <translation type="unfinished">Value</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/media_info/media_info.py" line="69"/>
+      <source>Warning</source>
+      <translation type="unfinished">Warning</translation>
+    </message>
+  </context>
+</TS>
