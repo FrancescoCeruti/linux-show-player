395bf3844f76bdce5c4130acafbe6921bf7ac7ad
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2019-03-13 17:43:05 +0100

    New translations network.ts (Arabic)

---
+++ lisp/i18n/ts/ar_SA/network.ts
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ar" sourcelanguage="en">
+  <context>
+    <name>APIServerInfo</name>
+    <message>
+      <location filename="../../../plugins/network/server.py" line="48"/>
+      <source>Stop serving network API</source>
+      <translation type="unfinished">Stop serving network API</translation>
+    </message>
+  </context>
+  <context>
+    <name>ApiServerError</name>
+    <message>
+      <location filename="../../../plugins/network/server.py" line="50"/>
+      <source>Network API server stopped working.</source>
+      <translation type="unfinished">Network API server stopped working.</translation>
+    </message>
+  </context>
+  <context>
+    <name>NetworkApiDebug</name>
+    <message>
+      <location filename="../../../plugins/network/api/__init__.py" line="14"/>
+      <source>New end-point: {}</source>
+      <translation type="unfinished">New end-point: {}</translation>
+    </message>
+  </context>
+  <context>
+    <name>NetworkDiscovery</name>
+    <message>
+      <location filename="../../../plugins/network/discovery_dialogs.py" line="72"/>
+      <source>Host discovery</source>
+      <translation type="unfinished">Host discovery</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/network/discovery_dialogs.py" line="149"/>
+      <source>Manage hosts</source>
+      <translation type="unfinished">Manage hosts</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/network/discovery_dialogs.py" line="150"/>
+      <source>Discover hosts</source>
+      <translation type="unfinished">Discover hosts</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/network/discovery_dialogs.py" line="153"/>
+      <source>Manually add a host</source>
+      <translation type="unfinished">Manually add a host</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/network/discovery_dialogs.py" line="156"/>
+      <source>Remove selected host</source>
+      <translation type="unfinished">Remove selected host</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/network/discovery_dialogs.py" line="159"/>
+      <source>Remove all host</source>
+      <translation type="unfinished">Remove all host</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/network/discovery_dialogs.py" line="164"/>
+      <source>Address</source>
+      <translation type="unfinished">Address</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/network/discovery_dialogs.py" line="164"/>
+      <source>Host IP</source>
+      <translation type="unfinished">Host IP</translation>
+    </message>
+  </context>
+</TS>
