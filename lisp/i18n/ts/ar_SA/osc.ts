08f8c19e289f54e4b032d5e5e2f4afe547489e38
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2019-03-13 17:43:05 +0100

    New translations osc.ts (Arabic)

---
+++ lisp/i18n/ts/ar_SA/osc.ts
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ar" sourcelanguage="en">
+  <context>
+    <name>OscServerDebug</name>
+    <message>
+      <location filename="../../../plugins/osc/osc_server.py" line="119"/>
+      <source>Message from {} -&gt; path: &quot;{}&quot; args: {}</source>
+      <translation type="unfinished">Message from {} -&gt; path: &quot;{}&quot; args: {}</translation>
+    </message>
+  </context>
+  <context>
+    <name>OscServerError</name>
+    <message>
+      <location filename="../../../plugins/osc/osc_server.py" line="99"/>
+      <source>Cannot start OSC sever</source>
+      <translation type="unfinished">Cannot start OSC sever</translation>
+    </message>
+  </context>
+  <context>
+    <name>OscServerInfo</name>
+    <message>
+      <location filename="../../../plugins/osc/osc_server.py" line="93"/>
+      <source>OSC server started at {}</source>
+      <translation type="unfinished">OSC server started at {}</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/osc/osc_server.py" line="111"/>
+      <source>OSC server stopped</source>
+      <translation type="unfinished">OSC server stopped</translation>
+    </message>
+  </context>
+  <context>
+    <name>OscSettings</name>
+    <message>
+      <location filename="../../../plugins/osc/osc_settings.py" line="43"/>
+      <source>OSC Settings</source>
+      <translation type="unfinished">OSC Settings</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/osc/osc_settings.py" line="50"/>
+      <source>Input Port:</source>
+      <translation type="unfinished">Input Port:</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/osc/osc_settings.py" line="59"/>
+      <source>Output Port:</source>
+      <translation type="unfinished">Output Port:</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/osc/osc_settings.py" line="68"/>
+      <source>Hostname:</source>
+      <translation type="unfinished">Hostname:</translation>
+    </message>
+  </context>
+  <context>
+    <name>SettingsPageName</name>
+    <message>
+      <location filename="../../../plugins/osc/osc_settings.py" line="34"/>
+      <source>OSC settings</source>
+      <translation type="unfinished">OSC settings</translation>
+    </message>
+  </context>
+</TS>
