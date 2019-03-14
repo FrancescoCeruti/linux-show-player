315bed0e539810b573f8c89d500a9383820967c5
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2019-03-14 11:46:40 +0100

    New translations synchronizer.ts (Arabic)

--- lisp/i18n/ts/ar_SA/synchronizer.ts
+++ lisp/i18n/ts/ar_SA/synchronizer.ts
@@ -16,12 +16,12 @@
     <message>
       <location filename="../../../plugins/synchronizer/synchronizer.py" line="52"/>
       <source>Show your IP</source>
-      <translation type="unfinished">Show your IP</translation>
+      <translation>إظهار IP الخاص بك</translation>
     </message>
     <message>
       <location filename="../../../plugins/synchronizer/synchronizer.py" line="78"/>
       <source>Your IP is:</source>
-      <translation type="unfinished">Your IP is:</translation>
+      <translation>IP الخاص بك:</translation>
     </message>
   </context>
 </TS>
