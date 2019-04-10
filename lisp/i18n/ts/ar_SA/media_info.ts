7e20c354bc7bcfbe6cd2f84c99e1dbe5a4326ba9
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2019-04-11 00:55:02 +0200

    New translations media_info.ts (Arabic)

--- lisp/i18n/ts/ar_SA/media_info.ts
+++ lisp/i18n/ts/ar_SA/media_info.ts
@@ -4,27 +4,27 @@
   <context>
     <name>MediaInfo</name>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="129"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="130"/>
       <source>Media Info</source>
       <translation type="unfinished">Media Info</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="69"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="70"/>
       <source>No info to display</source>
       <translation>لا توجد معلومات لعرضها</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="139"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="140"/>
       <source>Info</source>
       <translation>معلومات</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="139"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="140"/>
       <source>Value</source>
       <translation>القيمة</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="69"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="70"/>
       <source>Warning</source>
       <translation>تحذير</translation>
     </message>
