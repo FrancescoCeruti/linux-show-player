48033b40823f8c0dce7ab1a692cb916bfb368ba1
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2019-04-11 00:54:05 +0200

    New translations controller.ts (Spanish)

--- lisp/i18n/ts/es_ES/controller.ts
+++ lisp/i18n/ts/es_ES/controller.ts
@@ -78,52 +78,52 @@
   <context>
     <name>ControllerOscSettings</name>
     <message>
-      <location filename="../../../plugins/controller/protocols/osc.py" line="138"/>
+      <location filename="../../../plugins/controller/protocols/osc.py" line="137"/>
       <source>OSC Message</source>
       <translation>Mensaje OSC</translation>
     </message>
     <message>
-      <location filename="../../../plugins/controller/protocols/osc.py" line="189"/>
+      <location filename="../../../plugins/controller/protocols/osc.py" line="188"/>
       <source>OSC</source>
       <translation>OSC</translation>
     </message>
     <message>
-      <location filename="../../../plugins/controller/protocols/osc.py" line="193"/>
+      <location filename="../../../plugins/controller/protocols/osc.py" line="192"/>
       <source>Path</source>
       <translation>Ruta</translation>
     </message>
     <message>
-      <location filename="../../../plugins/controller/protocols/osc.py" line="193"/>
+      <location filename="../../../plugins/controller/protocols/osc.py" line="192"/>
       <source>Types</source>
       <translation>Tipos</translation>
     </message>
     <message>
-      <location filename="../../../plugins/controller/protocols/osc.py" line="193"/>
+      <location filename="../../../plugins/controller/protocols/osc.py" line="192"/>
       <source>Arguments</source>
       <translation>Argumentos</translation>
     </message>
     <message>
-      <location filename="../../../plugins/controller/protocols/osc.py" line="193"/>
+      <location filename="../../../plugins/controller/protocols/osc.py" line="192"/>
       <source>Actions</source>
       <translation>Acciones</translation>
     </message>
     <message>
-      <location filename="../../../plugins/controller/protocols/osc.py" line="222"/>
+      <location filename="../../../plugins/controller/protocols/osc.py" line="221"/>
       <source>OSC Capture</source>
       <translation>Captura OSC</translation>
     </message>
     <message>
-      <location filename="../../../plugins/controller/protocols/osc.py" line="251"/>
+      <location filename="../../../plugins/controller/protocols/osc.py" line="250"/>
       <source>Add</source>
       <translation>Añadir</translation>
     </message>
     <message>
-      <location filename="../../../plugins/controller/protocols/osc.py" line="252"/>
+      <location filename="../../../plugins/controller/protocols/osc.py" line="251"/>
       <source>Remove</source>
       <translation>Eliminar</translation>
     </message>
     <message>
-      <location filename="../../../plugins/controller/protocols/osc.py" line="253"/>
+      <location filename="../../../plugins/controller/protocols/osc.py" line="252"/>
       <source>Capture</source>
       <translation>Capturar</translation>
     </message>
@@ -210,12 +210,12 @@
   <context>
     <name>OscCue</name>
     <message>
-      <location filename="../../../plugins/controller/protocols/osc.py" line="147"/>
+      <location filename="../../../plugins/controller/protocols/osc.py" line="146"/>
       <source>Add</source>
       <translation>Añadir</translation>
     </message>
     <message>
-      <location filename="../../../plugins/controller/protocols/osc.py" line="148"/>
+      <location filename="../../../plugins/controller/protocols/osc.py" line="147"/>
       <source>Remove</source>
       <translation>Eliminar</translation>
     </message>
@@ -238,7 +238,7 @@
       <translation>Atajos de teclado</translation>
     </message>
     <message>
-      <location filename="../../../plugins/controller/protocols/osc.py" line="181"/>
+      <location filename="../../../plugins/controller/protocols/osc.py" line="180"/>
       <source>OSC Controls</source>
       <translation>Controles OSC</translation>
     </message>
