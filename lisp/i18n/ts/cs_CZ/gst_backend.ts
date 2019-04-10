af7e9adddf27d76f4bb1c0cca7c177a06e52bdec
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2019-04-11 00:54:45 +0200

    New translations gst_backend.ts (Czech)

--- lisp/i18n/ts/cs_CZ/gst_backend.ts
+++ lisp/i18n/ts/cs_CZ/gst_backend.ts
@@ -114,12 +114,12 @@
   <context>
     <name>GstBackend</name>
     <message>
-      <location filename="../../../plugins/gst_backend/gst_backend.py" line="73"/>
+      <location filename="../../../plugins/gst_backend/gst_backend.py" line="72"/>
       <source>Audio cue (from file)</source>
       <translation type="unfinished">Audio cue (from file)</translation>
     </message>
     <message>
-      <location filename="../../../plugins/gst_backend/gst_backend.py" line="113"/>
+      <location filename="../../../plugins/gst_backend/gst_backend.py" line="112"/>
       <source>Select media files</source>
       <translation type="unfinished">Select media files</translation>
     </message>
@@ -255,7 +255,7 @@
       <translation>Výška tónu</translation>
     </message>
     <message>
-      <location filename="../../../plugins/gst_backend/elements/uri_input.py" line="54"/>
+      <location filename="../../../plugins/gst_backend/elements/uri_input.py" line="53"/>
       <source>URI Input</source>
       <translation>Vstup URI</translation>
     </message>
