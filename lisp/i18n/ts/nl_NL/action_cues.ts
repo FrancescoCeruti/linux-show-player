9ecf404147211c3c80ec768d35ae48da5c4b95b0
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2019-04-28 22:34:11 +0200

    New translations action_cues.ts (Dutch)

--- lisp/i18n/ts/nl_NL/action_cues.ts
+++ lisp/i18n/ts/nl_NL/action_cues.ts
@@ -4,22 +4,22 @@
   <context>
     <name>CollectionCue</name>
     <message>
-      <location filename="../../../plugins/action_cues/collection_cue.py" line="84"/>
+      <location filename="../../../plugins/action_cues/collection_cue.py" line="88"/>
       <source>Add</source>
       <translation>Toevoegen</translation>
     </message>
     <message>
-      <location filename="../../../plugins/action_cues/collection_cue.py" line="89"/>
+      <location filename="../../../plugins/action_cues/collection_cue.py" line="93"/>
       <source>Remove</source>
       <translation>Verwijderen</translation>
     </message>
     <message>
-      <location filename="../../../plugins/action_cues/collection_cue.py" line="150"/>
+      <location filename="../../../plugins/action_cues/collection_cue.py" line="157"/>
       <source>Cue</source>
       <translation type="unfinished">Cue</translation>
     </message>
     <message>
-      <location filename="../../../plugins/action_cues/collection_cue.py" line="150"/>
+      <location filename="../../../plugins/action_cues/collection_cue.py" line="157"/>
       <source>Action</source>
       <translation>Actie</translation>
     </message>
