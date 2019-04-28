ec4bf376ab836a409f01f8d5e817e8d1414cf3c4
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2019-04-28 22:33:30 +0200

    New translations triggers.ts (French)

--- lisp/i18n/ts/fr_FR/triggers.ts
+++ lisp/i18n/ts/fr_FR/triggers.ts
@@ -35,27 +35,27 @@
   <context>
     <name>TriggersSettings</name>
     <message>
-      <location filename="../../../plugins/triggers/triggers_settings.py" line="63"/>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="67"/>
       <source>Add</source>
       <translation>Ajouter</translation>
     </message>
     <message>
-      <location filename="../../../plugins/triggers/triggers_settings.py" line="68"/>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="72"/>
       <source>Remove</source>
       <translation>Retirer</translation>
     </message>
     <message>
-      <location filename="../../../plugins/triggers/triggers_settings.py" line="148"/>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="152"/>
       <source>Trigger</source>
       <translation>Déclencheur</translation>
     </message>
     <message>
-      <location filename="../../../plugins/triggers/triggers_settings.py" line="148"/>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="152"/>
       <source>Cue</source>
       <translation>Cue</translation>
     </message>
     <message>
-      <location filename="../../../plugins/triggers/triggers_settings.py" line="148"/>
+      <location filename="../../../plugins/triggers/triggers_settings.py" line="152"/>
       <source>Action</source>
       <translation>Action</translation>
     </message>
