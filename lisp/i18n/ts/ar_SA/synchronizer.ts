c21bb1c20487a675d43f17ea42333f568e78cb8a
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2019-05-13 22:25:31 +0200

    New translations synchronizer.ts (Arabic)

--- lisp/i18n/ts/ar_SA/synchronizer.ts
+++ lisp/i18n/ts/ar_SA/synchronizer.ts
@@ -4,22 +4,22 @@
   <context>
     <name>Synchronizer</name>
     <message>
-      <location filename="../../../plugins/synchronizer/synchronizer.py" line="43"/>
+      <location filename="../../../plugins/synchronizer/synchronizer.py" line="44"/>
       <source>Synchronization</source>
       <translation>المزامنة</translation>
     </message>
     <message>
-      <location filename="../../../plugins/synchronizer/synchronizer.py" line="46"/>
+      <location filename="../../../plugins/synchronizer/synchronizer.py" line="47"/>
       <source>Manage connected peers</source>
       <translation>إدارة الأقران متصل</translation>
     </message>
     <message>
-      <location filename="../../../plugins/synchronizer/synchronizer.py" line="52"/>
+      <location filename="../../../plugins/synchronizer/synchronizer.py" line="53"/>
       <source>Show your IP</source>
       <translation>إظهار IP الخاص بك</translation>
     </message>
     <message>
-      <location filename="../../../plugins/synchronizer/synchronizer.py" line="78"/>
+      <location filename="../../../plugins/synchronizer/synchronizer.py" line="79"/>
       <source>Your IP is:</source>
       <translation>IP الخاص بك:</translation>
     </message>
