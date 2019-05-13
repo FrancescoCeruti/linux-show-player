58bdfda1d413210abdd4f0b550a10af5ca75df6a
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2019-05-13 22:24:53 +0200

    New translations media_info.ts (Slovenian)

--- lisp/i18n/ts/sl_SI/media_info.ts
+++ lisp/i18n/ts/sl_SI/media_info.ts
@@ -4,27 +4,27 @@
   <context>
     <name>MediaInfo</name>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="129"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="130"/>
       <source>Media Info</source>
       <translation>Podrobnosti o mediju</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="69"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="70"/>
       <source>No info to display</source>
       <translation>Brez podrobnosti za prikaz</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="139"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="140"/>
       <source>Info</source>
       <translation>Podrobnosti</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="139"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="140"/>
       <source>Value</source>
       <translation>Vrednost</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="69"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="70"/>
       <source>Warning</source>
       <translation type="unfinished">Warning</translation>
     </message>
