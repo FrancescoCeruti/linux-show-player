1e87a2a57baae3ab0c46081111fe48747bdbcf46
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2019-05-13 22:24:16 +0200

    New translations midi.ts (German)

--- lisp/i18n/ts/de_DE/midi.ts
+++ lisp/i18n/ts/de_DE/midi.ts
@@ -2,6 +2,22 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="de" sourcelanguage="en">
   <context>
+    <name>CueCategory</name>
+    <message>
+      <location filename="../../../plugins/midi/midi.py" line="45"/>
+      <source>Integration cues</source>
+      <translation type="unfinished">Integration cues</translation>
+    </message>
+  </context>
+  <context>
+    <name>CueName</name>
+    <message>
+      <location filename="../../../plugins/midi/midi_cue.py" line="36"/>
+      <source>MIDI Cue</source>
+      <translation type="unfinished">MIDI Cue</translation>
+    </message>
+  </context>
+  <context>
     <name>MIDICue</name>
     <message>
       <location filename="../../../plugins/midi/widgets.py" line="88"/>
@@ -130,7 +146,7 @@
     <message>
       <location filename="../../../plugins/midi/midi_settings.py" line="41"/>
       <source>MIDI default devices</source>
-      <translation type="unfinished">MIDI default devices</translation>
+      <translation>MIDI Standardgeräte</translation>
     </message>
     <message>
       <location filename="../../../plugins/midi/midi_settings.py" line="47"/>
@@ -148,7 +164,12 @@
     <message>
       <location filename="../../../plugins/midi/midi_settings.py" line="33"/>
       <source>MIDI settings</source>
-      <translation type="unfinished">MIDI settings</translation>
+      <translation>MIDI Einstellungen</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/midi/midi_cue.py" line="53"/>
+      <source>MIDI Settings</source>
+      <translation type="unfinished">MIDI Settings</translation>
     </message>
   </context>
 </TS>
