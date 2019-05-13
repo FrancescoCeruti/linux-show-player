34da6163e6b82367c588a2cf1b662d3790e9c971
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2019-05-13 22:26:30 +0200

    New translations media_info.ts (Dutch, Belgium)

--- lisp/i18n/ts/nl_BE/media_info.ts
+++ lisp/i18n/ts/nl_BE/media_info.ts
@@ -4,27 +4,27 @@
   <context>
     <name>MediaInfo</name>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="129"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="130"/>
       <source>Media Info</source>
-      <translation type="unfinished">Media Info</translation>
+      <translation>Media info</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="69"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="70"/>
       <source>No info to display</source>
-      <translation type="unfinished">No info to display</translation>
+      <translation>Geen informatie om weer te geven</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="139"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="140"/>
       <source>Info</source>
       <translation type="unfinished">Info</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="139"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="140"/>
       <source>Value</source>
-      <translation type="unfinished">Value</translation>
+      <translation>Waarde</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="69"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="70"/>
       <source>Warning</source>
       <translation type="unfinished">Warning</translation>
     </message>
