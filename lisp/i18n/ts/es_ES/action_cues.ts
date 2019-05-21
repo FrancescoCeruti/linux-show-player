60519c56d601518035fb3a3f08a8d2cc9d7db980
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2019-05-21 21:59:20 +0200

    New translations action_cues.ts (Spanish)

--- lisp/i18n/ts/es_ES/action_cues.ts
+++ lisp/i18n/ts/es_ES/action_cues.ts
@@ -68,11 +68,6 @@
       <translation>Cue de comando</translation>
     </message>
     <message>
-      <location filename="../../../plugins/action_cues/midi_cue.py" line="35"/>
-      <source>MIDI Cue</source>
-      <translation>Cue de MIDI</translation>
-    </message>
-    <message>
       <location filename="../../../plugins/action_cues/volume_control.py" line="54"/>
       <source>Volume Control</source>
       <translation>Control de volumen</translation>
@@ -132,32 +127,6 @@
     </message>
   </context>
   <context>
-    <name>Osc Cue</name>
-    <message>
-      <location filename="../../../plugins/action_cues/osc_cue.py" line="247"/>
-      <source>Fade</source>
-      <translation>Fundido</translation>
-    </message>
-  </context>
-  <context>
-    <name>OscCue</name>
-    <message>
-      <location filename="../../../plugins/action_cues/osc_cue.py" line="298"/>
-      <source>Add</source>
-      <translation>Añadir</translation>
-    </message>
-    <message>
-      <location filename="../../../plugins/action_cues/osc_cue.py" line="299"/>
-      <source>Remove</source>
-      <translation>Eliminar</translation>
-    </message>
-    <message>
-      <location filename="../../../plugins/action_cues/osc_cue.py" line="304"/>
-      <source>Fade</source>
-      <translation>Fundido</translation>
-    </message>
-  </context>
-  <context>
     <name>SeekCue</name>
     <message>
       <location filename="../../../plugins/action_cues/seek_cue.py" line="99"/>
@@ -191,11 +160,6 @@
       <location filename="../../../plugins/action_cues/command_cue.py" line="104"/>
       <source>Command</source>
       <translation>Comando</translation>
-    </message>
-    <message>
-      <location filename="../../../plugins/action_cues/midi_cue.py" line="52"/>
-      <source>MIDI Settings</source>
-      <translation>Ajustes MIDI</translation>
     </message>
     <message>
       <location filename="../../../plugins/action_cues/volume_control.py" line="139"/>
