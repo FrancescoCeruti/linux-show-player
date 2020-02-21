01ef62cdfffd44c39e6055751923e6ee52083b6a
s0600204 <dev@s0600204>
2020-02-21 23:56:32 +0000

    Support loading files without the prerequisite metadata

--- lisp/application.py
+++ lisp/application.py
@@ -219,8 +219,9 @@ class Application(metaclass=Singleton):
             self.session.update_properties(session_dict["session"])
             self.session.session_file = abspath(session_file)
 
-            for plugin_name, plugin_config in session_dict['session']['plugins'].items():
-                self.__session.set_plugin_session_config(plugin_name, plugin_config)
+            if 'plugins' in session_dict['session']:
+                for plugin_name, plugin_config in session_dict['session']['plugins'].items():
+                    self.__session.set_plugin_session_config(plugin_name, plugin_config)
 
             self.session_initialised.emit(self.session)
 
