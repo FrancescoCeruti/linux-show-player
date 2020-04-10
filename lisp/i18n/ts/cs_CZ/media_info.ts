ecd3ed8a76a7d8e3638ae80ab126cf7651248750
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2020-04-10 17:44:23 +0200

    New translations media_info.ts (Czech)

--- lisp/i18n/ts/cs_CZ/media_info.ts
+++ lisp/i18n/ts/cs_CZ/media_info.ts
@@ -4,22 +4,17 @@
   <context>
     <name>MediaInfo</name>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="130"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="125"/>
       <source>Media Info</source>
       <translation>Údaje o záznamu</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="70"/>
-      <source>No info to display</source>
-      <translation>Žádné údaje k zobrazení</translation>
-    </message>
-    <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="140"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="135"/>
       <source>Info</source>
       <translation>Informace</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="140"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="135"/>
       <source>Value</source>
       <translation>Hodnota</translation>
     </message>
@@ -27,6 +22,11 @@
       <location filename="../../../plugins/media_info/media_info.py" line="70"/>
       <source>Warning</source>
       <translation type="unfinished">Warning</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/media_info/media_info.py" line="70"/>
+      <source>Cannot get any information.</source>
+      <translation type="unfinished">Cannot get any information.</translation>
     </message>
   </context>
 </TS>
