6c1d87172455d68a9903679824c9bd3cca825fb6
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2020-04-11 14:12:10 +0200

    New translations cache_manager.ts (Dutch, Belgium)

---
+++ lisp/i18n/ts/nl_BE/cache_manager.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nl-BE" sourcelanguage="en">
+  <context>
+    <name>CacheManager</name>
+    <message>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="135"/>
+      <source>Cache size warning</source>
+      <translation type="unfinished">Cache size warning</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="138"/>
+      <source>Warning threshold in MB (0 = disabled)</source>
+      <translation type="unfinished">Warning threshold in MB (0 = disabled)</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="142"/>
+      <source>Cache cleanup</source>
+      <translation type="unfinished">Cache cleanup</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="143"/>
+      <source>Delete the cache content</source>
+      <translation type="unfinished">Delete the cache content</translation>
+    </message>
+  </context>
+  <context>
+    <name>SettingsPageName</name>
+    <message>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="96"/>
+      <source>Cache Manager</source>
+      <translation type="unfinished">Cache Manager</translation>
+    </message>
+  </context>
+</TS>
