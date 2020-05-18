077246c6cd629597468eb8f16c44c821e94edc16
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2020-05-18 23:43:56 +0200

    New translations media_info.ts (Chinese Simplified)

---
+++ lisp/i18n/ts/zh_CN/media_info.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh-CN" sourcelanguage="en">
+  <context>
+    <name>MediaInfo</name>
+    <message>
+      <location filename="../../../plugins/media_info/media_info.py" line="125"/>
+      <source>Media Info</source>
+      <translation type="unfinished">Media Info</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/media_info/media_info.py" line="135"/>
+      <source>Info</source>
+      <translation type="unfinished">Info</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/media_info/media_info.py" line="135"/>
+      <source>Value</source>
+      <translation type="unfinished">Value</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/media_info/media_info.py" line="70"/>
+      <source>Warning</source>
+      <translation type="unfinished">Warning</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/media_info/media_info.py" line="70"/>
+      <source>Cannot get any information.</source>
+      <translation type="unfinished">Cannot get any information.</translation>
+    </message>
+  </context>
+</TS>
