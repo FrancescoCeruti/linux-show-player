8d7e9511e38737e9e1724bf4c57b0e07f46012ef
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2020-05-18 23:44:03 +0200

    New translations timecode.ts (Chinese Simplified)

---
+++ lisp/i18n/ts/zh_CN/timecode.ts
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh-CN" sourcelanguage="en">
+  <context>
+    <name>SettingsPageName</name>
+    <message>
+      <location filename="../../../plugins/timecode/settings.py" line="108"/>
+      <source>Timecode Settings</source>
+      <translation type="unfinished">Timecode Settings</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/timecode/settings.py" line="38"/>
+      <source>Timecode</source>
+      <translation type="unfinished">Timecode</translation>
+    </message>
+  </context>
+  <context>
+    <name>Timecode</name>
+    <message>
+      <location filename="../../../plugins/timecode/timecode.py" line="97"/>
+      <source>Cannot load timecode protocol: &quot;{}&quot;</source>
+      <translation type="unfinished">Cannot load timecode protocol: &quot;{}&quot;</translation>
+    </message>
+  </context>
+  <context>
+    <name>TimecodeError</name>
+    <message>
+      <location filename="../../../plugins/timecode/protocols/artnet.py" line="72"/>
+      <source>Cannot send timecode.</source>
+      <translation type="unfinished">Cannot send timecode.</translation>
+    </message>
+  </context>
+  <context>
+    <name>TimecodeSettings</name>
+    <message>
+      <location filename="../../../plugins/timecode/settings.py" line="141"/>
+      <source>Timecode Format:</source>
+      <translation type="unfinished">Timecode Format:</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/timecode/settings.py" line="80"/>
+      <source>Replace HOURS by a static track number</source>
+      <translation type="unfinished">Replace HOURS by a static track number</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/timecode/settings.py" line="88"/>
+      <source>Track number</source>
+      <translation type="unfinished">Track number</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/timecode/settings.py" line="85"/>
+      <source>Enable Timecode</source>
+      <translation type="unfinished">Enable Timecode</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/timecode/settings.py" line="138"/>
+      <source>Timecode Settings</source>
+      <translation type="unfinished">Timecode Settings</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/timecode/settings.py" line="144"/>
+      <source>Timecode Protocol:</source>
+      <translation type="unfinished">Timecode Protocol:</translation>
+    </message>
+  </context>
+</TS>
