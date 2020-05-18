85863d6fbbf94455577cb86a39c030693bbbb7b8
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2020-05-18 23:44:01 +0200

    New translations synchronizer.ts (Chinese Simplified)

---
+++ lisp/i18n/ts/zh_CN/synchronizer.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh-CN" sourcelanguage="en">
+  <context>
+    <name>Synchronizer</name>
+    <message>
+      <location filename="../../../plugins/synchronizer/synchronizer.py" line="44"/>
+      <source>Synchronization</source>
+      <translation type="unfinished">Synchronization</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/synchronizer/synchronizer.py" line="47"/>
+      <source>Manage connected peers</source>
+      <translation type="unfinished">Manage connected peers</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/synchronizer/synchronizer.py" line="53"/>
+      <source>Show your IP</source>
+      <translation type="unfinished">Show your IP</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/synchronizer/synchronizer.py" line="79"/>
+      <source>Your IP is: {}</source>
+      <translation type="unfinished">Your IP is: {}</translation>
+    </message>
+  </context>
+</TS>
