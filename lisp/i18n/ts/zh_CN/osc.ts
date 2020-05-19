2d2818fadb10d077a9c1d18fba488bf88a021f10
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2020-05-19 21:43:00 +0200

    New translations osc.ts (Chinese Simplified)

--- lisp/i18n/ts/zh_CN/osc.ts
+++ lisp/i18n/ts/zh_CN/osc.ts
@@ -6,7 +6,7 @@
     <message>
       <location filename="../../../plugins/osc/osc_cue.py" line="181"/>
       <source>OSC Settings</source>
-      <translation type="unfinished">OSC Settings</translation>
+      <translation>OSC设定</translation>
     </message>
   </context>
   <context>
@@ -14,7 +14,7 @@
     <message>
       <location filename="../../../plugins/osc/osc.py" line="45"/>
       <source>Integration cues</source>
-      <translation type="unfinished">Integration cues</translation>
+      <translation>互联cue</translation>
     </message>
   </context>
   <context>
@@ -22,7 +22,7 @@
     <message>
       <location filename="../../../plugins/osc/osc_cue.py" line="64"/>
       <source>OSC Cue</source>
-      <translation type="unfinished">OSC Cue</translation>
+      <translation>OSC Cue</translation>
     </message>
   </context>
   <context>
@@ -30,57 +30,57 @@
     <message>
       <location filename="../../../plugins/osc/osc_cue.py" line="198"/>
       <source>Type</source>
-      <translation type="unfinished">Type</translation>
+      <translation>类型</translation>
     </message>
     <message>
       <location filename="../../../plugins/osc/osc_cue.py" line="198"/>
       <source>Value</source>
-      <translation type="unfinished">Value</translation>
+      <translation>值</translation>
     </message>
     <message>
       <location filename="../../../plugins/osc/osc_cue.py" line="198"/>
       <source>FadeTo</source>
-      <translation type="unfinished">FadeTo</translation>
+      <translation>淡入/淡出至</translation>
     </message>
     <message>
       <location filename="../../../plugins/osc/osc_cue.py" line="260"/>
       <source>Fade</source>
-      <translation type="unfinished">Fade</translation>
+      <translation>淡入淡出</translation>
     </message>
     <message>
       <location filename="../../../plugins/osc/osc_cue.py" line="253"/>
       <source>OSC Message</source>
-      <translation type="unfinished">OSC Message</translation>
+      <translation>OSC 讯息</translation>
     </message>
     <message>
       <location filename="../../../plugins/osc/osc_cue.py" line="254"/>
       <source>Add</source>
-      <translation type="unfinished">Add</translation>
+      <translation>添加</translation>
     </message>
     <message>
       <location filename="../../../plugins/osc/osc_cue.py" line="255"/>
       <source>Remove</source>
-      <translation type="unfinished">Remove</translation>
+      <translation>移除</translation>
     </message>
     <message>
       <location filename="../../../plugins/osc/osc_cue.py" line="256"/>
       <source>OSC Path:</source>
-      <translation type="unfinished">OSC Path:</translation>
+      <translation>OSC 路径：</translation>
     </message>
     <message>
       <location filename="../../../plugins/osc/osc_cue.py" line="257"/>
       <source>/path/to/something</source>
-      <translation type="unfinished">/path/to/something</translation>
+      <translation>路径</translation>
     </message>
     <message>
       <location filename="../../../plugins/osc/osc_cue.py" line="261"/>
       <source>Time (sec)</source>
-      <translation type="unfinished">Time (sec)</translation>
+      <translation>时间 (秒)</translation>
     </message>
     <message>
       <location filename="../../../plugins/osc/osc_cue.py" line="262"/>
       <source>Curve</source>
-      <translation type="unfinished">Curve</translation>
+      <translation>曲线</translation>
     </message>
   </context>
   <context>
@@ -88,7 +88,7 @@
     <message>
       <location filename="../../../plugins/osc/osc_server.py" line="119"/>
       <source>Message from {} -&gt; path: &quot;{}&quot; args: {}</source>
-      <translation type="unfinished">Message from {} -&gt; path: &quot;{}&quot; args: {}</translation>
+      <translation>来自 {} 的消息 -&gt; 路径: &quot;{}&quot; args: {}</translation>
     </message>
   </context>
   <context>
@@ -96,7 +96,7 @@
     <message>
       <location filename="../../../plugins/osc/osc_server.py" line="99"/>
       <source>Cannot start OSC sever</source>
-      <translation type="unfinished">Cannot start OSC sever</translation>
+      <translation>无法启动OSC伺服器</translation>
     </message>
   </context>
   <context>
