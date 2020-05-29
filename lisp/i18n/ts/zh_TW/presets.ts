7a6589778bf79609ad4663f0c002161dcb4524c4
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2020-05-29 20:04:17 +0200

    New translations presets.ts (Chinese Traditional)

---
+++ lisp/i18n/ts/zh_TW/presets.ts
@@ -0,0 +1,135 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh-TW" sourcelanguage="en">
+  <context>
+    <name>Preset</name>
+    <message>
+      <location filename="../../../plugins/presets/presets_ui.py" line="222"/>
+      <source>Create Cue</source>
+      <translation type="unfinished">Create Cue</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets_ui.py" line="223"/>
+      <source>Load on selected Cues</source>
+      <translation type="unfinished">Load on selected Cues</translation>
+    </message>
+  </context>
+  <context>
+    <name>Presets</name>
+    <message>
+      <location filename="../../../plugins/presets/presets.py" line="67"/>
+      <source>Presets</source>
+      <translation type="unfinished">Presets</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets.py" line="72"/>
+      <source>Save as preset</source>
+      <translation type="unfinished">Save as preset</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets_ui.py" line="67"/>
+      <source>Cannot scan presets</source>
+      <translation type="unfinished">Cannot scan presets</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets_ui.py" line="71"/>
+      <source>Error while deleting preset &quot;{}&quot;</source>
+      <translation type="unfinished">Error while deleting preset &quot;{}&quot;</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets_ui.py" line="78"/>
+      <source>Cannot load preset &quot;{}&quot;</source>
+      <translation type="unfinished">Cannot load preset &quot;{}&quot;</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets_ui.py" line="84"/>
+      <source>Cannot save preset &quot;{}&quot;</source>
+      <translation type="unfinished">Cannot save preset &quot;{}&quot;</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets_ui.py" line="90"/>
+      <source>Cannot rename preset &quot;{}&quot;</source>
+      <translation type="unfinished">Cannot rename preset &quot;{}&quot;</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets_ui.py" line="101"/>
+      <source>Select Preset</source>
+      <translation type="unfinished">Select Preset</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets_ui.py" line="422"/>
+      <source>Preset name</source>
+      <translation type="unfinished">Preset name</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets_ui.py" line="218"/>
+      <source>Add</source>
+      <translation type="unfinished">Add</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets_ui.py" line="219"/>
+      <source>Rename</source>
+      <translation type="unfinished">Rename</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets_ui.py" line="220"/>
+      <source>Edit</source>
+      <translation type="unfinished">Edit</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets_ui.py" line="221"/>
+      <source>Remove</source>
+      <translation type="unfinished">Remove</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets_ui.py" line="226"/>
+      <source>Export selected</source>
+      <translation type="unfinished">Export selected</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets_ui.py" line="229"/>
+      <source>Import</source>
+      <translation type="unfinished">Import</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets_ui.py" line="308"/>
+      <source>Warning</source>
+      <translation type="unfinished">Warning</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets_ui.py" line="267"/>
+      <source>The same name is already used!</source>
+      <translation type="unfinished">The same name is already used!</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets_ui.py" line="343"/>
+      <source>Cannot export correctly.</source>
+      <translation type="unfinished">Cannot export correctly.</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets_ui.py" line="368"/>
+      <source>Cannot import correctly.</source>
+      <translation type="unfinished">Cannot import correctly.</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets_ui.py" line="423"/>
+      <source>Cue type</source>
+      <translation type="unfinished">Cue type</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets.py" line="72"/>
+      <source>Load on cue</source>
+      <translation type="unfinished">Load on cue</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets.py" line="72"/>
+      <source>Load on selected cues</source>
+      <translation type="unfinished">Load on selected cues</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/presets/presets_ui.py" line="308"/>
+      <source>Cannot create a cue from this preset: {}</source>
+      <translation type="unfinished">Cannot create a cue from this preset: {}</translation>
+    </message>
+  </context>
+</TS>
