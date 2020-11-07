d89f558f4e430105e1c51d09b8a735be9dd889e4
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2020-11-07 14:25:19 +0100

    New translations synchronizer.ts (Dutch)

--- lisp/i18n/ts/nl_NL/synchronizer.ts
+++ lisp/i18n/ts/nl_NL/synchronizer.ts
@@ -9,12 +9,12 @@
       <translation>Synchronisatie</translation>
     </message>
     <message>
-      <location filename="../../../plugins/synchronizer/synchronizer.py" line="47"/>
+      <location filename="../../../plugins/synchronizer/synchronizer.py" line="48"/>
       <source>Manage connected peers</source>
       <translation>Beheer verbonden peers</translation>
     </message>
     <message>
-      <location filename="../../../plugins/synchronizer/synchronizer.py" line="53"/>
+      <location filename="../../../plugins/synchronizer/synchronizer.py" line="54"/>
       <source>Show your IP</source>
       <translation>Toon je IP</translation>
     </message>
