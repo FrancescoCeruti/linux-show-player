4c1358305dcac62c66f313ad6f74dfb96c78de29
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2020-11-07 14:25:17 +0100

    New translations replay_gain.ts (Dutch)

--- lisp/i18n/ts/nl_NL/replay_gain.ts
+++ lisp/i18n/ts/nl_NL/replay_gain.ts
@@ -4,7 +4,7 @@
   <context>
     <name>ReplayGain</name>
     <message>
-      <location filename="../../../plugins/replay_gain/gain_ui.py" line="110"/>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="111"/>
       <source>ReplayGain / Normalization</source>
       <translation>ReplayGain / Normalisatie</translation>
     </message>
@@ -19,7 +19,7 @@
       <translation>Alles herstellen</translation>
     </message>
     <message>
-      <location filename="../../../plugins/replay_gain/replay_gain.py" line="65"/>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="66"/>
       <source>Reset selected</source>
       <translation>Geselecteerde herstellen</translation>
     </message>
@@ -29,17 +29,17 @@
       <translation>Aantal processen</translation>
     </message>
     <message>
-      <location filename="../../../plugins/replay_gain/gain_ui.py" line="114"/>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="115"/>
       <source>Apply only to selected media</source>
       <translation>Alleen toepassen op geselecteerde media</translation>
     </message>
     <message>
-      <location filename="../../../plugins/replay_gain/gain_ui.py" line="117"/>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="118"/>
       <source>ReplayGain to (dB SPL)</source>
       <translation>ReplayGain naar (dB SPL)</translation>
     </message>
     <message>
-      <location filename="../../../plugins/replay_gain/gain_ui.py" line="120"/>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="121"/>
       <source>Normalize to (dB)</source>
       <translation>Normaliseren naar (dB)</translation>
     </message>
@@ -52,30 +52,43 @@
   <context>
     <name>ReplayGainDebug</name>
     <message>
-      <location filename="../../../plugins/replay_gain/replay_gain.py" line="236"/>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="237"/>
       <source>Applied gain for: {}</source>
       <translation type="unfinished">Applied gain for: {}</translation>
     </message>
     <message>
-      <location filename="../../../plugins/replay_gain/replay_gain.py" line="242"/>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="243"/>
       <source>Discarded gain for: {}</source>
       <translation type="unfinished">Discarded gain for: {}</translation>
     </message>
   </context>
   <context>
+    <name>ReplayGainError</name>
+    <message>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="331"/>
+      <source>An error occurred during gain calculation.</source>
+      <translation type="unfinished">An error occurred during gain calculation.</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="203"/>
+      <source>An error occurred while processing gain results.</source>
+      <translation type="unfinished">An error occurred while processing gain results.</translation>
+    </message>
+  </context>
+  <context>
     <name>ReplayGainInfo</name>
     <message>
-      <location filename="../../../plugins/replay_gain/replay_gain.py" line="216"/>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="217"/>
       <source>Gain processing stopped by user.</source>
       <translation type="unfinished">Gain processing stopped by user.</translation>
     </message>
     <message>
-      <location filename="../../../plugins/replay_gain/replay_gain.py" line="281"/>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="282"/>
       <source>Started gain calculation for: {}</source>
       <translation type="unfinished">Started gain calculation for: {}</translation>
     </message>
     <message>
-      <location filename="../../../plugins/replay_gain/replay_gain.py" line="316"/>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="317"/>
       <source>Gain calculated for: {}</source>
       <translation type="unfinished">Gain calculated for: {}</translation>
     </message>
