ab66e5afb11c9ea5124cb6bcfc3e901ba8b2c9a2
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2020-11-07 14:26:14 +0100

    New translations timecode.ts (Chinese Simplified)

--- lisp/i18n/ts/zh_CN/timecode.ts
+++ lisp/i18n/ts/zh_CN/timecode.ts
@@ -4,28 +4,35 @@
   <context>
     <name>SettingsPageName</name>
     <message>
-      <location filename="../../../plugins/timecode/settings.py" line="108"/>
-      <source>Timecode Settings</source>
-      <translation>时间码设定</translation>
-    </message>
-    <message>
       <location filename="../../../plugins/timecode/settings.py" line="38"/>
       <source>Timecode</source>
       <translation>时间码</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/timecode/settings.py" line="114"/>
+      <source>Timecode Settings</source>
+      <translation>时间码设定</translation>
     </message>
   </context>
   <context>
     <name>Timecode</name>
     <message>
-      <location filename="../../../plugins/timecode/timecode.py" line="97"/>
-      <source>Cannot load timecode protocol: &quot;{}&quot;</source>
-      <translation>无法加载时间码协议： &quot;{}&quot;</translation>
+      <location filename="../../../plugins/timecode/timecode.py" line="98"/>
+      <source>Cannot load timecode protocol: "{}"</source>
+      <translation type="unfinished">Cannot load timecode protocol: "{}"</translation>
     </message>
   </context>
   <context>
     <name>TimecodeError</name>
     <message>
-      <location filename="../../../plugins/timecode/protocols/artnet.py" line="72"/>
+      <location filename="../../../plugins/timecode/protocols/artnet.py" line="65"/>
+      <source>Cannot send timecode. 
+OLA daemon has stopped.</source>
+      <translation type="unfinished">Cannot send timecode. 
+OLA daemon has stopped.</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/timecode/protocols/artnet.py" line="73"/>
       <source>Cannot send timecode.</source>
       <translation>无法发送时间码</translation>
     </message>
@@ -33,14 +40,14 @@
   <context>
     <name>TimecodeSettings</name>
     <message>
-      <location filename="../../../plugins/timecode/settings.py" line="141"/>
-      <source>Timecode Format:</source>
-      <translation>时间码格式：</translation>
-    </message>
-    <message>
-      <location filename="../../../plugins/timecode/settings.py" line="80"/>
+      <location filename="../../../plugins/timecode/settings.py" line="81"/>
       <source>Replace HOURS by a static track number</source>
       <translation>以静态曲目编号代替小时</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/timecode/settings.py" line="86"/>
+      <source>Enable Timecode</source>
+      <translation>启用时间码</translation>
     </message>
     <message>
       <location filename="../../../plugins/timecode/settings.py" line="88"/>
@@ -48,19 +55,27 @@
       <translation>曲目编号</translation>
     </message>
     <message>
-      <location filename="../../../plugins/timecode/settings.py" line="85"/>
-      <source>Enable Timecode</source>
-      <translation>启用时间码</translation>
-    </message>
-    <message>
-      <location filename="../../../plugins/timecode/settings.py" line="138"/>
+      <location filename="../../../plugins/timecode/settings.py" line="145"/>
       <source>Timecode Settings</source>
       <translation>时间码设定</translation>
     </message>
     <message>
-      <location filename="../../../plugins/timecode/settings.py" line="144"/>
+      <location filename="../../../plugins/timecode/settings.py" line="148"/>
+      <source>Timecode Format:</source>
+      <translation>时间码格式：</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/timecode/settings.py" line="151"/>
       <source>Timecode Protocol:</source>
       <translation>时间码协议：</translation>
+    </message>
+  </context>
+  <context>
+    <name>TimecodeWarning</name>
+    <message>
+      <location filename="../../../plugins/timecode/cue_tracker.py" line="119"/>
+      <source>Cannot send timecode, untracking cue</source>
+      <translation type="unfinished">Cannot send timecode, untracking cue</translation>
     </message>
   </context>
 </TS>
