5e3738a513f2626ccb1e7db4da5b950acf577943
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2020-11-07 14:28:20 +0100

    New translations rename_cues.ts (German)

--- lisp/i18n/ts/de_DE/rename_cues.ts
+++ lisp/i18n/ts/de_DE/rename_cues.ts
@@ -4,7 +4,7 @@
   <context>
     <name>RenameCues</name>
     <message>
-      <location filename="../../../plugins/rename_cues/rename_cues.py" line="36"/>
+      <location filename="../../../plugins/rename_cues/rename_cues.py" line="37"/>
       <source>Rename Cues</source>
       <translation>Cues umbenennen</translation>
     </message>
@@ -12,16 +12,6 @@
       <location filename="../../../plugins/rename_cues/rename_ui.py" line="46"/>
       <source>Rename cues</source>
       <translation>Cues umbenennen</translation>
-    </message>
-    <message>
-      <location filename="../../../plugins/rename_cues/rename_ui.py" line="55"/>
-      <source>Current</source>
-      <translation>Aktuell</translation>
-    </message>
-    <message>
-      <location filename="../../../plugins/rename_cues/rename_ui.py" line="55"/>
-      <source>Preview</source>
-      <translation>Vorschau</translation>
     </message>
     <message>
       <location filename="../../../plugins/rename_cues/rename_ui.py" line="72"/>
@@ -44,7 +34,7 @@
       <translation>Nummern entfernen</translation>
     </message>
     <message>
-      <location filename="../../../plugins/rename_cues/rename_ui.py" line="92"/>
+      <location filename="../../../plugins/rename_cues/rename_ui.py" line="93"/>
       <source>Add numbering</source>
       <translation>Nummerierung hinzufügen</translation>
     </message>
@@ -54,7 +44,12 @@
       <translation>Zurücksetzen</translation>
     </message>
     <message>
-      <location filename="../../../plugins/rename_cues/rename_ui.py" line="120"/>
+      <location filename="../../../plugins/rename_cues/rename_ui.py" line="111"/>
+      <source>Rename all cue. () in regex below usable with $0, $1 ...</source>
+      <translation type="unfinished">Rename all cue. () in regex below usable with $0, $1 ...</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/rename_cues/rename_ui.py" line="121"/>
       <source>Type your regex here: </source>
       <translation>Geben Sie hier Ihr Regex ein: </translation>
     </message>
@@ -62,6 +57,39 @@
       <location filename="../../../plugins/rename_cues/rename_ui.py" line="232"/>
       <source>Regex help</source>
       <translation>Regex-Hilfe</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/rename_cues/rename_ui.py" line="234"/>
+      <source>You can use Regexes to rename your cues.
+
+Insert expressions captured with regexes in the line below with $0 for the first parenthesis, $1 forthe second, etc...
+In the second line, you can use standard Python Regexes to match expressions in the original cues names. Use parenthesis to capture parts of the matched expression.
+
+Exemple: 
+^[a-z]([0-9]+) will find a lower case character([a-z]), followed by one or more number.
+Only the numbers are between parenthesis and will be usable with $0 in the first line.
+
+For more information about Regexes, consult python documentation at: https://docs.python.org/3/howto/regex.html#regex-howto</source>
+      <translation type="unfinished">You can use Regexes to rename your cues.
+
+Insert expressions captured with regexes in the line below with $0 for the first parenthesis, $1 forthe second, etc...
+In the second line, you can use standard Python Regexes to match expressions in the original cues names. Use parenthesis to capture parts of the matched expression.
+
+Exemple: 
+^[a-z]([0-9]+) will find a lower case character([a-z]), followed by one or more number.
+Only the numbers are between parenthesis and will be usable with $0 in the first line.
+
+For more information about Regexes, consult python documentation at: https://docs.python.org/3/howto/regex.html#regex-howto</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/rename_cues/rename_ui.py" line="57"/>
+      <source>Current</source>
+      <translation>Aktuell</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/rename_cues/rename_ui.py" line="58"/>
+      <source>Preview</source>
+      <translation>Vorschau</translation>
     </message>
   </context>
   <context>
@@ -75,9 +103,14 @@
   <context>
     <name>RenameUiDebug</name>
     <message>
-      <location filename="../../../plugins/rename_cues/rename_ui.py" line="258"/>
+      <location filename="../../../plugins/rename_cues/rename_ui.py" line="259"/>
       <source>Regex error: Invalid pattern</source>
       <translation>Regex-Fehler: Ungültiges Muster</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/rename_cues/rename_ui.py" line="288"/>
+      <source>Regex error: catch with () before display with $n</source>
+      <translation type="unfinished">Regex error: catch with () before display with $n</translation>
     </message>
   </context>
 </TS>
