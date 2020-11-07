fe5f6caf0f0eacd458565f71c9d4cd5aa99ef5eb
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2020-11-07 14:24:53 +0100

    New translations media_info.ts (Chinese Simplified)

--- lisp/i18n/ts/zh_CN/media_info.ts
+++ lisp/i18n/ts/zh_CN/media_info.ts
@@ -4,29 +4,29 @@
   <context>
     <name>MediaInfo</name>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="125"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="126"/>
       <source>Media Info</source>
       <translation>媒体资料</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="135"/>
-      <source>Info</source>
-      <translation>资料</translation>
-    </message>
-    <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="135"/>
-      <source>Value</source>
-      <translation>值</translation>
-    </message>
-    <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="70"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="72"/>
       <source>Warning</source>
       <translation>警告</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="70"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="73"/>
       <source>Cannot get any information.</source>
       <translation>无法获取任何资料</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/media_info/media_info.py" line="136"/>
+      <source>Info</source>
+      <translation>资料</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/media_info/media_info.py" line="136"/>
+      <source>Value</source>
+      <translation>值</translation>
     </message>
   </context>
 </TS>
