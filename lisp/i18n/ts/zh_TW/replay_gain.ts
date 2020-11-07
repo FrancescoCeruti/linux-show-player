c55e388b962e3bf37c2b7e43bc1633518ae538bb
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2020-11-07 14:26:38 +0100

    New translations replay_gain.ts (Chinese Traditional)

--- lisp/i18n/ts/zh_TW/replay_gain.ts
+++ lisp/i18n/ts/zh_TW/replay_gain.ts
@@ -4,7 +4,7 @@
   <context>
     <name>ReplayGain</name>
     <message>
-      <location filename="../../../plugins/replay_gain/gain_ui.py" line="110"/>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="111"/>
       <source>ReplayGain / Normalization</source>
       <translation>回放增益/標準化</translation>
     </message>
@@ -19,7 +19,7 @@
       <translation>全部重設</translation>
     </message>
     <message>
-      <location filename="../../../plugins/replay_gain/replay_gain.py" line="65"/>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="66"/>
       <source>Reset selected</source>
       <translation>重設所選</translation>
     </message>
@@ -29,17 +29,17 @@
       <translation>線程數量</translation>
     </message>
     <message>
-      <location filename="../../../plugins/replay_gain/gain_ui.py" line="114"/>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="115"/>
       <source>Apply only to selected media</source>
       <translation>只套用於所選媒體</translation>
     </message>
     <message>
-      <location filename="../../../plugins/replay_gain/gain_ui.py" line="117"/>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="118"/>
       <source>ReplayGain to (dB SPL)</source>
       <translation>回放增益至 (分貝 聲壓級)</translation>
     </message>
     <message>
-      <location filename="../../../plugins/replay_gain/gain_ui.py" line="120"/>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="121"/>
       <source>Normalize to (dB)</source>
       <translation>標準化至 (分貝)</translation>
     </message>
@@ -52,30 +52,43 @@
   <context>
     <name>ReplayGainDebug</name>
     <message>
-      <location filename="../../../plugins/replay_gain/replay_gain.py" line="236"/>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="237"/>
       <source>Applied gain for: {}</source>
       <translation>已套用為 {} 作增益</translation>
     </message>
     <message>
-      <location filename="../../../plugins/replay_gain/replay_gain.py" line="242"/>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="243"/>
       <source>Discarded gain for: {}</source>
       <translation>已捨棄為 {} 作增益</translation>
     </message>
   </context>
   <context>
+    <name>ReplayGainError</name>
+    <message>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="331"/>
+      <source>An error occurred during gain calculation.</source>
+      <translation type="unfinished">An error occurred during gain calculation.</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="203"/>
+      <source>An error occurred while processing gain results.</source>
+      <translation type="unfinished">An error occurred while processing gain results.</translation>
+    </message>
+  </context>
+  <context>
     <name>ReplayGainInfo</name>
     <message>
-      <location filename="../../../plugins/replay_gain/replay_gain.py" line="216"/>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="217"/>
       <source>Gain processing stopped by user.</source>
       <translation>用戶已停止增益處理</translation>
     </message>
     <message>
-      <location filename="../../../plugins/replay_gain/replay_gain.py" line="281"/>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="282"/>
       <source>Started gain calculation for: {}</source>
       <translation>已開始為 {} 作增益計算</translation>
     </message>
     <message>
-      <location filename="../../../plugins/replay_gain/replay_gain.py" line="316"/>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="317"/>
       <source>Gain calculated for: {}</source>
       <translation>已為 {} 作增益計算</translation>
     </message>
