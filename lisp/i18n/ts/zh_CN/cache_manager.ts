1208eaf1c217f0d8bca51b498a2c4623a230def8
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2020-11-07 14:26:09 +0100

    New translations cache_manager.ts (Chinese Simplified)

--- lisp/i18n/ts/zh_CN/cache_manager.ts
+++ lisp/i18n/ts/zh_CN/cache_manager.ts
@@ -4,12 +4,17 @@
   <context>
     <name>CacheManager</name>
     <message>
-      <location filename="../../../plugins/cache_manager/cache_manager.py" line="135"/>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="54"/>
+      <source>Cache size</source>
+      <translation type="unfinished">Cache size</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="136"/>
       <source>Cache size warning</source>
       <translation>缓存大小警告</translation>
     </message>
     <message>
-      <location filename="../../../plugins/cache_manager/cache_manager.py" line="138"/>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="139"/>
       <source>Warning threshold in MB (0 = disabled)</source>
       <translation>警告阀值 (MB为单位) (0 = 停用)</translation>
     </message>
@@ -19,9 +24,16 @@
       <translation>清除缓存</translation>
     </message>
     <message>
-      <location filename="../../../plugins/cache_manager/cache_manager.py" line="143"/>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="144"/>
       <source>Delete the cache content</source>
       <translation>清除缓存</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="55"/>
+      <source>The cache has exceeded {}. Consider clean it.
+You can do it in the application settings.</source>
+      <translation type="unfinished">The cache has exceeded {}. Consider clean it.
+You can do it in the application settings.</translation>
     </message>
   </context>
   <context>
