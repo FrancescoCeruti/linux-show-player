10a0925c46b521f8a976b2be332cf053dfb162a1
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2020-11-07 14:28:50 +0100

    New translations media_info.ts (Czech)

--- lisp/i18n/ts/cs_CZ/media_info.ts
+++ lisp/i18n/ts/cs_CZ/media_info.ts
@@ -4,29 +4,29 @@
   <context>
     <name>MediaInfo</name>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="125"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="126"/>
       <source>Media Info</source>
       <translation>Údaje o záznamu</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="135"/>
-      <source>Info</source>
-      <translation>Informace</translation>
-    </message>
-    <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="135"/>
-      <source>Value</source>
-      <translation>Hodnota</translation>
-    </message>
-    <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="70"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="72"/>
       <source>Warning</source>
       <translation>Varování</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="70"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="73"/>
       <source>Cannot get any information.</source>
       <translation>Nelze získat žádné informace.</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/media_info/media_info.py" line="136"/>
+      <source>Info</source>
+      <translation>Informace</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/media_info/media_info.py" line="136"/>
+      <source>Value</source>
+      <translation>Hodnota</translation>
     </message>
   </context>
 </TS>
