56a75eafcf95265ec09da26f7562af7aaa163b8e
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2020-11-08 16:33:32 +0100

    New translations cache_manager.ts (Chinese Simplified)

--- lisp/i18n/ts/zh_CN/cache_manager.ts
+++ lisp/i18n/ts/zh_CN/cache_manager.ts
@@ -9,27 +9,27 @@
       <translation type="unfinished">Cache size</translation>
     </message>
     <message>
-      <location filename="../../../plugins/cache_manager/cache_manager.py" line="136"/>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="139"/>
       <source>Cache size warning</source>
       <translation>缓存大小警告</translation>
     </message>
     <message>
-      <location filename="../../../plugins/cache_manager/cache_manager.py" line="139"/>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="142"/>
       <source>Warning threshold in MB (0 = disabled)</source>
       <translation>警告阀值 (MB为单位) (0 = 停用)</translation>
     </message>
     <message>
-      <location filename="../../../plugins/cache_manager/cache_manager.py" line="142"/>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="145"/>
       <source>Cache cleanup</source>
       <translation>清除缓存</translation>
     </message>
     <message>
-      <location filename="../../../plugins/cache_manager/cache_manager.py" line="144"/>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="147"/>
       <source>Delete the cache content</source>
       <translation>清除缓存</translation>
     </message>
     <message>
-      <location filename="../../../plugins/cache_manager/cache_manager.py" line="55"/>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="58"/>
       <source>The cache has exceeded {}. Consider clean it.
 You can do it in the application settings.</source>
       <translation type="unfinished">The cache has exceeded {}. Consider clean it.
@@ -39,7 +39,7 @@ You can do it in the application settings.</translation>
   <context>
     <name>SettingsPageName</name>
     <message>
-      <location filename="../../../plugins/cache_manager/cache_manager.py" line="96"/>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="99"/>
       <source>Cache Manager</source>
       <translation>缓存管理</translation>
     </message>
