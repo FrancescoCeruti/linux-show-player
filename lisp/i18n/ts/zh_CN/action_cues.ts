efe505554f20ae32a6a9537d00aa9c08c8fa69b1
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2021-09-05 15:51:38 +0200

    New translations action_cues.ts (Chinese Simplified)

--- lisp/i18n/ts/zh_CN/action_cues.ts
+++ lisp/i18n/ts/zh_CN/action_cues.ts
@@ -54,7 +54,7 @@
     <message>
       <location filename="../../../plugins/action_cues/command_cue.py" line="80"/>
       <source>Command cue ended with an error status. Exit code: {}</source>
-      <translation type="unfinished">Command cue ended with an error status. Exit code: {}</translation>
+      <translation>指令 Cue 发生错误。退出代码：{}</translation>
     </message>
   </context>
   <context>
