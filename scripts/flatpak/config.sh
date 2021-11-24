5b0f4d3bd5de0d7bb3ec5e7b95b9de25358c6723
Francesco Ceruti <ceppofrancy@gmail.com>
2021-11-24 22:28:50 +0100

    Fix: ignore pyqt5-qt5 package when building the flatpak bundle

--- scripts/flatpak/config.sh
+++ scripts/flatpak/config.sh
@@ -7,7 +7,7 @@ export FLATPAK_SDK="org.gnome.Sdk"
 export FLATPAK_INSTALL="$FLATPAK_RUNTIME//$FLATPAK_RUNTIME_VERSION $FLATPAK_SDK//$FLATPAK_RUNTIME_VERSION"
 
 export FLATPAK_PY_LOCKFILE="../../poetry.lock"
-export FLATPAK_PY_IGNORE_PACKAGES="pygobject pycairo pyqt5 pyqt5-sip pyqt5-qt"
+export FLATPAK_PY_IGNORE_PACKAGES="pygobject pycairo pyqt5 pyqt5-sip pyqt5-qt pyqt5-qt5"
 
 export FLATPAK_APP_ID="org.linux_show_player.LinuxShowPlayer"
 export FLATPAK_APP_MODULE="linux-show-player"
