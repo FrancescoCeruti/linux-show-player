2b9b41986455b2c68acc9da24ea65b03f1caf2cb
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2022-10-08 14:26:50 +0200

    New translations list_layout.ts (Spanish)

--- lisp/i18n/ts/es_ES/list_layout.ts
+++ lisp/i18n/ts/es_ES/list_layout.ts
@@ -239,14 +239,9 @@
   <context>
     <name>ListLayoutInfoPanel</name>
     <message>
-      <location filename="../../../plugins/list_layout/info_panel.py" line="49"/>
+      <location filename="../../../plugins/list_layout/info_panel.py" line="50"/>
       <source>Cue name</source>
       <translation>Nombre del Cue</translation>
-    </message>
-    <message>
-      <location filename="../../../plugins/list_layout/info_panel.py" line="52"/>
-      <source>Cue description</source>
-      <translation>Descripción del cue</translation>
     </message>
   </context>
   <context>
