3d2efdd3d2a0a9ac082bc2e2de12f82c9b59b707
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2022-10-08 14:28:29 +0200

    New translations lisp.ts (Slovenian)

--- lisp/i18n/ts/sl_SI/lisp.ts
+++ lisp/i18n/ts/sl_SI/lisp.ts
@@ -247,42 +247,42 @@
   <context>
     <name>CueAppearanceSettings</name>
     <message>
-      <location filename="../../../ui/settings/cue_pages/cue_appearance.py" line="80"/>
+      <location filename="../../../ui/settings/cue_pages/cue_appearance.py" line="83"/>
       <source>The appearance depends on the layout</source>
       <translation>Prikaz je odvisen od uporabljene razporeditve</translation>
     </message>
     <message>
-      <location filename="../../../ui/settings/cue_pages/cue_appearance.py" line="92"/>
+      <location filename="../../../ui/settings/cue_pages/cue_appearance.py" line="95"/>
       <source>Cue name</source>
       <translation>Ime vrste</translation>
     </message>
     <message>
-      <location filename="../../../ui/settings/cue_pages/cue_appearance.py" line="94"/>
+      <location filename="../../../ui/settings/cue_pages/cue_appearance.py" line="97"/>
       <source>NoName</source>
       <translation>Brez imena</translation>
     </message>
     <message>
-      <location filename="../../../ui/settings/cue_pages/cue_appearance.py" line="96"/>
+      <location filename="../../../ui/settings/cue_pages/cue_appearance.py" line="99"/>
       <source>Description/Note</source>
       <translation>Opis/Zapiski</translation>
     </message>
     <message>
-      <location filename="../../../ui/settings/cue_pages/cue_appearance.py" line="99"/>
+      <location filename="../../../ui/settings/cue_pages/cue_appearance.py" line="102"/>
       <source>Set Font Size</source>
       <translation>Nastavi velikost pisave</translation>
     </message>
     <message>
-      <location filename="../../../ui/settings/cue_pages/cue_appearance.py" line="101"/>
+      <location filename="../../../ui/settings/cue_pages/cue_appearance.py" line="104"/>
       <source>Color</source>
       <translation>Barva</translation>
     </message>
     <message>
-      <location filename="../../../ui/settings/cue_pages/cue_appearance.py" line="103"/>
+      <location filename="../../../ui/settings/cue_pages/cue_appearance.py" line="106"/>
       <source>Select background color</source>
       <translation>Izberi barvo ozadja</translation>
     </message>
     <message>
-      <location filename="../../../ui/settings/cue_pages/cue_appearance.py" line="106"/>
+      <location filename="../../../ui/settings/cue_pages/cue_appearance.py" line="109"/>
       <source>Select font color</source>
       <translation>Izberi barvo pisave</translation>
     </message>
@@ -974,7 +974,7 @@
       <translation type="unfinished">Fade In/Out</translation>
     </message>
     <message>
-      <location filename="../../../ui/settings/cue_pages/cue_appearance.py" line="35"/>
+      <location filename="../../../ui/settings/cue_pages/cue_appearance.py" line="36"/>
       <source>Appearance</source>
       <translation>Prikaz</translation>
     </message>
