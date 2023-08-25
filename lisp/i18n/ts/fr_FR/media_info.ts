9298b7032ea9d14d302bf6ffecc6c96ce5544755
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2023-08-25 10:58:26 +0200

    New translations media_info.ts (French)

--- lisp/i18n/ts/fr_FR/media_info.ts
+++ lisp/i18n/ts/fr_FR/media_info.ts
@@ -4,27 +4,27 @@
   <context>
     <name>MediaInfo</name>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="126"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="125"/>
       <source>Media Info</source>
       <translation>Info sur le média</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="72"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="71"/>
       <source>Warning</source>
       <translation>Avertissement</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="73"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="72"/>
       <source>Cannot get any information.</source>
       <translation type="unfinished">Cannot get any information.</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="136"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="135"/>
       <source>Info</source>
       <translation>Information</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="136"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="135"/>
       <source>Value</source>
       <translation>Valeur</translation>
     </message>
