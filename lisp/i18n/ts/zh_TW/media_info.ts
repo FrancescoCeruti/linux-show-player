30bbf06b8f61460c5bfeb6b185a41849e06edbaf
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2023-08-25 11:00:33 +0200

    New translations media_info.ts (Chinese Traditional)

--- lisp/i18n/ts/zh_TW/media_info.ts
+++ lisp/i18n/ts/zh_TW/media_info.ts
@@ -4,27 +4,27 @@
   <context>
     <name>MediaInfo</name>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="126"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="125"/>
       <source>Media Info</source>
       <translation>媒體資料</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="72"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="71"/>
       <source>Warning</source>
       <translation>警告</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="73"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="72"/>
       <source>Cannot get any information.</source>
       <translation>無法獲取任何資料</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="136"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="135"/>
       <source>Info</source>
       <translation>資料</translation>
     </message>
     <message>
-      <location filename="../../../plugins/media_info/media_info.py" line="136"/>
+      <location filename="../../../plugins/media_info/media_info.py" line="135"/>
       <source>Value</source>
       <translation>值</translation>
     </message>
