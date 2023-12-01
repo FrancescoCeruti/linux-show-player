95ea9021f9f78c701accbb2b61e7f4731ff12e5a
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2023-12-01 23:53:43 +0100

    New translations cache_manager.ts (Catalan)

---
+++ lisp/i18n/ts/ca_ES/cache_manager.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ca" sourcelanguage="en">
+  <context>
+    <name>CacheManager</name>
+    <message>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="54"/>
+      <source>Cache size</source>
+      <translation type="unfinished">Cache size</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="139"/>
+      <source>Cache size warning</source>
+      <translation type="unfinished">Cache size warning</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="142"/>
+      <source>Warning threshold in MB (0 = disabled)</source>
+      <translation type="unfinished">Warning threshold in MB (0 = disabled)</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="145"/>
+      <source>Cache cleanup</source>
+      <translation type="unfinished">Cache cleanup</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="147"/>
+      <source>Delete the cache content</source>
+      <translation type="unfinished">Delete the cache content</translation>
+    </message>
+    <message>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="58"/>
+      <source>The cache has exceeded {}. Consider clean it.
+You can do it in the application settings.</source>
+      <translation type="unfinished">The cache has exceeded {}. Consider clean it.
+You can do it in the application settings.</translation>
+    </message>
+  </context>
+  <context>
+    <name>SettingsPageName</name>
+    <message>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="99"/>
+      <source>Cache Manager</source>
+      <translation type="unfinished">Cache Manager</translation>
+    </message>
+  </context>
+</TS>
