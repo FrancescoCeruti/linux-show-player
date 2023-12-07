d737695a12da6e197a8628c3aed8c8724451516f
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2023-12-07 20:28:08 +0100

    New translations controller.ts (Catalan)

--- lisp/i18n/ts/ca_ES/controller.ts
+++ lisp/i18n/ts/ca_ES/controller.ts
@@ -14,12 +14,12 @@
     <message>
       <location filename="../../../plugins/controller/protocols/keyboard.py" line="78"/>
       <source>Shortcuts</source>
-      <translation>Drecera</translation>
+      <translation>Dreceres</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/protocols/keyboard.py" line="57"/>
       <source>Shortcut</source>
-      <translation type="unfinished">Shortcut</translation>
+      <translation>Drecera</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/protocols/keyboard.py" line="58"/>
@@ -37,7 +37,7 @@
     <message>
       <location filename="../../../plugins/controller/protocols/midi.py" line="105"/>
       <source>-- All Messages --</source>
-      <translation type="unfinished">-- All Messages --</translation>
+      <translation>-- Tots els missatges --</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/protocols/midi.py" line="125"/>
@@ -47,7 +47,7 @@
     <message>
       <location filename="../../../plugins/controller/protocols/midi.py" line="127"/>
       <source>Capture filter</source>
-      <translation type="unfinished">Capture filter</translation>
+      <translation>Filtre de Captura</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/protocols/midi.py" line="162"/>
@@ -85,7 +85,7 @@
     <message>
       <location filename="../../../plugins/controller/protocols/midi.py" line="147"/>
       <source>Error while importing configuration entry, skipped.</source>
-      <translation type="unfinished">Error while importing configuration entry, skipped.</translation>
+      <translation>S&apos;ha produït un error en importar l&apos;entrada de configuració, s&apos;ha omès.</translation>
     </message>
   </context>
   <context>
@@ -93,17 +93,17 @@
     <message>
       <location filename="../../../plugins/controller/protocols/osc.py" line="102"/>
       <source>OSC Message</source>
-      <translation type="unfinished">OSC Message</translation>
+      <translation>Missatge OSC</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/protocols/osc.py" line="104"/>
       <source>OSC Path:</source>
-      <translation type="unfinished">OSC Path:</translation>
+      <translation>Camí OSC:</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/protocols/osc.py" line="110"/>
       <source>/path/to/method</source>
-      <translation type="unfinished">/path/to/method</translation>
+      <translation>/cami/al/metode</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/protocols/osc.py" line="238"/>
@@ -113,37 +113,37 @@
     <message>
       <location filename="../../../plugins/controller/protocols/osc.py" line="265"/>
       <source>OSC Capture</source>
-      <translation type="unfinished">OSC Capture</translation>
+      <translation>Captura OSC</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/protocols/osc.py" line="293"/>
       <source>Add</source>
-      <translation type="unfinished">Add</translation>
+      <translation>Afegeix</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/protocols/osc.py" line="294"/>
       <source>Remove</source>
-      <translation type="unfinished">Remove</translation>
+      <translation>Elimina</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/protocols/osc.py" line="295"/>
       <source>Capture</source>
-      <translation type="unfinished">Capture</translation>
+      <translation>Captura</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/protocols/osc.py" line="340"/>
       <source>Waiting for messages:</source>
-      <translation type="unfinished">Waiting for messages:</translation>
+      <translation>S&apos;estan esperant els missatges:</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/protocols/osc.py" line="443"/>
       <source>Path</source>
-      <translation type="unfinished">Path</translation>
+      <translation>Camí</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/protocols/osc.py" line="444"/>
       <source>Types</source>
-      <translation type="unfinished">Types</translation>
+      <translation>Tipus</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/protocols/osc.py" line="445"/>
@@ -153,7 +153,7 @@
     <message>
       <location filename="../../../plugins/controller/protocols/osc.py" line="446"/>
       <source>Action</source>
-      <translation type="unfinished">Action</translation>
+      <translation>Acció</translation>
     </message>
   </context>
   <context>
@@ -161,19 +161,19 @@
     <message>
       <location filename="../../../plugins/controller/protocols/osc.py" line="357"/>
       <source>Warning</source>
-      <translation type="unfinished">Warning</translation>
+      <translation>Avís</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/protocols/osc.py" line="358"/>
       <source>Osc path seems invalid,
 do not forget to edit the path later.</source>
-      <translation type="unfinished">Osc path seems invalid,
-do not forget to edit the path later.</translation>
+      <translation>El camí OSC sembla no vàlid.
+No us oblideu d&apos;editar el camí més tard.</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/protocols/osc.py" line="317"/>
       <source>Error while importing configuration entry, skipped.</source>
-      <translation type="unfinished">Error while importing configuration entry, skipped.</translation>
+      <translation>S&apos;ha produït un error en importar l&apos;entrada de configuració, s&apos;ha omès.</translation>
     </message>
   </context>
   <context>
@@ -204,42 +204,42 @@ do not forget to edit the path later.</translation>
     <message>
       <location filename="../../../plugins/controller/common.py" line="43"/>
       <source>Stop all cues</source>
-      <translation type="unfinished">Stop all cues</translation>
+      <translation>Atura tots el cues</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/common.py" line="44"/>
       <source>Pause all cues</source>
-      <translation type="unfinished">Pause all cues</translation>
+      <translation>Pausa tots els cues</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/common.py" line="45"/>
       <source>Resume all cues</source>
-      <translation type="unfinished">Resume all cues</translation>
+      <translation>Repren tots els cues</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/common.py" line="48"/>
       <source>Interrupt all cues</source>
-      <translation type="unfinished">Interrupt all cues</translation>
+      <translation>Interromp tots els cues</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/common.py" line="51"/>
       <source>Fade-out all cues</source>
-      <translation type="unfinished">Fade-out all cues</translation>
+      <translation>Fade Out tots els cues</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/common.py" line="54"/>
       <source>Fade-in all cues</source>
-      <translation type="unfinished">Fade-in all cues</translation>
+      <translation>Fade In tots els cues</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/common.py" line="57"/>
       <source>Move standby forward</source>
-      <translation type="unfinished">Move standby forward</translation>
+      <translation>Mou a l&apos;espera cap endavant</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/common.py" line="60"/>
       <source>Move standby back</source>
-      <translation type="unfinished">Move standby back</translation>
+      <translation>Mou a l&apos;espera cap enrrera</translation>
     </message>
   </context>
   <context>
@@ -247,7 +247,7 @@ do not forget to edit the path later.</translation>
     <message>
       <location filename="../../../plugins/controller/protocols/osc.py" line="73"/>
       <source>Type</source>
-      <translation type="unfinished">Type</translation>
+      <translation>Tipus</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/protocols/osc.py" line="73"/>
@@ -260,12 +260,12 @@ do not forget to edit the path later.</translation>
     <message>
       <location filename="../../../plugins/controller/protocols/osc.py" line="112"/>
       <source>Add</source>
-      <translation type="unfinished">Add</translation>
+      <translation>Afegeix</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/protocols/osc.py" line="113"/>
       <source>Remove</source>
-      <translation type="unfinished">Remove</translation>
+      <translation>Elimina</translation>
     </message>
   </context>
   <context>
@@ -278,7 +278,7 @@ do not forget to edit the path later.</translation>
     <message>
       <location filename="../../../plugins/controller/controller_settings.py" line="41"/>
       <source>Layout Controls</source>
-      <translation type="unfinished">Layout Controls</translation>
+      <translation>Control de la disposició</translation>
     </message>
     <message>
       <location filename="../../../plugins/controller/protocols/keyboard.py" line="44"/>
@@ -293,7 +293,7 @@ do not forget to edit the path later.</translation>
     <message>
       <location filename="../../../plugins/controller/protocols/osc.py" line="230"/>
       <source>OSC Controls</source>
-      <translation type="unfinished">OSC Controls</translation>
+      <translation>Controls OSC</translation>
     </message>
   </context>
 </TS>
