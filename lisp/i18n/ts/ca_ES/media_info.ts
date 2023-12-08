cf65b97f4a72867a1ef229d4734eb13707248091
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2023-12-08 15:59:03 +0100

    New translations media_info.ts (Catalan)

--- lisp/i18n/ts/ca_ES/media_info.ts
+++ lisp/i18n/ts/ca_ES/media_info.ts
@@ -1,42 +1,32 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ca_ES" sourcelanguage="en">
-<context>
+<TS version="2.0" language="ca" sourcelanguage="en">
+  <context>
     <name>MediaInfo</name>
     <message>
-        <location filename="../../../plugins/media_info/media_info.py" line="125"/>
-        <source>Media Info</source>
-        <translation>Informació del Media</translation>
+      <location filename="../../../plugins/media_info/media_info.py" line="125"/>
+      <source>Media Info</source>
+      <translation>Informació del Media</translation>
     </message>
     <message>
-        <location filename="../../../plugins/media_info/media_info.py" line="71"/>
-        <source>Warning</source>
-        <translation>Avís</translation>
+      <location filename="../../../plugins/media_info/media_info.py" line="71"/>
+      <source>Warning</source>
+      <translation>Avís</translation>
     </message>
     <message>
-        <location filename="../../../plugins/media_info/media_info.py" line="72"/>
-        <source>Cannot get any information.</source>
-        <translation>No es pot obtenir cap informació.</translation>
+      <location filename="../../../plugins/media_info/media_info.py" line="72"/>
+      <source>Cannot get any information.</source>
+      <translation>No es pot obtenir cap informació.</translation>
     </message>
     <message>
-        <location filename="../../../plugins/media_info/media_info.py" line="135"/>
-        <source>Info</source>
-        <translation>Informació</translation>
+      <location filename="../../../plugins/media_info/media_info.py" line="135"/>
+      <source>Info</source>
+      <translation>Informació</translation>
     </message>
     <message>
-        <location filename="../../../plugins/media_info/media_info.py" line="135"/>
-        <source>Value</source>
-        <translation>Valor</translation>
+      <location filename="../../../plugins/media_info/media_info.py" line="135"/>
+      <source>Value</source>
+      <translation>Valor</translation>
     </message>
-    <message>
-        <location filename="../media_info.py" line="51"/>
-        <source>Error</source>
-        <translation type="obsolete">Error</translation>
-    </message>
-    <message>
-        <location filename="../media_info.py" line="51"/>
-        <source>No info to display</source>
-        <translation type="obsolete">Cap informació per mostrar</translation>
-    </message>
-</context>
+  </context>
 </TS>
