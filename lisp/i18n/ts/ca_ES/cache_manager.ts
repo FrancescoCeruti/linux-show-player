94933a0fabf621dc87e731f1abae150f23d33894
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2023-12-08 16:00:48 +0100

    New translations cache_manager.ts (Catalan)

--- lisp/i18n/ts/ca_ES/cache_manager.ts
+++ lisp/i18n/ts/ca_ES/cache_manager.ts
@@ -1,47 +1,47 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ca_ES" sourcelanguage="en">
-<context>
+<TS version="2.0" language="ca" sourcelanguage="en">
+  <context>
     <name>CacheManager</name>
     <message>
-        <location filename="../../../plugins/cache_manager/cache_manager.py" line="54"/>
-        <source>Cache size</source>
-        <translation>Mida de la memòria cau</translation>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="54"/>
+      <source>Cache size</source>
+      <translation>Mida de la memòria cau</translation>
     </message>
     <message>
-        <location filename="../../../plugins/cache_manager/cache_manager.py" line="139"/>
-        <source>Cache size warning</source>
-        <translation>Avís de mida de la memòria cau</translation>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="139"/>
+      <source>Cache size warning</source>
+      <translation>Avís de mida de la memòria cau</translation>
     </message>
     <message>
-        <location filename="../../../plugins/cache_manager/cache_manager.py" line="142"/>
-        <source>Warning threshold in MB (0 = disabled)</source>
-        <translation>Llindar d&apos;avís en MB (0 = desactivat)</translation>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="142"/>
+      <source>Warning threshold in MB (0 = disabled)</source>
+      <translation>Llindar d'avís en MB (0 = desactivat)</translation>
     </message>
     <message>
-        <location filename="../../../plugins/cache_manager/cache_manager.py" line="145"/>
-        <source>Cache cleanup</source>
-        <translation>Neteja de la memòria cau</translation>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="145"/>
+      <source>Cache cleanup</source>
+      <translation>Neteja de la memòria cau</translation>
     </message>
     <message>
-        <location filename="../../../plugins/cache_manager/cache_manager.py" line="147"/>
-        <source>Delete the cache content</source>
-        <translation>Suprimeix el contingut de la memòria cau</translation>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="147"/>
+      <source>Delete the cache content</source>
+      <translation>Suprimeix el contingut de la memòria cau</translation>
     </message>
     <message>
-        <location filename="../../../plugins/cache_manager/cache_manager.py" line="58"/>
-        <source>The cache has exceeded {}. Consider clean it.
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="58"/>
+      <source>The cache has exceeded {}. Consider clean it.
 You can do it in the application settings.</source>
-        <translation>La memòria cau ha excedit {}. Penseu en netejar-lo.
-Podeu fer-ho a la configuració de l&apos;aplicació.</translation>
+      <translation>La memòria cau ha excedit {}. Penseu en netejar-lo.
+Podeu fer-ho a la configuració de l'aplicació.</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>SettingsPageName</name>
     <message>
-        <location filename="../../../plugins/cache_manager/cache_manager.py" line="99"/>
-        <source>Cache Manager</source>
-        <translation>Gestor de la memòria cau</translation>
+      <location filename="../../../plugins/cache_manager/cache_manager.py" line="99"/>
+      <source>Cache Manager</source>
+      <translation>Gestor de la memòria cau</translation>
     </message>
-</context>
+  </context>
 </TS>
