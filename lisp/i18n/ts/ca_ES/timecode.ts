c336ac9bf8760bbbf58547ec14df3e2d4363ea64
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2023-12-08 15:59:45 +0100

    New translations timecode.ts (Catalan)

--- lisp/i18n/ts/ca_ES/timecode.ts
+++ lisp/i18n/ts/ca_ES/timecode.ts
@@ -1,126 +1,81 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ca_ES" sourcelanguage="en">
-<context>
+<TS version="2.0" language="ca" sourcelanguage="en">
+  <context>
     <name>SettingsPageName</name>
     <message>
-        <location filename="../../../plugins/timecode/settings.py" line="38"/>
-        <source>Timecode</source>
-        <translation>Codi de Temps</translation>
+      <location filename="../../../plugins/timecode/settings.py" line="38"/>
+      <source>Timecode</source>
+      <translation>Codi de Temps</translation>
     </message>
     <message>
-        <location filename="../../../plugins/timecode/settings.py" line="114"/>
-        <source>Timecode Settings</source>
-        <translation>Paràmetres de Codi de Temps</translation>
+      <location filename="../../../plugins/timecode/settings.py" line="114"/>
+      <source>Timecode Settings</source>
+      <translation>Paràmetres de Codi de Temps</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>Timecode</name>
     <message>
-        <location filename="../../../plugins/timecode/timecode.py" line="99"/>
-        <source>Cannot load timecode protocol: &quot;{}&quot;</source>
-        <translation>No es pot carregar el protocol de codis de temps: &quot;{}&quot;</translation>
+      <location filename="../../../plugins/timecode/timecode.py" line="99"/>
+      <source>Cannot load timecode protocol: "{}"</source>
+      <translation>No es pot carregar el protocol de codis de temps: "{}"</translation>
     </message>
-    <message>
-        <location filename="../timecode.py" line="146"/>
-        <source>Cannot send timecode.</source>
-        <translation type="obsolete">No es pot enviar Codi de Temps.</translation>
-    </message>
-    <message>
-        <location filename="../timecode.py" line="146"/>
-        <source>OLA has stopped.</source>
-        <translation type="obsolete">OLA s&apos;ha aturat.</translation>
-    </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>TimecodeError</name>
     <message>
-        <location filename="../../../plugins/timecode/protocols/artnet.py" line="65"/>
-        <source>Cannot send timecode. 
+      <location filename="../../../plugins/timecode/protocols/artnet.py" line="65"/>
+      <source>Cannot send timecode. 
 OLA daemon has stopped.</source>
-        <translation>No es pot enviar el codi de temps. 
-El dimoni OLA s&apos;ha aturat.</translation>
+      <translation>No es pot enviar el codi de temps. 
+El dimoni OLA s'ha aturat.</translation>
     </message>
     <message>
-        <location filename="../../../plugins/timecode/protocols/artnet.py" line="73"/>
-        <source>Cannot send timecode.</source>
-        <translation>No es pot enviar el codi de temps.</translation>
+      <location filename="../../../plugins/timecode/protocols/artnet.py" line="73"/>
+      <source>Cannot send timecode.</source>
+      <translation>No es pot enviar el codi de temps.</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>TimecodeSettings</name>
     <message>
-        <location filename="../../../plugins/timecode/settings.py" line="81"/>
-        <source>Replace HOURS by a static track number</source>
-        <translation>Reemplaça HORES per un nombre de track estàtic</translation>
+      <location filename="../../../plugins/timecode/settings.py" line="81"/>
+      <source>Replace HOURS by a static track number</source>
+      <translation>Reemplaça HORES per un nombre de track estàtic</translation>
     </message>
     <message>
-        <location filename="../../../plugins/timecode/settings.py" line="86"/>
-        <source>Enable Timecode</source>
-        <translation>Activa el codi de temps</translation>
+      <location filename="../../../plugins/timecode/settings.py" line="86"/>
+      <source>Enable Timecode</source>
+      <translation>Activa el codi de temps</translation>
     </message>
     <message>
-        <location filename="../../../plugins/timecode/settings.py" line="88"/>
-        <source>Track number</source>
-        <translation>Nombre de Pistes</translation>
+      <location filename="../../../plugins/timecode/settings.py" line="88"/>
+      <source>Track number</source>
+      <translation>Nombre de Pistes</translation>
     </message>
     <message>
-        <location filename="../../../plugins/timecode/settings.py" line="145"/>
-        <source>Timecode Settings</source>
-        <translation>Ajustos del codi de temps</translation>
+      <location filename="../../../plugins/timecode/settings.py" line="145"/>
+      <source>Timecode Settings</source>
+      <translation>Ajustos del codi de temps</translation>
     </message>
     <message>
-        <location filename="../../../plugins/timecode/settings.py" line="148"/>
-        <source>Timecode Format:</source>
-        <translation>Format del Codi de Temps:</translation>
+      <location filename="../../../plugins/timecode/settings.py" line="148"/>
+      <source>Timecode Format:</source>
+      <translation>Format del Codi de Temps:</translation>
     </message>
     <message>
-        <location filename="../../../plugins/timecode/settings.py" line="151"/>
-        <source>Timecode Protocol:</source>
-        <translation>Protocol del codi de temps:</translation>
+      <location filename="../../../plugins/timecode/settings.py" line="151"/>
+      <source>Timecode Protocol:</source>
+      <translation>Protocol del codi de temps:</translation>
     </message>
-    <message>
-        <location filename="../timecode_settings.py" line="64"/>
-        <source>OLA Timecode Settings</source>
-        <translation type="obsolete">Paràmetres de Codi de Temps OLA</translation>
-    </message>
-    <message>
-        <location filename="../timecode_settings.py" line="66"/>
-        <source>Enable Plugin</source>
-        <translation type="obsolete">Activa el Connector</translation>
-    </message>
-    <message>
-        <location filename="../timecode_settings.py" line="67"/>
-        <source>High-Resolution Timecode</source>
-        <translation type="obsolete">Codi de Temps d&apos;Alta Resolució</translation>
-    </message>
-    <message>
-        <location filename="../timecode_settings.py" line="78"/>
-        <source>OLA status</source>
-        <translation type="obsolete">Esta de OLA</translation>
-    </message>
-    <message>
-        <location filename="../timecode_settings.py" line="78"/>
-        <source>OLA is not running - start the OLA daemon.</source>
-        <translation type="obsolete">OLA no s&apos;està executant - incia el dimoni OLA.</translation>
-    </message>
-    <message>
-        <location filename="../timecode_settings.py" line="148"/>
-        <source>Enable ArtNet Timecode</source>
-        <translation type="obsolete">Activa Codi de Temps ArtNet</translation>
-    </message>
-    <message>
-        <location filename="../timecode_settings.py" line="152"/>
-        <source>To send ArtNet Timecode you need to setup a running OLA session!</source>
-        <translation type="obsolete">Per enviar Codi de Temps ArtNet necessites configurar una sessió OLA en execució!</translation>
-    </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>TimecodeWarning</name>
     <message>
-        <location filename="../../../plugins/timecode/cue_tracker.py" line="119"/>
-        <source>Cannot send timecode, untracking cue</source>
-        <translation>No s&apos;ha pogut enviar el codi de temps, s&apos;està desfent el seguiment del cue</translation>
+      <location filename="../../../plugins/timecode/cue_tracker.py" line="119"/>
+      <source>Cannot send timecode, untracking cue</source>
+      <translation>No s'ha pogut enviar el codi de temps, s'està desfent el seguiment del cue</translation>
     </message>
-</context>
+  </context>
 </TS>
