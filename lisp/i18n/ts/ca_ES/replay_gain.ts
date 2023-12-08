426b42e3d6a1e03b0a52ea4226534ed5295ef8b8
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2023-12-08 15:59:28 +0100

    New translations replay_gain.ts (Catalan)

--- lisp/i18n/ts/ca_ES/replay_gain.ts
+++ lisp/i18n/ts/ca_ES/replay_gain.ts
@@ -1,96 +1,96 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ca_ES" sourcelanguage="en">
-<context>
+<TS version="2.0" language="ca" sourcelanguage="en">
+  <context>
     <name>ReplayGain</name>
     <message>
-        <location filename="../../../plugins/replay_gain/gain_ui.py" line="111"/>
-        <source>ReplayGain / Normalization</source>
-        <translation>ReplayGain / Normalització</translation>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="111"/>
+      <source>ReplayGain / Normalization</source>
+      <translation>ReplayGain / Normalització</translation>
     </message>
     <message>
-        <location filename="../../../plugins/replay_gain/replay_gain.py" line="55"/>
-        <source>Calculate</source>
-        <translation>Calcular</translation>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="55"/>
+      <source>Calculate</source>
+      <translation>Calcular</translation>
     </message>
     <message>
-        <location filename="../../../plugins/replay_gain/replay_gain.py" line="60"/>
-        <source>Reset all</source>
-        <translation>Reestableix tot</translation>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="60"/>
+      <source>Reset all</source>
+      <translation>Reestableix tot</translation>
     </message>
     <message>
-        <location filename="../../../plugins/replay_gain/replay_gain.py" line="66"/>
-        <source>Reset selected</source>
-        <translation>Reestableix els selecionats</translation>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="66"/>
+      <source>Reset selected</source>
+      <translation>Reestableix els selecionats</translation>
     </message>
     <message>
-        <location filename="../../../plugins/replay_gain/gain_ui.py" line="113"/>
-        <source>Threads number</source>
-        <translation>Nombre de processos</translation>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="113"/>
+      <source>Threads number</source>
+      <translation>Nombre de processos</translation>
     </message>
     <message>
-        <location filename="../../../plugins/replay_gain/gain_ui.py" line="115"/>
-        <source>Apply only to selected media</source>
-        <translation>Aplica només als media seleccionats</translation>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="115"/>
+      <source>Apply only to selected media</source>
+      <translation>Aplica només als media seleccionats</translation>
     </message>
     <message>
-        <location filename="../../../plugins/replay_gain/gain_ui.py" line="118"/>
-        <source>ReplayGain to (dB SPL)</source>
-        <translation>ReplayGain to (dB SPL)</translation>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="118"/>
+      <source>ReplayGain to (dB SPL)</source>
+      <translation>ReplayGain to (dB SPL)</translation>
     </message>
     <message>
-        <location filename="../../../plugins/replay_gain/gain_ui.py" line="121"/>
-        <source>Normalize to (dB)</source>
-        <translation>Normalitza a (dB)</translation>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="121"/>
+      <source>Normalize to (dB)</source>
+      <translation>Normalitza a (dB)</translation>
     </message>
     <message>
-        <location filename="../../../plugins/replay_gain/gain_ui.py" line="145"/>
-        <source>Processing files ...</source>
-        <translation>Processant arxius ...</translation>
+      <location filename="../../../plugins/replay_gain/gain_ui.py" line="145"/>
+      <source>Processing files ...</source>
+      <translation>Processant arxius ...</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>ReplayGainDebug</name>
     <message>
-        <location filename="../../../plugins/replay_gain/replay_gain.py" line="237"/>
-        <source>Applied gain for: {}</source>
-        <translation>Guany aplicat per: {}</translation>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="237"/>
+      <source>Applied gain for: {}</source>
+      <translation>Guany aplicat per: {}</translation>
     </message>
     <message>
-        <location filename="../../../plugins/replay_gain/replay_gain.py" line="243"/>
-        <source>Discarded gain for: {}</source>
-        <translation>Guany descartat per: {}</translation>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="243"/>
+      <source>Discarded gain for: {}</source>
+      <translation>Guany descartat per: {}</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>ReplayGainError</name>
     <message>
-        <location filename="../../../plugins/replay_gain/replay_gain.py" line="331"/>
-        <source>An error occurred during gain calculation.</source>
-        <translation>S&apos;ha produït un error durant el càlcul del guany.</translation>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="331"/>
+      <source>An error occurred during gain calculation.</source>
+      <translation>S'ha produït un error durant el càlcul del guany.</translation>
     </message>
     <message>
-        <location filename="../../../plugins/replay_gain/replay_gain.py" line="203"/>
-        <source>An error occurred while processing gain results.</source>
-        <translation>S&apos;ha produït un error en processar els resultats de guany.</translation>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="203"/>
+      <source>An error occurred while processing gain results.</source>
+      <translation>S'ha produït un error en processar els resultats de guany.</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>ReplayGainInfo</name>
     <message>
-        <location filename="../../../plugins/replay_gain/replay_gain.py" line="217"/>
-        <source>Gain processing stopped by user.</source>
-        <translation>L&apos;usuari ha aturat el processament de guanys.</translation>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="217"/>
+      <source>Gain processing stopped by user.</source>
+      <translation>L'usuari ha aturat el processament de guanys.</translation>
     </message>
     <message>
-        <location filename="../../../plugins/replay_gain/replay_gain.py" line="282"/>
-        <source>Started gain calculation for: {}</source>
-        <translation>S&apos;ha iniciat el càlcul del guany per a: {}</translation>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="282"/>
+      <source>Started gain calculation for: {}</source>
+      <translation>S'ha iniciat el càlcul del guany per a: {}</translation>
     </message>
     <message>
-        <location filename="../../../plugins/replay_gain/replay_gain.py" line="317"/>
-        <source>Gain calculated for: {}</source>
-        <translation>Guany calculat per: {}</translation>
+      <location filename="../../../plugins/replay_gain/replay_gain.py" line="317"/>
+      <source>Gain calculated for: {}</source>
+      <translation>Guany calculat per: {}</translation>
     </message>
-</context>
+  </context>
 </TS>
