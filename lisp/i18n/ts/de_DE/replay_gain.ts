b4c17898cb3ff701dedb2dc9e4a1b24c75ffb22f
Francesco Ceruti <FrancescoCeruti@users.noreply.github.com>
2024-01-20 15:52:33 +0100

    New translations replay_gain.ts (German)

--- lisp/i18n/ts/de_DE/replay_gain.ts
+++ lisp/i18n/ts/de_DE/replay_gain.ts
@@ -67,12 +67,12 @@
     <message>
       <location filename="../../../plugins/replay_gain/replay_gain.py" line="331"/>
       <source>An error occurred during gain calculation.</source>
-      <translation type="unfinished">An error occurred during gain calculation.</translation>
+      <translation>Bei der Gain-Berechnung ist ein Fehler aufgetreten.</translation>
     </message>
     <message>
       <location filename="../../../plugins/replay_gain/replay_gain.py" line="203"/>
       <source>An error occurred while processing gain results.</source>
-      <translation type="unfinished">An error occurred while processing gain results.</translation>
+      <translation>Ein Fehler trat bei der Verarbeitung der Gain-Ergebnisse auf.</translation>
     </message>
   </context>
   <context>
