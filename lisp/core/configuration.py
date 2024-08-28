c62e56f3d81826cd4ef88eabe2990d7f181009c8
Francesco Ceruti <ceppofrancy@gmail.com>
2024-08-28 21:50:40 +0200

    wip

--- lisp/core/configuration.py
+++ lisp/core/configuration.py
@@ -15,132 +15,21 @@
 # You should have received a copy of the GNU General Public License
 # along with Linux Show Player.  If not, see <http://www.gnu.org/licenses/>.
 
-# Used to indicate the default behaviour when a specific option is not found to
-# raise an exception. Created to enable `None' as a valid fallback value.
-
 import json
 import logging
 from abc import ABCMeta, abstractmethod
-from copy import deepcopy
 from os import path
 from shutil import copyfile
 
+from lisp.core.collections.nested_dict import _UNSET, NestedDict
 from lisp.core.signal import Signal
-from lisp.core.util import dict_merge, dict_merge_diff, typename
+from lisp.core.util import dict_merge_diff
 from lisp.ui.ui_utils import translate
 
 logger = logging.getLogger(__name__)
 
-_UNSET = object()
 
-
-class ConfDictError(Exception):
-    pass
-
-
-class ConfDict:
-    """Allow to access nested-dictionaries values using "paths"."""
-
-    def __init__(self, root=None, sep="."):
-        if not sep:
-            raise ValueError("ConfDict separator cannot be empty")
-        if not isinstance(sep, str):
-            raise TypeError(
-                f"ConfDict separator must be a str, not {typename(sep)}"
-            )
-
-        self._sep = sep
-
-        if root is None:
-            self._root = {}
-        elif isinstance(root, dict):
-            self._root = root
-        else:
-            raise TypeError(
-                f"ConfDict root must be a dict, not {typename(root)}"
-            )
-
-    def get(self, path, default=_UNSET):
-        try:
-            node, key = self.__traverse(self.sp(path), self._root)
-            return node[key]
-        except (KeyError, TypeError):
-            if default is not _UNSET:
-                logger.info(
-                    translate(
-                        "ConfigurationInfo",
-                        'Invalid path "{}", return default.',
-                    ).format(path)
-                )
-                return default
-
-            raise ConfDictError("invalid path")
-
-    def set(self, path, value):
-        try:
-            node, key = self.__traverse(self.sp(path), self._root, set_=True)
-            if node.get(key, _UNSET) != value:
-                node[key] = value
-                return True
-
-            return False
-        except (KeyError, TypeError):
-            raise ConfDictError("invalid path")
-
-    def pop(self, path):
-        try:
-            node, key = self.__traverse(self.sp(path), self._root)
-            return node.pop(key)
-        except (KeyError, TypeError):
-            raise ConfDictError("invalid path")
-
-    def update(self, new_conf):
-        """Update the ConfDict using the given dictionary.
-
-        :param new_conf: a dict containing the new values
-        :type new_conf: dict
-        """
-        dict_merge(self._root, deepcopy(new_conf))
-
-    def deep_copy(self):
-        """Return a deep-copy of the internal dictionary."""
-        return deepcopy(self._root)
-
-    def jp(self, *paths):
-        return self._sep.join(paths)
-
-    def sp(self, path):
-        return path.split(self._sep)
-
-    def __traverse(self, keys, root, set_=False):
-        next_step = keys.pop(0)
-
-        if keys:
-            if set_ and next_step not in root:
-                root[next_step] = {}
-
-            return self.__traverse(keys, root[next_step])
-
-        return root, next_step
-
-    def __getitem__(self, path):
-        return self.get(path)
-
-    def __setitem__(self, path, value):
-        self.set(path, value)
-
-    def __delitem__(self, path):
-        self.pop(path)
-
-    def __contains__(self, path):
-        try:
-            node, key = self.__traverse(path.split(self._sep), self._root)
-            return key in node
-        except (KeyError, TypeError):
-            return False
-
-
-class Configuration(ConfDict, metaclass=ABCMeta):
+class Configuration(NestedDict, metaclass=ABCMeta):
     """ABC for configuration objects.
 
     Subclasses need to implement `read` and `write` methods.
@@ -159,15 +48,28 @@ class Configuration(ConfDict, metaclass=ABCMeta):
     def write(self):
         pass
 
-    def set(self, path, value):
-        changed = super().set(path, value)
+    def get(self, key, default=_UNSET):
+        value = super().get(key, default)
+
+        if value is default:
+            logger.info(
+                translate(
+                    "ConfigurationInfo",
+                    'Invalid path "{}", return default.',
+                ).format(key)
+            )
+
+        return value
+
+    def set(self, key, value):
+        changed = super().set(key, value)
         if changed:
-            self.changed.emit(path, value)
+            self.changed.emit(key, value)
 
         return changed
 
-    def update(self, new_conf):
-        diff = dict_merge_diff(self._root, new_conf)
+    def update(self, other):
+        diff = dict_merge_diff(self._root, other)
         if diff:
             super().update(diff)
             self.updated.emit(diff)
@@ -251,6 +153,6 @@ class JSONFileConfiguration(Configuration):
         )
 
     @staticmethod
-    def _read_json(path):
-        with open(path, "r") as f:
+    def _read_json(key):
+        with open(key, "r") as f:
             return json.load(f)
