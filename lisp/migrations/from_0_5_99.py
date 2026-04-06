6170202a72ac77cb2a779ed30252fb743244403f
Francesco Ceruti <ceppofrancy@gmail.com>
2026-04-06 17:26:13 +0200

    cleanup

--- lisp/migrations/from_0_5_99.py
+++ lisp/migrations/from_0_5_99.py
@@ -9,8 +9,8 @@ def migrate(filename: str, session: DotDict):
     )
 
     for cue in session["cues"]:
-       match cue.get("next_action", ""):
-        case "AutoNext":
-            cue["next-action"] = "TriggerAfterWait"
-        case "AutoFollow":
-            cue["next-action"] = "TriggerAfterEnd"
+        match cue.get("next_action", ""):
+            case "AutoNext":
+                cue["next-action"] = "TriggerAfterWait"
+            case "AutoFollow":
+                cue["next-action"] = "TriggerAfterEnd"
