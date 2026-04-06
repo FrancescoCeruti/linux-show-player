e14e4b72233289a107a348571fab14dd5921dde3
Francesco Ceruti <ceppofrancy@gmail.com>
2026-04-06 17:24:27 +0200

    cleanup

--- lisp/core/util.py
+++ lisp/core/util.py
@@ -19,7 +19,7 @@ import functools
 import hashlib
 import re
 import socket
-from collections.abc import Mapping, MutableMapping, Sequence
+from collections.abc import Mapping, MutableMapping
 from enum import Enum
 from os import listdir
 from os.path import exists, isdir, join
