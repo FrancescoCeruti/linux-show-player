31c9dab45bfa6db6458b6ce95bec4059cc5495d6
Bryan Enneking <109436942+BryanEnneking@users.noreply.github.com>
2026-08-01 16:48:38 -0500

    Recolor Cue icons (#367)
    Instead of having separate files for each variation of a cue type icon (running, pause, error, cart), this code we now recolor the base icons.
    
    Co-authored-by: Francesco Ceruti <ceppofrancy@gmail.com>

--- lisp/ui/icons/__init__.py
+++ lisp/ui/icons/__init__.py
@@ -17,9 +17,11 @@
 
 import glob
 import os
-from typing import Union
+from xml.etree import ElementTree as ET
 
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QPixmap, QPainter
+from PyQt5.QtSvg import QSvgRenderer
+from PyQt5.QtCore import Qt, QByteArray
 
 from lisp import ICON_THEMES_DIR, ICON_THEME_COMMON
 
@@ -35,8 +37,14 @@ def icon_themes_names():
 
 
 class IconTheme:
-    _SEARCH_PATTERN = "{}/**/{}.*"
+    _GLOB_PATTERN = "{}/**/{}.*"
     _BLANK_ICON = QIcon()
+    _VARIANTS = {
+        "-cart": {"stroke": "black", "fill": "black", "opacity": "0.1"},
+        "-running": {"stroke": "#0D0", "fill": "#0D0", "opacity": "1"},
+        "-pause": {"stroke": "#F90", "fill": "#F90", "opacity": "1"},
+        "-error": {"stroke": "#D11", "fill": "#D11", "opacity": "1"},
+    }
     _GlobalCache = {}
     _GlobalTheme = None
 
@@ -47,25 +55,76 @@ class IconTheme:
         yield from self._lookup_dirs
 
     @staticmethod
-    def get(icon_name) -> Union[QIcon, None]:
+    def get(icon_name: str) -> QIcon:
         icon = IconTheme._GlobalCache.get(icon_name, None)
+
+        if icon is not None:
+            return icon
+
+        for search_dir in IconTheme._GlobalTheme:
+            icon = IconTheme._get_icon(search_dir, icon_name)
+            if icon is not None:
+                break
 
         if icon is None:
             icon = IconTheme._BLANK_ICON
-            for dir_ in IconTheme._GlobalTheme:
-                pattern = IconTheme._SEARCH_PATTERN.format(dir_, icon_name)
-                for icon in glob.iglob(pattern, recursive=True):
-                    icon = QIcon(icon)
-                    break
 
-            IconTheme._GlobalCache[icon_name] = icon
+        IconTheme._GlobalCache[icon_name] = icon
 
         return icon
 
     @staticmethod
-    def set_theme_name(theme_name):
+    def _get_icon(search_dir: str, icon_name: str) -> QIcon | None:
+        pattern = IconTheme._GLOB_PATTERN.format(search_dir, icon_name)
+        for icon in glob.iglob(pattern, recursive=True):
+            return QIcon(icon)
+
+        icon_name, suffix = IconTheme._split_variant(icon_name)
+        if suffix:
+            pattern = IconTheme._GLOB_PATTERN.format(search_dir, icon_name)
+            for icon in glob.iglob(pattern, recursive=True):
+                return IconTheme._load_modified_icon(icon, suffix)
+
+    @staticmethod
+    def set_theme_name(theme_name: str):
         IconTheme._GlobalCache.clear()
         IconTheme._GlobalTheme = IconTheme(theme_name, ICON_THEME_COMMON)
 
         QIcon.setThemeSearchPaths([ICON_THEMES_DIR])
         QIcon.setThemeName(theme_name)
+
+    @staticmethod
+    def _split_variant(icon_name: str) -> tuple[str, str]:
+        for suffix in IconTheme._VARIANTS:
+            if icon_name.endswith(suffix):
+                return (icon_name[: -len(suffix)], suffix)
+
+        return (icon_name, "")
+
+    @staticmethod
+    def _load_modified_icon(icon_path: str, suffix: str) -> QIcon | None:
+        try:
+            with open(icon_path, "rb") as icon_file:
+                icon_content = icon_file.read()
+
+            root = ET.fromstring(icon_content)
+            variant = IconTheme._VARIANTS[suffix]
+            for attr, val in variant.items():
+                root.set(attr, val)
+
+            renderer = QSvgRenderer(
+                QByteArray(
+                    ET.tostring(root, encoding="utf-8", xml_declaration=True)
+                )
+            )
+
+            pixmap = QPixmap(renderer.defaultSize())
+            pixmap.fill(Qt.transparent)
+
+            painter = QPainter(pixmap)
+            renderer.render(painter)
+            painter.end()
+
+            return QIcon(pixmap)
+        except Exception:
+            return None
