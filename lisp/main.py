31e60b430e3afbae780829adc139aec831aa3851
KeelTrace <218190424+keeltrace@users.noreply.github.com>
2026-09-12 09:53:12 -0700

    fix: use Flatpak app ID for desktop file name

--- lisp/main.py
+++ lisp/main.py
@@ -108,7 +108,7 @@ def main():
     # Create the QApplication
     qt_app = QApplication(sys.argv)
     qt_app.setApplicationName("Linux Show Player")
-    qt_app.setDesktopFileName("linuxshowplayer")
+    qt_app.setDesktopFileName(os.environ.get("FLATPAK_ID", "linuxshowplayer"))
     qt_app.setQuitOnLastWindowClosed(True)
 
     # Get/Set the locale
